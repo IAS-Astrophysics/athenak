75487f8a9e19b485c348c42accb69c4c622ff437
James Stone <jmstone@ias.edu>
2020-06-27 11:25:01 -0400

    first commit of IO wrapper files

---
+++ src/outputs/io_wrapper.hpp
@@ -0,0 +1,57 @@
+#ifndef OUTPUTS_IO_WRAPPER_HPP_
+#define OUTPUTS_IO_WRAPPER_HPP_
+//==================================================================================================
+// Athena++ astrophysical MHD code
+// Copyright(C) 2014 James M. Stone <jmstone@princeton.edu> and other code contributors
+//==================================================================================================
+// AthenaXXX astrophysical plasma code
+// Copyright(C) 2020 James M. Stone <jmstone@ias.edu> and the Athena code team
+// Licensed under the 3-clause BSD License (the "LICENSE")
+//==================================================================================================
+//! \file io_wrapper.hpp
+//  \brief defines a set of small wrapper functions for MPI versus serial outputs.
+
+#include <cstdio>
+#include "athena.hpp"
+
+#if MPI_PARALLEL_ENABLED
+#include <mpi.h>
+using  IOWrapperFile = MPI_File;
+#else
+using  IOWrapperFile = FILE*;
+#endif
+
+using IOWrapperSizeT = std::uint64_t;
+
+class IOWrapper {
+ public:
+#if MPI_PARALLEL_ENABLED
+  IOWrapper() : fh_(nullptr), comm_(MPI_COMM_WORLD) {}
+  void SetCommunicator(MPI_Comm scomm) { comm_=scomm;}
+#else
+  IOWrapper() {fh_=nullptr;}
+#endif
+  ~IOWrapper() {}
+  // nested type definition of strongly typed/scoped enum in class definition
+  enum class FileMode {read, write};
+
+  // wrapper functions for basic I/O tasks
+  int Open(const char* fname, FileMode rw);
+  std::size_t Read(void *buf, IOWrapperSizeT size, IOWrapperSizeT count);
+  std::size_t Read_all(void *buf, IOWrapperSizeT size, IOWrapperSizeT count);
+  std::size_t Read_at_all(void *buf, IOWrapperSizeT size, IOWrapperSizeT count,
+                          IOWrapperSizeT offset);
+  std::size_t Write(const void *buf, IOWrapperSizeT size, IOWrapperSizeT count);
+  std::size_t Write_at_all(const void *buf, IOWrapperSizeT size, IOWrapperSizeT count,
+                           IOWrapperSizeT offset);
+  int Close();
+  int Seek(IOWrapperSizeT offset);
+  IOWrapperSizeT GetPosition();
+
+ private:
+  IOWrapperFile fh_;
+#if MPI_PARALLEL_ENABLED
+  MPI_Comm comm_;
+#endif
+};
+#endif // OUTPUTS_IO_WRAPPER_HPP_
