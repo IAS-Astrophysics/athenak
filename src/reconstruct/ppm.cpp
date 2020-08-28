17afa7ac95cd7e13e577a9b833a5d4715588f4bb
James Stone <jmstone@ias.edu>
2020-08-28 12:47:02 -0400

    Added PPM reconstruction in x2 and x3.  Passes simple advection test.

--- src/reconstruct/ppm.cpp
+++ src/reconstruct/ppm.cpp
@@ -4,8 +4,13 @@
 // Licensed under the 3-clause BSD License (the "LICENSE")
 //========================================================================================
 //! \file ppm.cpp
-//  \brief piecewise parabolic reconstruction with modified McCorquodale/Colella limiter
-//         for a Cartesian-like coordinate with uniform spacing
+//  \brief piecewise parabolic reconstruction with Collela-Sekora extremum preserving
+//  limiters for a Cartesian-like coordinate with uniform spacing.
+//
+// This version does not include the extensions to the CS limiters described by
+// McCorquodale et al. and as implemented in Athena++ by K. Felker.  This is to keep the
+// code simple, because Kyle found these extensions did not improve the solution very
+// much in practice, and because they can break monotonicity.
 //
 // REFERENCES:
 // (CW) P. Colella & P. Woodward, "The Piecewise Parabolic Method (PPM) for Gas-Dynamical
@@ -145,6 +150,93 @@ void PiecewiseParabolic::ReconstructX1(const int k,const int j,const int il,cons
 void PiecewiseParabolic::ReconstructX2(const int k,const int j,const int il,const int iu,
     const AthenaArray<Real> &q, AthenaArray<Real> &ql_jp1, AthenaArray<Real> &qr_j)
 {
+  int nvar = q.GetDim(4);
+  for (int n=0; n<nvar; ++n) {
+    // Compute L/R values (CS eqns 12-15, PH 3.26 and 3.27)
+    // qlv = q at left  side of cell-center = q[i-1/2] = a_{j,-} in CS
+    // qrv = q at right side of cell-center = q[i+1/2] = a_{j,+} in CS
+    for (int i=il; i<=iu; ++i) {
+      qlv_(i) = (7.0*(q(n,k,j,i) + q(n,k,j-1,i)) - (q(n,k,j-2,i) + q(n,k,j+1,i)))/12.0;
+      qrv_(i) = (7.0*(q(n,k,j,i) + q(n,k,j+1,i)) - (q(n,k,j-1,i) + q(n,k,j+2,i)))/12.0;
+    }
+
+    // Apply CS monotonicity limiters to qrv_ and qlv_
+    for (int i=il; i<=iu; ++i) {
+      // approximate second derivatives at i-1/2 (PH 3.35) 
+      Real d2qc = 3.0*(q(n,k,j-1,i) - 2.0*qlv_(i) + q(n,k,j,i));
+      Real d2ql = (q(n,k,j-2,i) - 2.0*q(n,k,j-1,i) + q(n,k,j  ,i));
+      Real d2qr = (q(n,k,j-1,i) - 2.0*q(n,k,j  ,i) + q(n,k,j+1,i));
+    
+      // limit second derivative (PH 3.36)
+      Real d2qlim = 0.0;
+      Real lim_slope = std::min(fabs(d2ql),fabs(d2qr));
+      if (d2qc > 0.0 && d2ql > 0.0 && d2qr > 0.0) {
+        d2qlim = SIGN(d2qc)*std::min(1.25*lim_slope,fabs(d2qc));
+      }
+      if (d2qc < 0.0 && d2ql < 0.0 && d2qr < 0.0) {
+        d2qlim = SIGN(d2qc)*std::min(1.25*lim_slope,fabs(d2qc));
+      } 
+      // compute limited value for qlv_ (PH 3.34)
+      qlv_(i) = 0.5*(q(n,k,j,i) + q(n,k,j-1,i)) - d2qlim/6.0;
+
+      // approximate second derivatives at i+1/2 (PH 3.35) 
+      d2qc = 3.0*(q(n,k,j,i) - 2.0*qrv_(i) + q(n,k,j+1,i));
+      d2ql = d2qr;
+      d2qr = (q(n,k,j,i  ) - 2.0*q(n,k,j+1,i) + q(n,k,j+2,i));
+
+      // limit second derivative (PH 3.36)
+      d2qlim = 0.0;
+      lim_slope = std::min(fabs(d2ql),fabs(d2qr));
+      if (d2qc > 0.0 && d2ql > 0.0 && d2qr > 0.0) {
+        d2qlim = SIGN(d2qc)*std::min(1.25*lim_slope,fabs(d2qc));
+      }
+      if (d2qc < 0.0 && d2ql < 0.0 && d2qr < 0.0) {
+        d2qlim = SIGN(d2qc)*std::min(1.25*lim_slope,fabs(d2qc));
+      }
+      // compute limited value for qrv_ (PH 3.33)
+      qrv_(i) = 0.5*(q(n,k,j,i) + q(n,k,j+1,i)) - d2qlim/6.0;
+    }
+
+    // identify extrema, use smooth extremum limiter
+    for (int i=il; i<=iu; ++i) {
+      // CS 20 (missing "OR"), and PH 3.31
+      Real qa = (qrv_(i) - q(n,k,j,i))*(q(n,k,j,i) - qlv_(i));
+      Real qb = (q(n,k,j-1,i) - q(n,k,j,i))*(q(n,k,j,i) - q(n,k,j+1,i));
+      if (qa <= 0.0 || qb <= 0.0) {
+        // approximate secnd derivates (PH 3.37)
+        Real d2q  = 6.0*(qlv_(i) - 2.0*q(n,k,j,i) + qrv_(i));
+        Real d2qc = (q(n,k,j-1,i) - 2.0*q(n,k,j  ,i) + q(n,k,j+1,i));
+        Real d2ql = (q(n,k,j-2,i) - 2.0*q(n,k,j-1,i) + q(n,k,j  ,i));
+        Real d2qr = (q(n,k,j  ,i) - 2.0*q(n,k,j+1,i) + q(n,k,j+2,i));
+
+        // limit second derivatives (PH 3.38)
+        Real d2qlim = 0.0;
+        Real lim_slope = std::min(fabs(d2ql),fabs(d2qr));
+        lim_slope = std::min(fabs(d2qc),lim_slope);
+        if (d2qc > 0.0 && d2ql > 0.0 && d2qr > 0.0 && d2q > 0.0) {
+          d2qlim = SIGN(d2q)*std::min(1.25*lim_slope,fabs(d2q));
+        }
+        if (d2qc < 0.0 && d2ql < 0.0 && d2qr < 0.0 && d2q < 0.0) {
+          d2qlim = SIGN(d2q)*std::min(1.25*lim_slope,fabs(d2q));
+        }
+
+        // limit L/R states at extrema (PH 3.39)
+        if (d2q == 0.0) {  // revert to donor cell
+          qlv_(i) = q(n,k,j,i);
+          qrv_(i) = q(n,k,j,i);
+        } else {  // add limited slope (PH 3.39)
+          qlv_(i) = q(n,k,j,i) + (qlv_(i) - q(n,k,j,i))*d2qlim/d2q;
+          qrv_(i) = q(n,k,j,i) + (qrv_(i) - q(n,k,j,i))*d2qlim/d2q;
+        }
+      }
+    }
+
+    // set L/R states
+    for (int i=il; i<=iu; ++i) {
+      ql_jp1(n,i) = qrv_(i);
+      qr_j(n,i)   = qlv_(i);
+    }
+  }
   return;
 }
 
@@ -156,5 +248,92 @@ void PiecewiseParabolic::ReconstructX2(const int k,const int j,const int il,cons
 void PiecewiseParabolic::ReconstructX3(const int k,const int j,const int il,const int iu,
     const AthenaArray<Real> &q, AthenaArray<Real> &ql_kp1, AthenaArray<Real> &qr_k)
 {
+  int nvar = q.GetDim(4);
+  for (int n=0; n<nvar; ++n) {
+    // Compute L/R values (CS eqns 12-15, PH 3.26 and 3.27)
+    // qlv = q at left  side of cell-center = q[i-1/2] = a_{j,-} in CS
+    // qrv = q at right side of cell-center = q[i+1/2] = a_{j,+} in CS
+    for (int i=il; i<=iu; ++i) {
+      qlv_(i) = (7.0*(q(n,k,j,i) + q(n,k-1,j,i)) - (q(n,k-2,j,i) + q(n,k+1,j,i)))/12.0;
+      qrv_(i) = (7.0*(q(n,k,j,i) + q(n,k+1,j,i)) - (q(n,k-1,j,i) + q(n,k+2,j,i)))/12.0;
+    }
+
+    // Apply CS monotonicity limiters to qrv_ and qlv_
+    for (int i=il; i<=iu; ++i) {
+      // approximate second derivatives at i-1/2 (PH 3.35) 
+      Real d2qc = 3.0*(q(n,k-1,j,i) - 2.0*qlv_(i) + q(n,k,j,i));
+      Real d2ql = (q(n,k-2,j,i) - 2.0*q(n,k-1,j,i) + q(n,k  ,j,i));
+      Real d2qr = (q(n,k-1,j,i) - 2.0*q(n,k  ,j,i) + q(n,k+1,j,i));
+    
+      // limit second derivative (PH 3.36)
+      Real d2qlim = 0.0;
+      Real lim_slope = std::min(fabs(d2ql),fabs(d2qr));
+      if (d2qc > 0.0 && d2ql > 0.0 && d2qr > 0.0) {
+        d2qlim = SIGN(d2qc)*std::min(1.25*lim_slope,fabs(d2qc));
+      }
+      if (d2qc < 0.0 && d2ql < 0.0 && d2qr < 0.0) {
+        d2qlim = SIGN(d2qc)*std::min(1.25*lim_slope,fabs(d2qc));
+      } 
+      // compute limited value for qlv_ (PH 3.34)
+      qlv_(i) = 0.5*(q(n,k,j,i) + q(n,k-1,j,i)) - d2qlim/6.0;
+
+      // approximate second derivatives at i+1/2 (PH 3.35) 
+      d2qc = 3.0*(q(n,k,j,i) - 2.0*qrv_(i) + q(n,k+1,j,i));
+      d2ql = d2qr;
+      d2qr = (q(n,k,j,i  ) - 2.0*q(n,k+1,j,i) + q(n,k+2,j,i));
+
+      // limit second derivative (PH 3.36)
+      d2qlim = 0.0;
+      lim_slope = std::min(fabs(d2ql),fabs(d2qr));
+      if (d2qc > 0.0 && d2ql > 0.0 && d2qr > 0.0) {
+        d2qlim = SIGN(d2qc)*std::min(1.25*lim_slope,fabs(d2qc));
+      }
+      if (d2qc < 0.0 && d2ql < 0.0 && d2qr < 0.0) {
+        d2qlim = SIGN(d2qc)*std::min(1.25*lim_slope,fabs(d2qc));
+      }
+      // compute limited value for qrv_ (PH 3.33)
+      qrv_(i) = 0.5*(q(n,k,j,i) + q(n,k+1,j,i)) - d2qlim/6.0;
+    }
+
+    // identify extrema, use smooth extremum limiter
+    for (int i=il; i<=iu; ++i) {
+      // CS 20 (missing "OR"), and PH 3.31
+      Real qa = (qrv_(i) - q(n,k,j,i))*(q(n,k,j,i) - qlv_(i));
+      Real qb = (q(n,k-1,j,i) - q(n,k,j,i))*(q(n,k,j,i) - q(n,k+1,j,i));
+      if (qa <= 0.0 || qb <= 0.0) {
+        // approximate secnd derivates (PH 3.37)
+        Real d2q  = 6.0*(qlv_(i) - 2.0*q(n,k,j,i) + qrv_(i));
+        Real d2qc = (q(n,k-1,j,i) - 2.0*q(n,k  ,j,i) + q(n,k+1,j,i));
+        Real d2ql = (q(n,k-2,j,i) - 2.0*q(n,k-1,j,i) + q(n,k  ,j,i));
+        Real d2qr = (q(n,k  ,j,i) - 2.0*q(n,k+1,j,i) + q(n,k+2,j,i));
+
+        // limit second derivatives (PH 3.38)
+        Real d2qlim = 0.0;
+        Real lim_slope = std::min(fabs(d2ql),fabs(d2qr));
+        lim_slope = std::min(fabs(d2qc),lim_slope);
+        if (d2qc > 0.0 && d2ql > 0.0 && d2qr > 0.0 && d2q > 0.0) {
+          d2qlim = SIGN(d2q)*std::min(1.25*lim_slope,fabs(d2q));
+        }
+        if (d2qc < 0.0 && d2ql < 0.0 && d2qr < 0.0 && d2q < 0.0) {
+          d2qlim = SIGN(d2q)*std::min(1.25*lim_slope,fabs(d2q));
+        }
+
+        // limit L/R states at extrema (PH 3.39)
+        if (d2q == 0.0) {  // revert to donor cell
+          qlv_(i) = q(n,k,j,i);
+          qrv_(i) = q(n,k,j,i);
+        } else {  // add limited slope (PH 3.39)
+          qlv_(i) = q(n,k,j,i) + (qlv_(i) - q(n,k,j,i))*d2qlim/d2q;
+          qrv_(i) = q(n,k,j,i) + (qrv_(i) - q(n,k,j,i))*d2qlim/d2q;
+        }
+      }
+    }
+
+    // set L/R states
+    for (int i=il; i<=iu; ++i) {
+      ql_kp1(n,i) = qrv_(i);
+      qr_k(n,i  ) = qlv_(i);
+    }
+  }
   return;
 }
