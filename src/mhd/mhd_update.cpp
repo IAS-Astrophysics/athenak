1060ebf7b47860a42d76cd26c6a88f9bd764c678
James Stone <jmstone@ias.edu>
2021-02-02 20:26:54 -0500

    Added MHD update function.
    This design stores 3D fluxes, so update function now computes divF.
    This is needed for MHD where 3D EMFs are needed.  Also will enable
    first-order flux correction.

---
+++ src/mhd/mhd_update.cpp
@@ -0,0 +1,89 @@
+//========================================================================================
+// AthenaXXX astrophysical plasma code
+// Copyright(C) 2020 James M. Stone <jmstone@ias.edu> and the Athena code team
+// Licensed under the 3-clause BSD License (the "LICENSE")
+//========================================================================================
+//! \file mhd_update.cpp
+//  \brief Updates MHD conserved variables, using weighted average and partial time
+//  step appropriate for various SSP RK integrators (e.g. RK1, RK2, RK3)
+
+#include "athena.hpp"
+#include "mesh/mesh.hpp"
+#include "driver/driver.hpp"
+#include "mhd.hpp"
+
+namespace mhd {
+//----------------------------------------------------------------------------------------
+//! \fn  void MHD::MHDUpdate
+//  \brief Update conserved variables 
+
+TaskStatus MHD::MHDUpdate(Driver *pdrive, int stage)
+{
+  int is = pmy_pack->mb_cells.is; int ie = pmy_pack->mb_cells.ie;
+  int js = pmy_pack->mb_cells.js; int je = pmy_pack->mb_cells.je;
+  int ks = pmy_pack->mb_cells.ks; int ke = pmy_pack->mb_cells.ke;
+  int ncells1 = pmy_pack->mb_cells.nx1 + 2*(pmy_pack->mb_cells.ng);
+  bool &two_d   = pmy_pack->pmesh->nx2gt1;
+  bool &three_d = pmy_pack->pmesh->nx3gt1;
+
+  Real &gam0 = pdrive->gam0[stage-1];
+  Real &gam1 = pdrive->gam1[stage-1];
+  Real beta_dt = (pdrive->beta[stage-1])*(pmy_pack->pmesh->dt);
+  int nmb1 = pmy_pack->nmb_thispack - 1;
+  int nv1 = nmhd + nscalars - 1;
+  auto u0_ = u0;
+  auto u1_ = u1;
+  auto flx1 = flux1;
+  auto flx2 = flux2;
+  auto flx3 = flux3;
+  auto &mbsize = pmy_pack->pmb->mbsize;
+
+  // hierarchical parallel loop that updates conserved variables to intermediate step
+  // using weights and fractional time step appropriate to stages of time-integrator used
+  // Important to use vector inner loop for good performance on cpus
+  int scr_level = 0;
+  size_t scr_size = ScrArray1D<Real>::shmem_size(ncells1);
+
+  par_for_outer("mhd_update",DevExeSpace(),scr_size,scr_level,0,nmb1,0,nv1, ks, ke, js, je,
+    KOKKOS_LAMBDA(TeamMember_t member, const int m, const int n, const int k, const int j)
+    {
+      ScrArray1D<Real> divf(member.team_scratch(scr_level), ncells1);
+
+      // compute dF1/dx1
+      par_for_inner(member, is, ie, [&](const int i)
+      {
+        divf(i) = (flux1(m,n,k,j,i+1) - flux1(m,n,k,j,i))/mbsize.dx1.d_view(m);
+      });
+      member.team_barrier();
+
+      // Add dF2/dx2
+      // Fluxes must be summed in pairs to symmetrize round-off error in each dir
+      if (two_d) {
+        par_for_inner(member, is, ie, [&](const int i)
+        {
+          divf(i) += (flux2(m,n,k,j+1,i) - flux2(m,n,k,j,i))/mbsize.dx2.d_view(m);
+        });
+        member.team_barrier();
+      }
+
+      // Add dF3/dx3
+      // Fluxes must be summed in pairs to symmetrize round-off error in each dir
+      if (three_d) {
+        par_for_inner(member, is, ie, [&](const int i)
+        {
+          divf(i) += (flux3(m,n,k+1,j,i) - flux3(m,n,k,j,i))/mbsize.dx3.d_view(m);
+        });
+        member.team_barrier();
+      }
+
+      par_for_inner(member, is, ie, [&](const int i)
+      {
+        u0_(m,n,k,j,i) = gam0*u0_(m,n,k,j,i) + gam1*u1_(m,n,k,j,i) - beta_dt*divf(i);
+      });
+
+    }
+  );
+
+  return TaskStatus::complete;
+}
+} // namespace hydro
