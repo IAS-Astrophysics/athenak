8e43efeae0ba0bc13559a9a9c51776d5888701a8
James Stone <jmstone@ias.edu>
2021-11-19 15:43:58 -0500

    Added RestartOutput class and hook for outputting restarts.

---
+++ src/outputs/restart.cpp
@@ -0,0 +1,96 @@
+//========================================================================================
+// AthenaXXX astrophysical plasma code
+// Copyright(C) 2020 James M. Stone <jmstone@ias.edu> and the Athena code team
+// Licensed under the 3-clause BSD License (the "LICENSE")
+//========================================================================================
+//! \file restart.cpp
+//  \brief writes restart files
+
+#include <algorithm>
+#include <cstdio>      // fwrite(), fclose(), fopen(), fnprintf(), snprintf()
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <sys/stat.h>  // mkdir
+
+#include "athena.hpp"
+#include "coordinates/cell_locations.hpp"
+#include "globals.hpp"
+#include "mesh/mesh.hpp"
+#include "hydro/hydro.hpp"
+#include "outputs.hpp"
+
+//----------------------------------------------------------------------------------------
+// ctor: also calls OutputType base class constructor
+
+RestartOutput::RestartOutput(OutputParameters op, Mesh *pm)
+  : OutputType(op, pm)
+{
+  // create directories for outputs. Comments in binary.cpp constructor explain why
+  mkdir("rst",0775);
+}
+
+//----------------------------------------------------------------------------------------
+//! \fn void RestartOutput:::WriteOutputFile(Mesh *pm)
+//  \brief Cycles over all MeshBlocks and writes everything to a single restart file
+
+void RestartOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin)
+{
+  // create filename: "rst/file_basename" + "." + XXXXX + ".rst"
+  // where XXXXX = 5-digit file_number
+  std::string fname;
+  char number[6];
+  std::snprintf(number, sizeof(number), "%05d", out_params.file_number);
+
+  fname.assign("rst/");
+  fname.append(out_params.file_basename);
+  fname.append(".");
+  fname.append(number);
+  fname.append(".rst");
+
+  // increment counters now so values for *next* dump are stored in restart file
+  output_params.file_number++;
+  output_params.next_time += output_params.dt;
+  pin->SetInteger(output_params.block_name, "file_number", output_params.file_number);
+  pin->SetReal(output_params.block_name, "next_time", output_params.next_time);
+
+  IOWrapper rstfile;
+  rstfile.Open(fname.c_str(), IOWrapper::FileMode::write);
+
+  // prepare the input parameters
+  std::stringstream ost;
+  pin->ParameterDump(ost);
+  std::string sbuf = ost.str();
+
+  // calculate size of header
+  IOWrapperSizeT headeroffset = sbuf.size()*sizeof(char) + 3*sizeof(int)+sizeof(RegionSize)
+                 + 2*sizeof(Real)+sizeof(IOWrapperSizeT);
+  // the size of an element of the ID and cost list
+  listsize = sizeof(LogicalLocation)+sizeof(double);
+  // the size of each MeshBlock
+  datasize = pm->my_blocks(0)->GetBlockSizeInBytes();
+  int nbtotal = pm->nbtotal;
+  int myns = pm->nslist[Globals::my_rank];
+  int mynb = pm->nblist[Globals::my_rank];
+
+  // write the header; this part is serial
+  if (Globals::my_rank == 0) {
+    // output the input parameters
+    rstfile.Write(sbuf.c_str(),sizeof(char),sbuf.size());
+
+    // output Mesh information
+    rstfile.Write(&(pm->nbtotal), sizeof(int), 1);
+    rstfile.Write(&(pm->root_level), sizeof(int), 1);
+    rstfile.Write(&(pm->mesh_size), sizeof(RegionSize), 1);
+    rstfile.Write(&(pm->time), sizeof(Real), 1);
+    rstfile.Write(&(pm->dt), sizeof(Real), 1);
+    rstfile.Write(&(pm->ncycle), sizeof(int), 1);
+    rstfile.Write(&(datasize), sizeof(IOWrapperSizeT), 1);
+  }
+
+  rstfile.Close();
+
+  return;
+}
