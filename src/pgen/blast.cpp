5f11157724eae1c0465cfa153d327624eb287958
James Stone <jmstone@ias.edu>
2022-02-26 10:10:28 -0500

    Updated blast pgen to work with MHD. Added input file for Komissarov SR MHD blast test.  Tested that code runs NR version of this test OK with both PLM and PPM.

--- src/pgen/blast.cpp
+++ src/pgen/blast.cpp
@@ -18,6 +18,7 @@
 #include "mesh/mesh.hpp"
 #include "eos/eos.hpp"
 #include "hydro/hydro.hpp"
+#include "mhd/mhd.hpp"
 #include "coordinates/cell_locations.hpp"
 
 //----------------------------------------------------------------------------------------
@@ -27,12 +28,13 @@
 void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
   if (restart) return;
 
-  Real rout = pin->GetReal("problem", "radius");
-  Real rin  = rout - pin->GetOrAddReal("problem", "ramp", 0.0);
+  Real rout = pin->GetReal("problem", "outer_radius");
+  Real rin  = rout - pin->GetReal("problem", "inner_radius");
   Real pa   = pin->GetOrAddReal("problem", "pamb", 1.0);
   Real da   = pin->GetOrAddReal("problem", "damb", 1.0);
   Real prat = pin->GetReal("problem", "prat");
   Real drat = pin->GetOrAddReal("problem", "drat", 1.0);
+  Real bamb = pin->GetOrAddReal("problem", "bamb", 0.1);
 
   // capture variables for the kernel
   auto &indcs = pmy_mesh_->mb_indcs;
@@ -42,62 +44,96 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
   MeshBlockPack *pmbp = pmy_mesh_->pmb_pack;
   auto &size = pmbp->pmb->mb_size;
 
-  // setup uniform ambient medium with spherical over-pressured region in Hydro
+  // Select either Hydro or MHD
+  Real gm1;
+  DvceArray5D<Real> u0_, w0_;
   if (pmbp->phydro != nullptr) {
-    auto &eos = pmbp->phydro->peos->eos_data;
-    auto &w0 = pmbp->phydro->w0;
-    par_for("pgen_cloud1",DevExeSpace(),0,(pmbp->nmb_thispack-1),ks,ke,js,je,is,ie,
-    KOKKOS_LAMBDA(int m,int k,int j,int i) {
-      Real &x1min = size.d_view(m).x1min;
-      Real &x1max = size.d_view(m).x1max;
-      int nx1 = indcs.nx1;
-      Real x1v = CellCenterX(i-is, nx1, x1min, x1max);
+    u0_ = pmbp->phydro->u0;
+    w0_ = pmbp->phydro->w0;
+    gm1 = pmbp->phydro->peos->eos_data.gamma - 1.0;
+  } else if (pmbp->pmhd != nullptr) {
+    u0_ = pmbp->pmhd->u0;
+    w0_ = pmbp->pmhd->w0;
+    gm1 = pmbp->pmhd->peos->eos_data.gamma - 1.0;
+  }
 
-      Real &x2min = size.d_view(m).x2min;
-      Real &x2max = size.d_view(m).x2max;
-      int nx2 = indcs.nx2;
-      Real x2v = CellCenterX(j-js, nx2, x2min, x2max);
+  par_for("pgen_blast1",DevExeSpace(),0,(pmbp->nmb_thispack-1),ks,ke,js,je,is,ie,
+  KOKKOS_LAMBDA(int m,int k,int j,int i) {
+    Real &x1min = size.d_view(m).x1min;
+    Real &x1max = size.d_view(m).x1max;
+    int nx1 = indcs.nx1;
+    Real x1v = CellCenterX(i-is, nx1, x1min, x1max);
 
-      Real &x3min = size.d_view(m).x3min;
-      Real &x3max = size.d_view(m).x3max;
-      int nx3 = indcs.nx3;
-      Real x3v = CellCenterX(k-ks, nx3, x3min, x3max);
+    Real &x2min = size.d_view(m).x2min;
+    Real &x2max = size.d_view(m).x2max;
+    int nx2 = indcs.nx2;
+    Real x2v = CellCenterX(j-js, nx2, x2min, x2max);
 
-      Real rad = std::sqrt(SQR(x1v) + SQR(x2v) + SQR(x3v));
+    Real &x3min = size.d_view(m).x3min;
+    Real &x3max = size.d_view(m).x3max;
+    int nx3 = indcs.nx3;
+    Real x3v = CellCenterX(k-ks, nx3, x3min, x3max);
 
-      Real den = da;
-      Real pres = pa;
-      if (rad < rout) {
-        if (rad < rin) {
-          den = drat*da;
-          pres = prat*pa;
-        } else {   // add smooth ramp in density
-          Real f = (rad-rin) / (rout-rin);
-          Real log_den = (1.0-f) * log(drat*da) + f * log(da);
-          den = exp(log_den);
-          Real log_pres = (1.0-f) * log(prat*pa) + f * log(pa);
-          pres = exp(log_pres);
-        }
+    Real rad = std::sqrt(SQR(x1v) + SQR(x2v) + SQR(x3v));
+
+    Real den = da;
+    Real pres = pa;
+    if (rad < rout) {
+      if (rad < rin) {
+        den = drat*da;
+        pres = prat*pa;
+      } else {   // add smooth ramp in density
+        Real f = (rad-rin) / (rout-rin);
+        Real log_den = (1.0-f) * log(drat*da) + f * log(da);
+        den = exp(log_den);
+        Real log_pres = (1.0-f) * log(prat*pa) + f * log(pa);
+        pres = exp(log_pres);
       }
+    }
 
-      // set either internal energy density or temparature as primitive
-      Real prim;
-      if (eos.use_e) {
-        prim = pres/(eos.gamma - 1.0);
-      } else {
-        prim = pres/((eos.gamma - 1.0)*den);
-      }
+    w0_(m,IDN,k,j,i) = den;
+    w0_(m,IVX,k,j,i) = 0.0;
+    w0_(m,IVY,k,j,i) = 0.0;
+    w0_(m,IVZ,k,j,i) = 0.0;
+    w0_(m,IEN,k,j,i) = pres/gm1;
+  });
 
-      w0(m,IDN,k,j,i) = den;
-      w0(m,IVX,k,j,i) = 0.0;
-      w0(m,IVY,k,j,i) = 0.0;
-      w0(m,IVZ,k,j,i) = 0.0;
-      w0(m,IEN,k,j,i) = prim;
+  // initialize magnetic fields ---------------------------------------
+
+  if (pmbp->pmhd != nullptr) {
+    auto &b0 = pmbp->pmhd->b0;
+    par_for("pgen_blast2",DevExeSpace(),0,(pmbp->nmb_thispack-1),ks,ke,js,je,is,ie,
+    KOKKOS_LAMBDA(int m, int k, int j, int i) {
+      b0.x1f(m,k,j,i) = bamb;
+      b0.x2f(m,k,j,i) = 0.0;
+      b0.x3f(m,k,j,i) = 0.0;
+
+      // Include extra face-component at edge of block in each direction
+      if (i==ie) {b0.x1f(m,k,j,i+1) = bamb;}
+      if (j==je) {b0.x2f(m,k,j+1,i) = 0.0;}
+      if (k==ke) {b0.x3f(m,k+1,j,i) = 0.0;}
     });
 
-    // Convert primitives to conserved
-    auto &u0 = pmbp->phydro->u0;
-    pmbp->phydro->peos->PrimToCons(w0, u0, is, ie, js, je, ks, ke);
+    // Compute cell-centered fields
+    auto &bcc_ = pmbp->pmhd->bcc0;
+    par_for("pgen_blast3",DevExeSpace(),0,(pmbp->nmb_thispack-1),ks,ke,js,je,is,ie,
+    KOKKOS_LAMBDA(int m, int k, int j, int i) {
+      // cell-centered fields are simple linear average of face-centered fields
+      Real& w_bx = bcc_(m,IBX,k,j,i);
+      Real& w_by = bcc_(m,IBY,k,j,i);
+      Real& w_bz = bcc_(m,IBZ,k,j,i);
+      w_bx = 0.5*(b0.x1f(m,k,j,i) + b0.x1f(m,k,j,i+1));
+      w_by = 0.5*(b0.x2f(m,k,j,i) + b0.x2f(m,k,j+1,i));
+      w_bz = 0.5*(b0.x3f(m,k,j,i) + b0.x3f(m,k+1,j,i));
+    });
+  }
+
+  // Convert primitives to conserved
+  if (pmbp->phydro != nullptr) {
+    pmbp->phydro->peos->PrimToCons(w0_, u0_, is, ie, js, je, ks, ke);
+  } else if (pmbp->pmhd != nullptr) {
+    auto &bcc0_ = pmbp->pmhd->bcc0;
+    pmbp->pmhd->peos->PrimToCons(w0_, bcc0_, u0_, is, ie, js, je, ks, ke);
   }
 
   return;
