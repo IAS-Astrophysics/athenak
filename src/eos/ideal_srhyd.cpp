de7f180d6068a156a59c87756995b1214f7a73ec
James Stone <jmstone@ias.edu>
2022-05-29 12:04:54 -0400

    Added inline functions for PrimToCon and ConToPrim for all EOS derived classes.  These are located in two new include files (one for hydro and one for MHD).  Each EOS class now contains wrapper functions that extract a single Cons or Prim state and then calls inline functions.  This way they can be used elsewhere. Also allowed sharing of the SR and GR conversion functions (which are identical except for input states).

--- src/eos/ideal_srhyd.cpp
+++ src/eos/ideal_srhyd.cpp
@@ -9,10 +9,9 @@
 //! of Galeazzi et al., PhysRevD, 88, 064009 (2013). Equation refs are to this paper.
 
 #include "athena.hpp"
-#include "parameter_input.hpp"
-#include "mesh/mesh.hpp"
 #include "hydro/hydro.hpp"
 #include "eos.hpp"
+#include "eos/ideal_hyd.hpp"
 
 //----------------------------------------------------------------------------------------
 // ctor: also calls EOS base class constructor
@@ -24,44 +23,6 @@ IdealSRHydro::IdealSRHydro(MeshBlockPack *pp, ParameterInput *pin) :
   eos_data.iso_cs = 0.0;
   eos_data.use_e = true;  // ideal gas EOS always uses internal energy
   eos_data.use_t = false;
-}
-
-//--------------------------------------------------------------------------------------
-//! \fn void PrimToConsSingle()
-//! \brief Converts primitive into conserved variables in SR Hydro.
-//! Operates on only one active cell.
-
-KOKKOS_INLINE_FUNCTION
-void PrimToConsSingle(const Real &gammap, const HydPrim1D &w, HydCons1D &u) {
-    // Calculate Lorentz factor
-    Real u0 = sqrt(1.0 + SQR(w.vx) + SQR(w.vy) + SQR(w.vz));
-    Real wgas_u0 = (w.d + gammap * w.p) * u0;
-
-    // Set conserved quantities
-    u.d  = w.d * u0;
-    u.e  = wgas_u0 * u0 - w.p - u.d;  // In SR, evolve E - D
-    u.mx = wgas_u0 * w.vx;            // In SR, w_ux/y/z are 4-velocity
-    u.my = wgas_u0 * w.vy;
-    u.mz = wgas_u0 * w.vz;
-  return;
-}
-
-//----------------------------------------------------------------------------------------
-//! \fn Real EquationC22()
-//! \brief Inline function to compute function f(z) defined in eq. C22 of Galeazzi et al.
-//! The ConsToPrim algorithms finds the root of this function f(z)=0
-
-KOKKOS_INLINE_FUNCTION
-Real EquationC22(Real z, Real &u_d, Real q, Real r, Real gm1, Real pfloor) {
-  Real const w = sqrt(1.0 + z*z);         // (C15)
-  Real const wd = u_d/w;                  // (C15)
-  Real eps = w*q - z*r + (z*z)/(1.0 + w); // (C16)
-
-  //NOTE: The following generalizes to ANY equation of state
-  eps = fmax(pfloor/(wd*gm1), eps);                           // (C18)
-  Real const h = (1.0 + eps) * (1.0 + (gm1*eps)/(1.0+eps));   // (C1) & (C21)
-
-  return (z - r/h); // (C22)
 }
 
 //----------------------------------------------------------------------------------------
@@ -94,16 +55,7 @@ void IdealSRHydro::ConsToPrim(DvceArray5D<Real> &cons, DvceArray5D<Real> &prim,
   int &nhyd  = pmy_pack->phydro->nhydro;
   int &nscal = pmy_pack->phydro->nscalars;
   int &nmb = pmy_pack->nmb_thispack;
-  Real gm1 = eos_data.gamma - 1.0;
-  Real gamma_prime = eos_data.gamma/(gm1);
-
-  Real &pfloor_ = eos_data.pfloor;
-  Real &dfloor_ = eos_data.dfloor;
-
-  // Parameters
-  int const max_iterations = 25;
-  Real const tol = 1.0e-12;
-  Real const v_sq_max = 1.0 - tol;
+  auto eos = eos_data;
 
   const int ni   = (iu - il + 1);
   const int nji  = (ju - jl + 1)*ni;
@@ -111,8 +63,8 @@ void IdealSRHydro::ConsToPrim(DvceArray5D<Real> &cons, DvceArray5D<Real> &prim,
   const int nmkji = nmb*nkji;
 
   int nfloord_=0, nfloore_=0, maxit_=0;
-  Kokkos::parallel_reduce("hyd_c2p",Kokkos::RangePolicy<>(DevExeSpace(), 0, nmkji),
-  KOKKOS_LAMBDA(const int &idx, int &sum_d, int &sum_e, int &max_iter) {
+  Kokkos::parallel_reduce("srhyd_c2p",Kokkos::RangePolicy<>(DevExeSpace(), 0, nmkji),
+  KOKKOS_LAMBDA(const int &idx, int &sumd, int &sume, int &max_it) {
     int m = (idx)/nkji;
     int k = (idx - m*nkji)/nji;
     int j = (idx - m*nkji - k*nji)/ni;
@@ -120,127 +72,46 @@ void IdealSRHydro::ConsToPrim(DvceArray5D<Real> &cons, DvceArray5D<Real> &prim,
     j += jl;
     k += kl;
 
-    Real& u_d  = cons(m,IDN,k,j,i);
-    Real& u_e  = cons(m,IEN,k,j,i);
-    const Real& u_m1 = cons(m,IM1,k,j,i);
-    const Real& u_m2 = cons(m,IM2,k,j,i);
-    const Real& u_m3 = cons(m,IM3,k,j,i);
+    // load single state conserved variables
+    HydCons1D u;
+    u.d  = cons(m,IDN,k,j,i);
+    u.mx = cons(m,IM1,k,j,i);
+    u.my = cons(m,IM2,k,j,i);
+    u.mz = cons(m,IM3,k,j,i);
+    u.e  = cons(m,IEN,k,j,i);
 
-    Real& w_d  = prim(m,IDN,k,j,i);
-    Real& w_ux = prim(m,IVX,k,j,i);
-    Real& w_uy = prim(m,IVY,k,j,i);
-    Real& w_uz = prim(m,IVZ,k,j,i);
-    Real& w_e  = prim(m,IEN,k,j,i);
+    // Compute (S^i S_i) (eqn C2)
+    Real s2 = SQR(u.mx) + SQR(u.my) + SQR(u.mz);
 
-    // apply density floor, without changing momentum or energy
-    bool fixup_hit = false;
-    if (u_d < dfloor_) {
-      u_d = dfloor_;
-      sum_d++;
-      fixup_hit = true;
-    }
+    // call c2p function
+    HydPrim1D w;
+    bool dfloor_used, efloor_used;
+    int iter_used;
+    SingleC2P_IdealSRHyd(u, eos, s2, w, dfloor_used, efloor_used, iter_used);
+    if (dfloor_used) {sumd++;}
+    if (efloor_used) {sume++;}
+    max_it = fmax(max_it, iter_used);
 
-    // apply energy floor
-    // Real ee_min = pfloor_/gm1;
-    // u_e = (u_e > ee_min) ?  u_e : ee_min;
-
-    // Recast all variables (eq C2)
-    Real q = u_e/u_d;  // q is (E-D)/D, and we evolve u_e = E-D
-    Real r = sqrt(SQR(u_m1) + SQR(u_m2) + SQR(u_m3))/u_d;
-    Real kk = r/(1.+q);
-
-    // Enforce lower velocity bound (eq. C13). This bound combined with a floor on
-    // the value of p will guarantee "some" result of the inversion
-    kk = fmin(2.* sqrt(v_sq_max)/(1.0 + v_sq_max), kk);
-
-    // Compute bracket (C23)
-    auto zm = 0.5*kk/sqrt(1.0 - 0.25*kk*kk);
-    auto zp = kk/sqrt(1.0 - kk*kk);
-
-    // Evaluate master function (eq C22) at bracket values
-    Real fm = EquationC22(zm, u_d, q, r, gm1, pfloor_);
-    Real fp = EquationC22(zp, u_d, q, r, gm1, pfloor_);
-
-    // For simplicity on the GPU, find roots using the false position method
-    int iterations = max_iterations;
-    // If bracket within tolerances, don't bother doing any iterations
-    if ((fabs(zm-zp) < tol) || ((fabs(fm) + fabs(fp)) < 2.0*tol)) {
-      iterations = -1;
-    }
-    Real z = 0.5*(zm + zp);
-
-    {int iter;
-    for (iter=0; iter < iterations; ++iter) {
-      z =  (zm*fp - zp*fm)/(fp-fm);  // linear interpolation to point f(z)=0
-      Real f = EquationC22(z, u_d, q, r, gm1, pfloor_);
-
-      // Quit if convergence reached
-      // NOTE: both z and f are of order unity
-      if ((fabs(zm-zp) < tol ) || (fabs(f) < tol )) {
-        break;
-      }
-
-      // assign zm-->zp if root bracketed by [z,zp]
-      if (f * fp < 0.0) {
-        zm = zp;
-        fm = fp;
-        zp = z;
-        fp = f;
-      } else {  // assign zp-->z if root bracketed by [zm,z]
-        fm = 0.5*fm; // 1/2 comes from "Illinois algorithm" to accelerate convergence
-        zp = z;
-        fp = f;
-      }
-    }
-    max_iter = (iter > max_iter)? iter : max_iter;
-    }
-
-    // iterations ended, compute primitives from resulting value of z
-    Real const w = sqrt(1.0 + z*z); // (C15)
-    w_d = u_d/w;                    // (C15)
-
-    // NOTE(@ermost): The following generalizes to ANY equation of state
-    Real eps = w*q - z*r + (z*z)/(1.0 + w);           // (C16)
-    Real epsmin = pfloor_/(w_d*gm1);
-    if (eps <= epsmin) {                               // C18
-      eps = epsmin;
-      sum_e++;
-      fixup_hit = true;
-    }
-    Real h = (1. + eps) * (1.0 + (gm1*eps)/(1.+eps)); // (C1) & (C21)
-    w_e = w_d*eps;
-
-    Real const conv = 1.0/(h*u_d); // (C26)
-    w_ux = conv * u_m1;            // (C26)
-    w_uy = conv * u_m2;            // (C26)
-    w_uz = conv * u_m3;            // (C26)
-
-    // convert scalars (if any)
-    for (int n=nhyd; n<(nhyd+nscal); ++n) {
-      prim(m,n,k,j,i) = cons(m,n,k,j,i)/u_d;
-    }
+    // store primitive state in 3D array
+    prim(m,IDN,k,j,i) = w.d;
+    prim(m,IVX,k,j,i) = w.vx;
+    prim(m,IVY,k,j,i) = w.vy;
+    prim(m,IVZ,k,j,i) = w.vz;
+    prim(m,IEN,k,j,i) = w.e;
 
     // reset conserved variables if floor is hit
-    if (fixup_hit) {
-      HydPrim1D w;
-      w.d  = w_d;
-      w.vx = w_ux;
-      w.vy = w_uy;
-      w.vz = w_uz;
-      w.p = w_e*gm1;
-
-      HydCons1D u;
-      PrimToConsSingle(gamma_prime, w, u);
-
+    if (dfloor_used || efloor_used) {
+      SingleP2C_IdealSRHyd(w, eos.gamma, u);
       cons(m,IDN,k,j,i) = u.d;
-      cons(m,IEN,k,j,i) = u.e;
       cons(m,IM1,k,j,i) = u.mx;
       cons(m,IM2,k,j,i) = u.my;
       cons(m,IM3,k,j,i) = u.mz;
-      // convert scalars (if any)
-      for (int n=nhyd; n<(nhyd+nscal); ++n) {
-        cons(m,n,k,j,i) = prim(m,n,k,j,i)*cons(m,IDN,k,j,i);
-      }
+      cons(m,IEN,k,j,i) = u.e;
+    }
+
+    // convert scalars (if any)
+    for (int n=nhyd; n<(nhyd+nscal); ++n) {
+      prim(m,n,k,j,i) = cons(m,n,k,j,i)/u.d;
     }
   }, Kokkos::Sum<int>(nfloord_), Kokkos::Sum<int>(nfloore_), Kokkos::Max<int>(maxit_));
 
@@ -265,38 +136,32 @@ void IdealSRHydro::PrimToCons(const DvceArray5D<Real> &prim, DvceArray5D<Real> &
   int &nhyd  = pmy_pack->phydro->nhydro;
   int &nscal = pmy_pack->phydro->nscalars;
   int &nmb = pmy_pack->nmb_thispack;
-  Real gm1 = eos_data.gamma - 1.0;
-  Real gamma_prime = eos_data.gamma/(eos_data.gamma - 1.0);
+  Real &gamma = eos_data.gamma;
 
-  par_for("srhyd_prim2cons", DevExeSpace(), 0, (nmb-1), kl, ku, jl, ju, il, iu,
+  par_for("srhyd_p2c", DevExeSpace(), 0, (nmb-1), kl, ku, jl, ju, il, iu,
   KOKKOS_LAMBDA(int m, int k, int j, int i) {
-    Real& u_d  = cons(m, IDN,k,j,i);
-    Real& u_e  = cons(m, IEN,k,j,i);
-    Real& u_m1 = cons(m, IM1,k,j,i);
-    Real& u_m2 = cons(m, IM2,k,j,i);
-    Real& u_m3 = cons(m, IM3,k,j,i);
-
     // Load single state of primitive variables
     HydPrim1D w;
     w.d  = prim(m,IDN,k,j,i);
     w.vx = prim(m,IVX,k,j,i);
     w.vy = prim(m,IVY,k,j,i);
     w.vz = prim(m,IVZ,k,j,i);
-    w.p = prim(m,IEN,k,j,i)*gm1;
+    w.e  = prim(m,IEN,k,j,i);
 
+    // call p2c function
     HydCons1D u;
-    PrimToConsSingle(gamma_prime, w, u);
+    SingleP2C_IdealSRHyd(w, gamma, u);
 
-    // Set conserved quantities
+    // store conserved state in 3D array
     cons(m,IDN,k,j,i) = u.d;
-    cons(m,IEN,k,j,i) = u.e;
     cons(m,IM1,k,j,i) = u.mx;
     cons(m,IM2,k,j,i) = u.my;
     cons(m,IM3,k,j,i) = u.mz;
+    cons(m,IEN,k,j,i) = u.e;
 
     // convert scalars (if any)
     for (int n=nhyd; n<(nhyd+nscal); ++n) {
-      cons(m,n,k,j,i) = prim(m,n,k,j,i)*u_d;
+      cons(m,n,k,j,i) = u.d*prim(m,n,k,j,i);
     }
   });
 
