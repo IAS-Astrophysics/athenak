73e0351ef748a685c162ce04cdf4735d27b5eca5
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2023-01-10 19:22:26 -0500

    Adding dtnew.

--- src/radiation_femn/radiation_femn_newdt.cpp
+++ src/radiation_femn/radiation_femn_newdt.cpp
@@ -30,6 +30,30 @@ namespace radiationfemn {
 //        Only computed once at beginning of calculation.
 
     TaskStatus RadiationFEMN::NewTimeStep(Driver *pdriver, int stage) {
+        auto &indcs = pmy_pack->pmesh->mb_indcs;
+        int &is = indcs.is, &nx1 = indcs.nx1;
+        int &js = indcs.js, &nx2 = indcs.nx2;
+        int &ks = indcs.ks, &nx3 = indcs.nx3;
+
+        Real dt1 = std::numeric_limits<float>::max();
+        Real dt2 = std::numeric_limits<float>::max();
+        Real dt3 = std::numeric_limits<float>::max();
+
+        // capture class variables for kernel
+        auto &mbsize = pmy_pack->pmb->mb_size;
+        int nmb1 = pmy_pack->nmb_thispack;
+
+        Kokkos::parallel_reduce("RadFEMNDt", Kokkos::RangePolicy<>(DevExeSpace(), 0, nmb1),
+                                KOKKOS_LAMBDA(const int &m, Real &min_dt1, Real &min_dt2, Real &min_dt3) {
+                                    min_dt1 = std::min(mbsize.d_view(m).dx1, min_dt1);
+                                    min_dt2 = std::min(mbsize.d_view(m).dx2, min_dt2);
+                                    min_dt3 = std::min(mbsize.d_view(m).dx3, min_dt3);
+                                }, Kokkos::Min<Real>(dt1), Kokkos::Min<Real>(dt2), Kokkos::Min<Real>(dt3));
+
+        // compute minimum of dt1/dt2/dt3 for 1D/2D/3D problems
+        dtnew = dt1;
+        if (pmy_pack->pmesh->multi_d) { dtnew = std::min(dtnew, dt2); }
+        if (pmy_pack->pmesh->three_d) { dtnew = std::min(dtnew, dt3); }
 
         return TaskStatus::complete;
     }
