993ac4fb35714f5651dae31a0b40d574b02d148d
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2023-01-13 14:35:30 -0500

    Adding linetest.
    rebasing femn in progress [II]

--- src/pgen/pgen.cpp
+++ src/pgen/pgen.cpp
@@ -75,6 +75,8 @@ ProblemGenerator::ProblemGenerator(ParameterInput *pin, Mesh *pm) :
     ShockTube(pin, false);
   } else if (pgen_fun_name.compare("z4c_linear_wave") == 0) {
     Z4cLinearWave(pin, false);
+  } else if (pgen_fun_name.compare("rad_femn_linetest") == 0) {
+    RadiationFEMNLinetest(pin, false);
   // else, name not set on command line or input file, print warning and quit
   } else {
     std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__ << std::endl
