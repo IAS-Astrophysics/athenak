6a9fb54a1c52f2409f46ae195c9896c59e7e9c24
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2023-02-09 20:20:49 -0500

    Working mass matrix and output to file.

--- src/pgen/tests/rad_femn_gridtest.cpp
+++ src/pgen/tests/rad_femn_gridtest.cpp
@@ -9,6 +9,7 @@
 
 // C++ headers
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <cmath>        // exp
 #include <algorithm>    // max
@@ -16,7 +17,6 @@
 // AthenaK headers
 #include "athena.hpp"
 #include "parameter_input.hpp"
-#include "coordinates/cell_locations.hpp"
 #include "mesh/mesh.hpp"
 #include "pgen/pgen.hpp"
 #include "radiation_femn/radiation_femn.hpp"
@@ -33,93 +33,53 @@ void ProblemGenerator::RadiationFEMNGridtest(ParameterInput *pin, const bool res
         exit(EXIT_FAILURE);
     }
 
-    std::cout << "Geodesic grid metadata: " << std::endl;
-    std::cout << "nangles = " << pmbp->pradfemn->nangles << std::endl;
-    std::cout << "num_points = " << pmbp->pradfemn->num_points << std::endl;
-    std::cout << "num_edges = " << pmbp->pradfemn->num_edges << std::endl;
-    std::cout << "num_triangles = " << pmbp->pradfemn->num_triangles << std::endl;
+    std::string pathdir = pin->GetString("radiation-femn", "geogrid_savedir");
+    std::string filenamepart = "/geogrid_basis_" + std::to_string(pmbp->pradfemn->basis) + "_ref_" +
+                               std::to_string(pmbp->pradfemn->num_ref);
 
-    std::cout << std::endl;
-    std::cout << "-------------------------------------------------------------" << std::endl;
-    std::cout << std::endl;
+    std::ofstream fout(pathdir + filenamepart + "_metadata" + ".txt");
+    fout << "Geodesic grid metadata: " << std::endl;
+    fout << "basis = " << pmbp->pradfemn->basis << std::endl;
+    fout << "num_ref = " << pmbp->pradfemn->num_ref << std::endl;
+    fout << "nangles = " << pmbp->pradfemn->nangles << std::endl;
+    fout << "num_points = " << pmbp->pradfemn->num_points << std::endl;
+    fout << "num_edges = " << pmbp->pradfemn->num_edges << std::endl;
+    fout << "num_triangles = " << pmbp->pradfemn->num_triangles << std::endl;
 
-    std::cout << "Coordinates of angles in cartesian and polar: " << std::endl;
-    std::cout << "x y z r theta phi" << std::endl;
+    std::ofstream fout2(pathdir + filenamepart + "_coords_xyzrthph" + ".txt");
     for (size_t i = 0; i < pmbp->pradfemn->num_points; i++) {
-        std::cout << pmbp->pradfemn->x(i) << " " << pmbp->pradfemn->y(i) << " " << pmbp->pradfemn->z(i) << " "
+        fout2 << pmbp->pradfemn->x(i) << " " << pmbp->pradfemn->y(i) << " " << pmbp->pradfemn->z(i) << " "
                   << pmbp->pradfemn->r(i) << " " << pmbp->pradfemn->theta(i) << " " << pmbp->pradfemn->phi(i)
                   << std::endl;
     }
 
-    std::cout << std::endl;
-    std::cout << "-------------------------------------------------------------" << std::endl;
-    std::cout << std::endl;
-
-    std::cout << "Triangle information:" << std::endl;
+    std::ofstream fout3(pathdir + filenamepart + "_triangles" + ".txt");
     for (size_t i = 0; i < pmbp->pradfemn->num_triangles; i++) {
-        std::cout << pmbp->pradfemn->triangles(i, 0) << " " << pmbp->pradfemn->triangles(i, 1) << " "
+        fout3 << pmbp->pradfemn->triangles(i, 0) << " " << pmbp->pradfemn->triangles(i, 1) << " "
                   << pmbp->pradfemn->triangles(i, 2) << std::endl;
     }
 
-    std::cout << std::endl;
-    std::cout << "-------------------------------------------------------------" << std::endl;
-    std::cout << std::endl;
-
-    std::cout << "Edge information:" << std::endl;
+    std::ofstream fout4(pathdir + filenamepart + "_edges" + ".txt");
     for (size_t i = 0; i < pmbp->pradfemn->num_edges; i++) {
-        std::cout << pmbp->pradfemn->edges(i, 0) << " " << pmbp->pradfemn->edges(i, 1) << std::endl;
+        fout4 << pmbp->pradfemn->edges(i, 0) << " " << pmbp->pradfemn->edges(i, 1) << std::endl;
     }
 
-    std::cout << std::endl;
-    std::cout << "-------------------------------------------------------------" << std::endl;
-    std::cout << std::endl;
-
-    /*
-    for (size_t j = 0; j < 12; j++) {
-        bool tempval;
-        pmbp->pradfemn->FindTriangles(j,j,tempval);
-        auto etriangles = pmbp->pradfemn->edge_triangles;
-        std::cout << "Finding triangle information for vertex: " << j << std::endl;
-        for (size_t i = 0; i < 6; i++) {
-            std::cout << "[ " << etriangles(i,0) << ", " << etriangles(i,1) << ", " << etriangles(i,2) << " ]" << std::endl;
-        }
-
-        std::cout << std::endl;
-    }
-
-    bool tempval;
-    pmbp->pradfemn->FindTriangles(3,5,tempval);
-    auto etriangles = pmbp->pradfemn->edge_triangles;
-    std::cout << "Finding triangle information for vertex: (3, 5)" << std::endl;
-    for (size_t i = 0; i <6; i++) {
-        std::cout << "[ " << etriangles(i,0) << ", " << etriangles(i,1) << ", " << etriangles(i,2) << " ]" << std::endl;
-    }
-
-    std::cout << std::endl;
-
-    bool tempval2;
-    pmbp->pradfemn->FindTriangles(5,11,tempval2);
-    auto etriangles2 = pmbp->pradfemn->edge_triangles;
-    std::cout << "Finding triangle information for vertex: (5, 11)" << std::endl;
-    for (size_t i = 0; i <6; i++) {
-        std::cout << "[ " << etriangles2(i,0) << ", " << etriangles2(i,1) << ", " << etriangles2(i,2) << " ]" << std::endl;
-    }
-    */
-    std::cout << std::endl;
-    std::cout << "Print the mass matrix: " << std::endl;
+    std::ofstream fout5(pathdir + filenamepart + "_massmatrix" + ".txt");
     auto mass_matrix = pmbp->pradfemn->mass_matrix;
-
     double mass_matrix_sum{0.};
     for (size_t i = 0; i < pmbp->pradfemn->num_points; i++) {
         for (size_t j = 0; j < pmbp->pradfemn->num_points; j++) {
-            std::cout << mass_matrix(i,j) << " ";
-            mass_matrix_sum += mass_matrix(i,j);
+            fout5 << mass_matrix(i, j) << " ";
+            mass_matrix_sum += mass_matrix(i, j);
         }
-        std::cout << std::endl;
+        fout5 << std::endl;
     }
-
     std::cout << std::endl;
-
+    std::cout << "-------------------------" << std::endl;
+    std::cout << "Geodesic grid information" << std::endl;
+    std::cout << "-------------------------" << std::endl;
+    std::cout << "Choice of basis: " << pmbp->pradfemn->basis << std::endl;
+    std::cout << "Refinement level: " << pmbp->pradfemn->num_ref << std::endl;
     std::cout << "The sum of all the elements of the mass matrix is: " << mass_matrix_sum << std::endl;
     std::cout << std::endl;
 
