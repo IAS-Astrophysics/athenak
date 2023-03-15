998fc5bf2148ab49c4e28123e1afe9cf0d52453c
gonihalevi <ghalevi@princeton.edu>
2023-03-15 07:38:22 -0400

    made consistent with master

--- src/outputs/history.cpp
+++ src/outputs/history.cpp
@@ -162,26 +162,41 @@ void HistoryOutput::LoadHydroHistoryData(HistoryData *pdata, Mesh *pm) {
 void HistoryOutput::LoadMHDHistoryData(HistoryData *pdata, Mesh *pm) {
   auto &eos_data = pm->pmb_pack->pmhd->peos->eos_data;
   int &nmhd_ = pm->pmb_pack->pmhd->nmhd;
+  bool gr = pm->pmb_pack->pcoord->is_general_relativistic;
 
   // set number of and names of history variables for mhd
-  if (eos_data.is_ideal) {
-    pdata->nhist = 11;
-  } else {
-    pdata->nhist = 10;
+  if (gr) {
+    pdata->nhist = 8;
+    pdata->label[IDN] = "mass";
+    pdata->label[1] = "T^0_0 + D";
+    pdata->label[2] = "T^0_1";
+    pdata->label[3] = "T^0_2";
+    pdata->label[4] = "T^0_3";
+    pdata->label[5] = "1-ME";
+    pdata->label[6] = "2-ME";
+    pdata->label[7] = "3-ME";
   }
-  pdata->label[IDN] = "mass";
-  pdata->label[IM1] = "1-mom";
-  pdata->label[IM2] = "2-mom";
-  pdata->label[IM3] = "3-mom";
-  if (eos_data.is_ideal) {
-    pdata->label[IEN] = "tot-E";
+  else {
+    if (eos_data.is_ideal) {
+      pdata->nhist = 11;
+    }
+    else {
+      pdata->nhist = 10;
+    }
+    pdata->label[IDN] = "mass";
+    pdata->label[IM1] = "1-mom";
+    pdata->label[IM2] = "2-mom";
+    pdata->label[IM3] = "3-mom";
+    if (eos_data.is_ideal) {
+      pdata->label[IEN] = "tot-E";
+    }
+    pdata->label[nmhd_  ] = "1-KE";
+    pdata->label[nmhd_+1] = "2-KE";
+    pdata->label[nmhd_+2] = "3-KE";
+    pdata->label[nmhd_+3] = "1-ME";
+    pdata->label[nmhd_+4] = "2-ME";
+    pdata->label[nmhd_+5] = "3-ME";
   }
-  pdata->label[nmhd_  ] = "1-KE";
-  pdata->label[nmhd_+1] = "2-KE";
-  pdata->label[nmhd_+2] = "3-KE";
-  pdata->label[nmhd_+3] = "1-ME";
-  pdata->label[nmhd_+4] = "2-ME";
-  pdata->label[nmhd_+5] = "3-ME";
 
   // capture class variabels for kernel
   auto &u0_ = pm->pmb_pack->pmhd->u0;
@@ -190,6 +205,12 @@ void HistoryOutput::LoadMHDHistoryData(HistoryData *pdata, Mesh *pm) {
   auto &bx3f = pm->pmb_pack->pmhd->b0.x3f;
   auto &size = pm->pmb_pack->pmb->mb_size;
   int &nhist_ = pdata->nhist;
+  // need coordinate parameters for GR
+  bool &flat = pm->pmb_pack->pcoord->coord_data.is_minkowski;
+  Real &spin = pm->pmb_pack->pcoord->coord_data.bh_spin;
+  // include primitive variables and cell-centered B-fields for GR
+  auto &w0_ = pm->pmb_pack->pmhd->w0;
+  auto &bcc_ = pm->pmb_pack->pmhd->bcc0;
 
   // loop over all MeshBlocks in this pack
   auto &indcs = pm->pmb_pack->pmesh->mb_indcs;
@@ -211,26 +232,99 @@ void HistoryOutput::LoadMHDHistoryData(HistoryData *pdata, Mesh *pm) {
     j += js;
 
     Real vol = size.d_view(m).dx1*size.d_view(m).dx2*size.d_view(m).dx3;
+    Real b1, b_1, b2, b_2, b3, b_3;
+    b1 = b_1 = bcc_(m,IBX,k,j,i);
+    b2 = b_2 = bcc_(m,IBY,k,j,i);
+    b3 = b_3 = bcc_(m,IBZ,k,j,i);
+
+    if (gr) {
+      Real &x1min = size.d_view(m).x1min;
+      Real &x1max = size.d_view(m).x1max;
+      Real x1v = CellCenterX(i-is, indcs.nx1, x1min, x1max);
+
+      Real &x2min = size.d_view(m).x2min;
+      Real &x2max = size.d_view(m).x2max;
+      Real x2v = CellCenterX(j-js, indcs.nx2, x2min, x2max);
+
+      Real &x3min = size.d_view(m).x3min;
+      Real &x3max = size.d_view(m).x3max;
+      Real x3v = CellCenterX(k-ks, indcs.nx3, x3min, x3max);
+      Real glower[4][4], gupper[4][4];
+      ComputeMetricAndInverse(x1v, x2v, x3v, flat, spin, glower, gupper);
+      // Extract primitive velocity, magnetic field B^i, and gas pressure
+      Real &widn = w0_(m,IDN,k,j,i);
+      Real &wvx = w0_(m,IVX,k,j,i);
+      Real &wvy = w0_(m,IVY,k,j,i);
+      Real &wvz = w0_(m,IVZ,k,j,i);
+      Real &wbx = bcc_(m,IBX,k,j,i);
+      Real &wby = bcc_(m,IBY,k,j,i);
+      Real &wbz = bcc_(m,IBZ,k,j,i);
+
+      // Calculate 4-velocity (exploiting symmetry of metric)
+      Real q = glower[1][1]*wvx*wvx +2.0*glower[1][2]*wvx*wvy +2.0*glower[1][3]*wvx*wvz
+             + glower[2][2]*wvy*wvy +2.0*glower[2][3]*wvy*wvz
+             + glower[3][3]*wvz*wvz;
+      Real alpha = sqrt(-1.0/gupper[0][0]);
+      Real lor = sqrt(1.0 + q);
+      Real u0 = lor / alpha;
+      Real u1 = wvx - alpha * lor * gupper[0][1];
+      Real u2 = wvy - alpha * lor * gupper[0][2];
+      Real u3 = wvz - alpha * lor * gupper[0][3];
+
+      // lower vector indices
+      Real u_0 = glower[0][0]*u0 + glower[0][1]*u1 + glower[0][2]*u2 + glower[0][3]*u3;
+      Real u_1 = glower[1][0]*u0 + glower[1][1]*u1 + glower[1][2]*u2 + glower[1][3]*u3;
+      Real u_2 = glower[2][0]*u0 + glower[2][1]*u1 + glower[2][2]*u2 + glower[2][3]*u3;
+      Real u_3 = glower[3][0]*u0 + glower[3][1]*u1 + glower[3][2]*u2 + glower[3][3]*u3;
+
+      // Calculate 4-magnetic field
+      Real b0 = u_1*wbx + u_2*wby + u_3*wbz;
+      Real b1 = (wbx + b0 * u1) / u0;
+      Real b2 = (wby + b0 * u2) / u0;
+      Real b3 = (wbz + b0 * u3) / u0;
+
+      // lower vector indices 
+      Real b_0 = glower[0][0]*b0 + glower[0][1]*b1 + glower[0][2]*b2 + glower[0][3]*b3;
+      Real b_1 = glower[1][0]*b0 + glower[1][1]*b1 + glower[1][2]*b2 + glower[1][3]*b3;
+      Real b_2 = glower[2][0]*b0 + glower[2][1]*b1 + glower[2][2]*b2 + glower[2][3]*b3;
+      Real b_3 = glower[3][0]*b0 + glower[3][1]*b1 + glower[3][2]*b2 + glower[3][3]*b3;
+    }
 
     // MHD conserved variables:
     array_sum::GlobalSum hvars;
     hvars.the_array[IDN] = vol*u0_(m,IDN,k,j,i);
-    hvars.the_array[IM1] = vol*u0_(m,IM1,k,j,i);
-    hvars.the_array[IM2] = vol*u0_(m,IM2,k,j,i);
-    hvars.the_array[IM3] = vol*u0_(m,IM3,k,j,i);
-    if (eos_data.is_ideal) {
-      hvars.the_array[IEN] = vol*u0_(m,IEN,k,j,i);
+    
+    if (gr) {
+      // T^0_0 + D
+      hvars.the_array[1] = vol*u0_(m,IEN,k,j,i);
+      // T^0_1,2,3
+      hvars.the_array[2] = vol*u0_(m,IM1,k,j,i);
+      hvars.the_array[3] = vol*u0_(m,IM2,k,j,i);
+      hvars.the_array[4] = vol*u0_(m,IM3,k,j,i);
+
+      // GRMHD averaged (b^2) components
+      hvars.the_array[5] = vol*(b1*b_1);
+      hvars.the_array[6] = vol*(b2*b_2);
+      hvars.the_array[7] = vol*(b3*b_3);
     }
+    else {
+      hvars.the_array[IM1] = vol*u0_(m,IM1,k,j,i);
+      hvars.the_array[IM2] = vol*u0_(m,IM2,k,j,i);
+      hvars.the_array[IM3] = vol*u0_(m,IM3,k,j,i);
+      if (eos_data.is_ideal) {
+        hvars.the_array[IEN] = vol*u0_(m,IEN,k,j,i);
+      }
 
-    // MHD KE
-    hvars.the_array[nmhd_  ] = vol*0.5*SQR(u0_(m,IM1,k,j,i))/u0_(m,IDN,k,j,i);
-    hvars.the_array[nmhd_+1] = vol*0.5*SQR(u0_(m,IM2,k,j,i))/u0_(m,IDN,k,j,i);
-    hvars.the_array[nmhd_+2] = vol*0.5*SQR(u0_(m,IM3,k,j,i))/u0_(m,IDN,k,j,i);
+      // MHD KE
+      hvars.the_array[nmhd_  ] = vol*0.5*SQR(u0_(m,IM1,k,j,i))/u0_(m,IDN,k,j,i);
+      hvars.the_array[nmhd_+1] = vol*0.5*SQR(u0_(m,IM2,k,j,i))/u0_(m,IDN,k,j,i);
+      hvars.the_array[nmhd_+2] = vol*0.5*SQR(u0_(m,IM3,k,j,i))/u0_(m,IDN,k,j,i);
 
-    // MHD ME
-    hvars.the_array[nmhd_+3] = vol*0.25*(SQR(bx1f(m,k,j,i+1)) + SQR(bx1f(m,k,j,i)));
-    hvars.the_array[nmhd_+4] = vol*0.25*(SQR(bx2f(m,k,j+1,i)) + SQR(bx2f(m,k,j,i)));
-    hvars.the_array[nmhd_+5] = vol*0.25*(SQR(bx3f(m,k+1,j,i)) + SQR(bx3f(m,k,j,i)));
+      // MHD ME
+      hvars.the_array[nmhd_+3] = vol*0.25*(SQR(bx1f(m,k,j,i+1)) + SQR(bx1f(m,k,j,i)));
+      hvars.the_array[nmhd_+4] = vol*0.25*(SQR(bx2f(m,k,j+1,i)) + SQR(bx2f(m,k,j,i)));
+      hvars.the_array[nmhd_+5] = vol*0.25*(SQR(bx3f(m,k+1,j,i)) + SQR(bx3f(m,k,j,i)));
+    }
 
     // fill rest of the_array with zeros, if nhist < NHISTORY_VARIABLES
     for (int n=nhist_; n<NHISTORY_VARIABLES; ++n) {
