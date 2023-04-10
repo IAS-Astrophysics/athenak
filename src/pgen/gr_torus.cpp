507bcd1843c8795400e3ff9492cd8855bad2fb1a
Goni Halevi <ghalevi@della-gpu.princeton.edu>
2023-04-10 14:51:40 -0400

    almost fixed but need to rearrange

--- src/pgen/gr_torus.cpp
+++ src/pgen/gr_torus.cpp
@@ -1076,9 +1076,7 @@ static void CalculateVectorPotentialInTiltedTorus(struct torus_pgen pgen,
         }
         else {
           Real r_cutoff = fmin(r, pgen.potential_rmax);
-          if (pgas_cut > 0.0) {
-            atheta = pgen.potential_tor_frac * pow(r_cutoff, pgen.potential_r_pow_tor);
-          }
+          atheta = pgen.potential_tor_frac * pow(r_cutoff, pgen.potential_r_pow_tor);
         }
       }
       if (pgen.psi != 0.0) {
