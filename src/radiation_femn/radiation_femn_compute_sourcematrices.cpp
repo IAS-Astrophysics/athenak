5ca72f6e6f5cac37875c5f6bdc237300ba546147
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2023-09-28 13:56:15 -0400

    adding building blocks for source matrices

---
+++ src/radiation_femn/radiation_femn_compute_sourcematrices.cpp
@@ -0,0 +1,28 @@
+//========================================================================================
+// GR radiation code for AthenaK with FEM_N & FP_N
+// Copyright (C) 2023 Maitraya Bhattacharyya <mbb6217@psu.edu> and David Radice <dur566@psu.edu>
+// AthenaXX copyright(C) James M. Stone <jmstone@ias.edu> and the Athena code team
+// Licensed under the 3-clause BSD License (the "LICENSE")
+//========================================================================================
+//! \file  radiation_femn_compute_sourcematrices.cpp
+//  \brief compute matrices for the source terms
+
+#include <iostream>
+
+#include "radiation_femn/radiation_femn.hpp"
+#include "radiation_femn/radiation_femn_geodesic_grid_matrices.hpp"
+
+namespace radiationfemn {
+
+void RadiationFEMN::ComputeSourceMatrices() {
+    std::cout << "Computing source matrices ..." << std::endl;
+
+    auto mm_ = pmy_pack->pradfemn->mass_matrix;
+
+    HostArray2D<Real> mass_inv_temp;
+    Kokkos::realloc(mass_inv_temp, num_points, num_points);
+    radiationfemn::LUInverse(mm_, mass_inv_temp);
+
+}
+
+}
--- src/radiation_femn/radiation_femn_geodesic_grid_matrix_integrate.cpp
+++ src/radiation_femn/radiation_femn_geodesic_grid_matrix_integrate.cpp
@@ -92,8 +92,13 @@ Real IntegrateMatrixSphericalTriangle(int a, int b, int basis, int t1, int t2, i
       result += MomentumUnitEnergy(nu, x1, y1, z1, x2, y2, z2, x3, y3, z3, scheme_points(i, 0), scheme_points(i, 1), scheme_points(i, 2))
           * MomentumUnitEnergy(mu, x1, y1, z1, x2, y2, z2, x3, y3, z3, scheme_points(i, 0), scheme_points(i, 1), scheme_points(i, 2))
           * sqrt(CalculateDeterminantJacobian(x1, y1, z1, x2, y2, z2, x3, y3, z3, scheme_points(i, 0), scheme_points(i, 1), scheme_points(i, 2)))
-          * FEMBasisABasisB(a, b, t1, t2, t3, scheme_points(i, 0), scheme_points(i, 1), scheme_points(i, 2), basis) * scheme_weights(i) *
+          * FEMBasisABasisB(a, b, t1, t2, t3, scheme_points(i, 0), scheme_points(i, 1), scheme_points(i, 2), basis) *
           MomentumUnitEnergy(ihat, x1, y1, z1, x2, y2, z2, x3, y3, z3, scheme_points(i, 0), scheme_points(i, 1), scheme_points(i, 2)) * scheme_weights(i);
+    }
+  } else if (matrixnumber == 6) {
+    for (size_t i = 0; i < scheme_weights.size(); i++) {
+      result += sqrt(CalculateDeterminantJacobian(x1, y1, z1, x2, y2, z2, x3, y3, z3, scheme_points(i, 0), scheme_points(i, 1), scheme_points(i, 2)))
+          * FEMBasisA(a, t1, t2, t3, scheme_points(i, 0), scheme_points(i, 1), scheme_points(i, 2), basis) * scheme_weights(i);
     }
   }
 
@@ -236,6 +241,10 @@ Real IntegrateMatrixFPN(int la, int ma, int lb, int mb, const HostArray1D<Real> 
           * FPNBasis(la, ma, scheme_points(i, 0), scheme_points(i, 1))
           * FPNBasis(lb, mb, scheme_points(i, 0), scheme_points(i, 1))
           * MomentumUnitEnergy(ihat, scheme_points(i, 0), scheme_points(i, 1)) * scheme_weights(i);
+    }
+  } else if (matrixchoice == 6) {
+    for (size_t i = 0; i < scheme_weights.size(); i++) {
+      result += 4. * M_PI * FPNBasis(la, ma, scheme_points(i, 0), scheme_points(i, 1)) * scheme_weights(i);
     }
   }
   return result;
