7cc177c0f5e8520214af4aefada06202ade4758b
lzhang23 <lizhong4physics@gmail.com>
2023-12-09 20:23:57 -0500

    use NH for variable inversion

--- src/eos/ideal_grmhd.cpp
+++ src/eos/ideal_grmhd.cpp
@@ -146,16 +146,16 @@ void IdealGRMHD::ConsToPrim(DvceArray5D<Real> &cons, const DvceFaceFld4D<Real> &
 
       // call c2p function
       // (inline function in ideal_c2p_mhd.hpp file)
-      SingleC2P_IdealSRMHD(u_sr, eos, s2, b2, rpar, w,
-                           dfloor_used, efloor_used, c2p_failure, iter_used);
+      // SingleC2P_IdealSRMHD(u_sr, eos, s2, b2, rpar, w,
+      //                      dfloor_used, efloor_used, c2p_failure, iter_used);
       HydPrim1D w_old;
       w_old.d  = prim(m,IDN,k,j,i);
       w_old.vx = prim(m,IVX,k,j,i);
       w_old.vy = prim(m,IVY,k,j,i);
       w_old.vz = prim(m,IVZ,k,j,i);
       w_old.e  = prim(m,IEN,k,j,i);
-      // SingleC2P_IdealSRMHD_NH(u_sr, eos, s2, b2, rpar, w, w_old,
-      //                         dfloor_used, efloor_used, c2p_failure, iter_used);
+      SingleC2P_IdealSRMHD_NH(u_sr, eos, s2, b2, rpar, w, w_old,
+                              dfloor_used, efloor_used, c2p_failure, iter_used);
 
       // apply entropy fix
       if (entropy_fix_ && !entropy_fix_turnoff_) {
