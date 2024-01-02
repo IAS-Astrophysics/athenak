ce44f2cfb98d7ab764c9971ed1ac4fe2ddd230b1
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2024-01-02 17:32:57 -0500

    adding new implementation of beam source. wip.

--- src/radiation_femn/radiation_femn_beams.cpp
+++ src/radiation_femn/radiation_femn_beams.cpp
@@ -6,22 +6,42 @@
 //! \file radiation_femn_beams.cpp
 //  \brief set up beam sources
 
-#include <string>
-#include <cmath>
 #include "athena.hpp"
-#include "parameter_input.hpp"
 #include "mesh/mesh.hpp"
 #include "eos/eos.hpp"
-#include "bvals/bvals.hpp"
 #include "units/units.hpp"
 #include "radiation_femn/radiation_femn.hpp"
-#include "radiation_femn/radiation_femn_geodesic_grid_matrices.hpp"
 
 namespace radiationfemn {
 
 TaskStatus RadiationFEMN::BeamsSourcesFEMN(Driver *pdriver, int stage) {
 
-  std::cout << "Hello! I am beam sources!" << std::endl;
+  auto &indcs = pmy_pack->pmesh->mb_indcs;
+  int &is = indcs.is, &ie = indcs.ie;
+  int &js = indcs.js, &je = indcs.je;
+  int &ks = indcs.ks, &ke = indcs.ke;
+  int npts1 = num_points_total - 1;
+  int nmb1 = pmy_pack->nmb_thispack - 1;
+  auto &mb_bcs = pmy_pack->pmb->mb_bcs;
+
+  int &ng = indcs.ng;
+  int n1 = indcs.nx1 + 2*ng;
+  int n2 = (indcs.nx2 > 1)? (indcs.nx2 + 2*ng) : 1;
+  int n3 = (indcs.nx3 > 1)? (indcs.nx3 + 2*ng) : 1;
+  auto &f0_ = pmy_pack->pradfemn->f0;
+
+  par_for("radiation_femn_beams_populate", DevExeSpace(), 0, nmb1, 0, npts1, 0, (n3 - 1), 0, (n2 - 1),
+          KOKKOS_LAMBDA(int m, int n, int k, int j) {
+            switch (mb_bcs.d_view(m, BoundaryFace::inner_x1)) {
+              case BoundaryFlag::outflow:
+                for (int i = 0; i < ng; ++i) {
+                  f0_(m, n, k, j, is - i - 1) = 1.;
+                }
+                break;
+              default:break;
+            }
+          });
+
   return TaskStatus::complete;
 }
 
