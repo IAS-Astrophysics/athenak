a1b105f97e60621b4e64782f01d90235940aea0b
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2024-01-23 14:18:26 -0500

    replace copysign

--- src/radiation_femn/radiation_femn_fluxes.cpp
+++ src/radiation_femn/radiation_femn_fluxes.cpp
@@ -86,13 +86,13 @@ TaskStatus RadiationFEMN::CalculateFluxes(Driver *pdriver, int stage) {
                       Real L_mu_muhat0_L = 1.5 * L_mu_muhat0_(m, 1, muhat, kk, jj, ii) - 0.5 * L_mu_muhat0_(m, 1, muhat, kk, jj, ii + 1);
                       Fminus += (0.5) * Ven * (sqrt_det_g_L * L_mu_muhat0_L) * (P_matrix_(muhat, B, A)
                           * ((1.5) * f0_scratch(A) - (0.5) * f0_scratch_p1(A) + (1.5) * f0_scratch_m1(A) - (0.5) * f0_scratch_m2(A))
-                          - std::copysign(1.0, L_mu_muhat0_L) * Pmod_matrix_(muhat, B, A)
+                          - Sgn(L_mu_muhat0_L) * Pmod_matrix_(muhat, B, A)
                               * ((1.5) * f0_scratch_m1(A) - (0.5) * f0_scratch_m2(A) - (1.5) * f0_scratch(A) + (0.5) * f0_scratch_p1(A)));
 
                       Real L_mu_muhat0_R = -0.5 * L_mu_muhat0_(m, 1, muhat, kk, jj, ii) + 1.5 * L_mu_muhat0_(m, 1, muhat, kk, jj, ii + 1);
                       Fplus += (0.5) * Ven * (sqrt_det_g_R * L_mu_muhat0_R) * (P_matrix_(muhat, B, A) *
                           ((1.5) * f0_scratch_p2(A) - (0.5) * f0_scratch_p3(A) + (1.5) * f0_scratch_p1(A) - (0.5) * f0_scratch(A))
-                          - std::copysign(1.0, L_mu_muhat0_R) * Pmod_matrix_(muhat, B, A) *
+                          - Sgn(L_mu_muhat0_R) * Pmod_matrix_(muhat, B, A) *
                               ((1.5) * f0_scratch_p1(A) - (0.5) * f0_scratch(A) - (1.5) * f0_scratch_p2(A) + (0.5) * f0_scratch_p3(A)));
                     }
 
@@ -152,13 +152,13 @@ TaskStatus RadiationFEMN::CalculateFluxes(Driver *pdriver, int stage) {
                         Real L_mu_muhat0_L = 1.5 * L_mu_muhat0_(m, 2, muhat, kk, jj, ii) - 0.5 * L_mu_muhat0_(m, 2, muhat, kk, jj + 1, ii);
                         Fminus += (0.5) * Ven * (sqrt_det_g_L * L_mu_muhat0_L) * (P_matrix_(muhat, B, A)
                             * ((1.5) * f0_scratch(A) - (0.5) * f0_scratch_p1(A) + (1.5) * f0_scratch_m1(A) - (0.5) * f0_scratch_m2(A))
-                            - std::copysign(1.0, L_mu_muhat0_L) * Pmod_matrix_(muhat, B, A)
+                            - Sgn(L_mu_muhat0_L) * Pmod_matrix_(muhat, B, A)
                                 * ((1.5) * f0_scratch_m1(A) - (0.5) * f0_scratch_m2(A) - (1.5) * f0_scratch(A) + (0.5) * f0_scratch_p1(A)));
 
                         Real L_mu_muhat0_R = -0.5 * L_mu_muhat0_(m, 2, muhat, kk, jj, ii) + 1.5 * L_mu_muhat0_(m, 2, muhat, kk, jj + 1, ii);
                         Fplus += (0.5) * Ven * (sqrt_det_g_R * L_mu_muhat0_R) * (P_matrix_(muhat, B, A) *
                             ((1.5) * f0_scratch_p2(A) - (0.5) * f0_scratch_p3(A) + (1.5) * f0_scratch_p1(A) - (0.5) * f0_scratch(A))
-                            - std::copysign(1.0, L_mu_muhat0_R) * Pmod_matrix_(muhat, B, A) *
+                            - Sgn(L_mu_muhat0_R) * Pmod_matrix_(muhat, B, A) *
                                 ((1.5) * f0_scratch_p1(A) - (0.5) * f0_scratch(A) - (1.5) * f0_scratch_p2(A) + (0.5) * f0_scratch_p3(A)));
                       }
 
@@ -222,14 +222,14 @@ TaskStatus RadiationFEMN::CalculateFluxes(Driver *pdriver, int stage) {
 
                         Fminus += (0.5) * Ven * (sqrt_det_g_L * L_mu_muhat0_L) * (P_matrix_(muhat, B, A)
                             * ((1.5) * f0_scratch(A) - (0.5) * f0_scratch_p1(A) + (1.5) * f0_scratch_m1(A) - (0.5) * f0_scratch_m2(A))
-                            - std::copysign(1.0, L_mu_muhat0_L) * Pmod_matrix_(muhat, B, A)
+                            - Sgn(L_mu_muhat0_L) * Pmod_matrix_(muhat, B, A)
                                 * ((1.5) * f0_scratch_m1(A) - (0.5) * f0_scratch_m2(A) - (1.5) * f0_scratch(A) + (0.5) * f0_scratch_p1(A)));
 
                         Real L_mu_muhat0_R = -0.5 * L_mu_muhat0_(m, 3, muhat, kk, jj, ii) + 1.5 * L_mu_muhat0_(m, 3, muhat, kk + 1, jj, ii);
 
                         Fplus += (0.5) * Ven * (sqrt_det_g_R * L_mu_muhat0_R) * (P_matrix_(muhat, B, A) *
                             ((1.5) * f0_scratch_p2(A) - (0.5) * f0_scratch_p3(A) + (1.5) * f0_scratch_p1(A) - (0.5) * f0_scratch(A))
-                            - std::copysign(1.0, L_mu_muhat0_R) * Pmod_matrix_(muhat, B, A) *
+                            - Sgn(L_mu_muhat0_R) * Pmod_matrix_(muhat, B, A) *
                                 ((1.5) * f0_scratch_p1(A) - (0.5) * f0_scratch(A) - (1.5) * f0_scratch_p2(A) + (0.5) * f0_scratch_p3(A)));
                       }
 
