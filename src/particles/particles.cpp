8a181d2f99bf510c97d23fcb400980d980c148d4
James Stone <jmstone@ias.edu>
2024-02-29 13:14:53 -0800

    Fixrd bug in particles when run without MPI.

--- src/particles/particles.cpp
+++ src/particles/particles.cpp
@@ -39,8 +39,6 @@ Particles::Particles(MeshBlockPack *ppack, ParameterInput *pin) :
   Real r_npart = ppc*static_cast<Real>((pmy_pack->nmb_thispack)*ncells);
   // then cast to integer
   nprtcl_thispack = static_cast<int>(r_npart);
-  pmy_pack->pmesh->nprtcl_thisrank += nprtcl_thispack;
-  pmy_pack->pmesh->nprtcl_total += nprtcl_thispack;
 
   // select particle type
   {
