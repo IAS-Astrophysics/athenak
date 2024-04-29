619bb95811897cff5bffa445e2d4576dc5b9f0b9
Alireza <rashti.alireza@gmail.com>
2024-04-29 12:25:10 -0400

    Add header.

--- src/z4c/cce/cce.cpp
+++ src/z4c/cce/cce.cpp
@@ -1,3 +1,9 @@
+//========================================================================================
+// AthenaXXX astrophysical plasma code
+// Copyright(C) 2020 James M. Stone <jmstone@ias.edu> and the Athena code team
+// Licensed under the 3-clause BSD License (the "LICENSE")
+//========================================================================================
+
 #include "athena.hpp"
 #include "globals.hpp"
 
