318e03e7be55f3ab939995e293d7568abd36f731
Goni Halevi <goni@northwestern.edu>
2024-05-01 11:54:33 -0500

    Revert "tried to cutoff toroidal vector potential"
    This reverts commit d60e3a48de9c71382a2c56e9feb86ae75e89ade1.

--- src/pgen/gr_torus_mixed_A.cpp
+++ src/pgen/gr_torus_mixed_A.cpp
@@ -638,7 +638,7 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
     par_for("pgen_bcc", DevExeSpace(), 0,nmb-1,ks,ke,js,je,is,ie,
     KOKKOS_LAMBDA(int m, int k, int j, int i) {
       // cell-centered fields are simple linear average of face-centered fields
-      Real& w_bx = bcc_(m,IBX,k,j,i); 
+      Real& w_bx = bcc_(m,IBX,k,j,i);
       Real& w_by = bcc_(m,IBY,k,j,i);
       Real& w_bz = bcc_(m,IBZ,k,j,i);
       w_bx = 0.5*(b0.x1f(m,k,j,i) + b0.x1f(m,k,j,i+1));
@@ -774,7 +774,7 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
       if (i==ie) { b0.x1f(m,k,j,i+1) *= bnorm; }
       if (j==je) { b0.x2f(m,k,j+1,i) *= bnorm; }
       if (k==ke) { b0.x3f(m,k+1,j,i) *= bnorm; }
-    });   
+    });
 
     // Recompute cell-centered magnetic field
     par_for("pgen_normbcc", DevExeSpace(), 0,nmb-1,ks,ke,js,je,is,ie,
@@ -1303,10 +1303,9 @@ static void CalculateVectorPotentialInTiltedTorus(struct torus_pgen pgen,
         aphi_tilt -= pgen.potential_cutoff;
         aphi_tilt = fmax(aphi_tilt, 0.0);
         aphi_tilt *= pgen.potential_pol_frac;
-        
-        Real rho_cut = fmax(rho - pgen.potential_cutoff, 0.0);
-        atheta_tilt = pow(rho_cut/pgen.rho_max, pgen.potential_rho_pow_tor)*scaling_param_tor;
-        // atheta_tilt -= pgen.potential_cutoff;
+
+        atheta_tilt = pow(rho/pgen.rho_max, pgen.potential_rho_pow_tor)*scaling_param_tor;
+        atheta_tilt -= pgen.potential_cutoff;
         atheta_tilt = fmax(atheta_tilt, 0.0);
         atheta_tilt *= (1.0 - pgen.potential_pol_frac);
         if (pgen.psi != 0.0) {
