5ed911fc763eb37a361e29dbfe7575bda1c6ab9b
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2024-06-02 00:28:36 -0400

    adding modifications to M1 closures

--- src/radiation_femn/radiation_femn_closure.hpp
+++ src/radiation_femn/radiation_femn_closure.hpp
@@ -21,7 +21,7 @@ void ApplyFEMNFPNClosure(TeamMember_t member, int num_points, int m, int en, int
 
 KOKKOS_INLINE_FUNCTION
 void ApplyM1Closure(TeamMember_t member, int num_points, int m, int en, int kk, int jj, int ii, DvceArray5D<Real> f, ScrArray1D<Real> f_scratch,
-                    M1Closure m1_closure, ClosureFunc m1_closure_fun) {
+                    M1Closure m1_closure, ClosureFunc m1_closure_fun, Real rad_E_floor = 1e-15, Real rad_eps = 1e-5) {
 
   Real E = Kokkos::sqrt(4. * M_PI) * f(m, en * num_points + 0, kk, jj, ii);          // (0,0)
   Real Fx = -Kokkos::sqrt(4. * M_PI / 3.0) * f(m, en * num_points + 3, kk, jj, ii);  // (1, 1)
@@ -30,102 +30,87 @@ void ApplyM1Closure(TeamMember_t member, int num_points, int m, int en, int kk, 
   Real F2 = Fx * Fx + Fy * Fy + Fz * Fz;
   Real Fnorm = Kokkos::sqrt(F2);
 
-  if (E < 1e-14 || Fnorm < 1e-14) {
-    f_scratch(0) = f(m, en * num_points + 0, kk, jj, ii);
-    f_scratch(1) = f(m, en * num_points + 1, kk, jj, ii);
-    f_scratch(2) = f(m, en * num_points + 2, kk, jj, ii);
-    f_scratch(3) = f(m, en * num_points + 3, kk, jj, ii);
+  E = Kokkos::fmax(E, rad_E_floor);
+  Real lim = E * E * (1. - rad_eps);
+  if (F2 > lim) {
+    Real fac = lim / F2;
+    Fx = fac * Fx;
+    Fy = fac * Fy;
+    Fz = fac * Fz;
+  }
+  F2 = Fx * Fx + Fy * Fy + Fz * Fz;
+  Fnorm = Kokkos::sqrt(F2);
 
-    Real a = 1. / 3.;
-    Real Pxx = a * E;
-    Real Pyy = a * E;
-    Real Pzz = a * E;
-    Real Pxy = 0.;
-    Real Pxz = 0.;
-    Real Pyz = 0.;
+  // Normalized flux
+  Real fx = Fx / E;
+  Real fy = Fy / E;
+  Real fz = Fz / E;
+  Real fnorm = Fnorm / E;
+  Real fixed_fnorm = Kokkos::fmin(1.0, fnorm);
 
-    //f_scratch(4) = 0;
-    //f_scratch(5) = 0;
-    //f_scratch(6) = 0;
-    //f_scratch(7) = 0;
-    //f_scratch(8) = 0;
+  // Flux direction
+  Real nx = (fnorm > 0) ? fx / fnorm : 0;
+  Real ny = (fnorm > 0) ? fy / fnorm : 0;
+  Real nz = (fnorm > 0) ? fz / fnorm : 0;
 
-    f_scratch(4) = Kokkos::sqrt(60. * M_PI) * Pxy / (4. * M_PI);            // (2, -2)
-    f_scratch(5) = -Kokkos::sqrt(60. * M_PI) * Pyz / (4. * M_PI);           // (2, -1)
-    f_scratch(6) = Kokkos::sqrt(5. * M_PI) * (3. * Pzz - E) / (4. * M_PI);   // (2, 0)
-    f_scratch(7) = -Kokkos::sqrt(60. * M_PI) * Pxz / (4. * M_PI);           // (2, 1)
-    f_scratch(8) = Kokkos::sqrt(15. * M_PI) * (Pxx - Pyy) / (4. * M_PI);    // (2, 2)
-  } else {
+  Real xi = fixed_fnorm;
+  Real chi = 1.0 / 3.0 + xi * xi * (6.0 - 2.0 * xi + 6.0 * xi * xi) / 15.0;
 
-    // Normalized flux
-    Real fx = Fx / E;
-    Real fy = Fy / E;
-    Real fz = Fz / E;
-    Real fnorm = Fnorm / E;
-    Real fixed_fnorm = Kokkos::fmin(1.0, fnorm);
-
-    // Flux direction
-    Real nx = fx / fnorm;
-    Real ny = fy / fnorm;
-    Real nz = fz / fnorm;
-
-
-    Real chi = (3. + 4. * fixed_fnorm * fixed_fnorm) / (5. + 2. * Kokkos::sqrt(4. - 3. * fixed_fnorm * fixed_fnorm));
-
-    if (m1_closure_fun == ClosureFunc::Eddington) {
-      chi = 1. / 3.;
-    }
-    if (m1_closure_fun == ClosureFunc::Thin) {
-      chi = 1;
-    }
-    if (m1_closure_fun == ClosureFunc::Kershaw) {
-      chi = 1; // @TODO: fix later
-    }
-
-    Real a = (1. - chi) / 2.;
-    Real b = (3. * chi - 1.) / 2.;
-
-    Real Pxx = 0;
-    Real Pyy = 0;
-    Real Pzz = 0;
-    Real Pxy = 0;
-    Real Pxz = 0;
-    Real Pyz = 0;
-
-    if (m1_closure == M1Closure::Charon) {
-      Pxx = a * E + b * nx * nx * E;
-      Pyy = a * E + b * ny * ny * E;
-      Pzz = a * E + b * nz * nz * E;
-      Pxy = b * nx * ny * E;
-      Pxz = b * nx * nz * E;
-      Pyz = b * ny * nz * E;
-    } else if (m1_closure == M1Closure::Shibata) {
-      Pxx = a * E + b * Fx * Fx / Fnorm;
-      Pyy = a * E + b * Fy * Fy / Fnorm;
-      Pzz = a * E + b * Fz * Fz / Fnorm;
-      Pxy = b * Fx * Fy / Fnorm;
-      Pxz = b * Fx * Fz / Fnorm;
-      Pyz = b * Fy * Fz / Fnorm;
-    } else {
-      Pxx = a * E + b * nx * nx * (F2 / E);
-      Pyy = a * E + b * ny * ny * (F2 / E);
-      Pzz = a * E + b * nz * nz * (F2 / E);
-      Pxy = b * nx * ny * (F2 / E);
-      Pxz = b * nx * nz * (F2 / E);
-      Pyz = b * ny * nz * (F2 / E);
-    }
-
-    f_scratch(0) = f(m, en * num_points + 0, kk, jj, ii);
-    f_scratch(1) = f(m, en * num_points + 1, kk, jj, ii);
-    f_scratch(2) = f(m, en * num_points + 2, kk, jj, ii);
-    f_scratch(3) = f(m, en * num_points + 3, kk, jj, ii);
-    f_scratch(4) = Kokkos::sqrt(60. * M_PI) * Pxy / (4. * M_PI);            // (2, -2)
-    f_scratch(5) = -Kokkos::sqrt(60. * M_PI) * Pyz / (4. * M_PI);           // (2, -1)
-    f_scratch(6) = Kokkos::sqrt(5. * M_PI) * (3. * Pzz - E) / (4. * M_PI);   // (2, 0)
-    f_scratch(7) = -Kokkos::sqrt(60. * M_PI) * Pxz / (4. * M_PI);           // (2, 1)
-    f_scratch(8) = Kokkos::sqrt(15. * M_PI) * (Pxx - Pyy) / (4. * M_PI);    // (2, 2)
+  if (m1_closure_fun == ClosureFunc::Eddington) {
+    chi = 1. / 3.;
+  }
+  if (m1_closure_fun == ClosureFunc::Thin) {
+    chi = 1;
+  }
+  if (m1_closure_fun == ClosureFunc::Kershaw) {
+    chi = 1.0 / 3.0 + 2.0 / 3.0 * xi * xi;
   }
 
+  chi = Kokkos::fmin(chi, 1);
+  chi = Kokkos::fmax(1. / 3., chi);
+
+  Real a = (1. - chi) / 2.;
+  Real b = (3. * chi - 1.) / 2.;
+
+  Real Pxx = 0;
+  Real Pyy = 0;
+  Real Pzz = 0;
+  Real Pxy = 0;
+  Real Pxz = 0;
+  Real Pyz = 0;
+
+  if (m1_closure == M1Closure::Charon) {
+    Pxx = a * E + b * nx * nx * E;
+    Pyy = a * E + b * ny * ny * E;
+    Pzz = a * E + b * nz * nz * E;
+    Pxy = b * nx * ny * E;
+    Pxz = b * nx * nz * E;
+    Pyz = b * ny * nz * E;
+  } else if (m1_closure == M1Closure::Shibata) {
+    Pxx = a * E + b * nx * nx * fnorm * E;
+    Pyy = a * E + b * ny * ny * fnorm * E;
+    Pzz = a * E + b * nz * nz * fnorm * E;
+    Pxy = b * nx * ny * fnorm * E;
+    Pxz = b * nx * nz * fnorm * E;
+    Pyz = b * ny * nz * fnorm * E;
+  } else {
+    Pxx = a * E + b * fx * fx * E;
+    Pyy = a * E + b * fy * fy * E;
+    Pzz = a * E + b * fz * fz * E;
+    Pxy = b * fx * fy * E;
+    Pxz = b * fx * fz * E;
+    Pyz = b * fy * fz * E;
+  }
+
+  f_scratch(0) = E / Kokkos::sqrt(4. * M_PI);                             // (0,0)
+  f_scratch(1) = -Fy / Kokkos::sqrt(4. * M_PI / 3.0);                     // (1,-1)
+  f_scratch(2) = Fz / Kokkos::sqrt(4. * M_PI / 3.0);                      // (1,0)
+  f_scratch(3) = -Fx / Kokkos::sqrt(4. * M_PI / 3.0);                     // (1,1)
+  f_scratch(4) = Kokkos::sqrt(60. * M_PI) * Pxy / (4. * M_PI);            // (2, -2)
+  f_scratch(5) = -Kokkos::sqrt(60. * M_PI) * Pyz / (4. * M_PI);           // (2, -1)
+  f_scratch(6) = Kokkos::sqrt(5. * M_PI) * (3. * Pzz - E) / (4. * M_PI);  // (2, 0)
+  f_scratch(7) = -Kokkos::sqrt(60. * M_PI) * Pxz / (4. * M_PI);           // (2, 1)
+  f_scratch(8) = Kokkos::sqrt(15. * M_PI) * (Pxx - Pyy) / (4. * M_PI);    // (2, 2)
 }
 
 KOKKOS_INLINE_FUNCTION
@@ -215,7 +200,7 @@ void ApplyM1Floor(TeamMember_t member, ScrArray1D<Real> x, Real rad_E_floor, Rea
   Real F2 = Fx * Fx + Fy * Fy + Fz * Fz;
   Real lim = E * E * (1. - rad_eps);
 
-  x(0) = Kokkos::fmax(x(0), rad_E_floor / Kokkos::sqrt(4. * M_PI));
+  x(0) = Kokkos::fmax(x(0), rad_E_floor);
 
   if (F2 > lim) {
     Real fac = lim / F2;
