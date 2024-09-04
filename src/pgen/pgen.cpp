IAS-Astrophysics/athenak

commit d26b7072b32c13b46f0884d2a21b66e25a078a1d

Restructure the PunctureTracker to be a general CompactObjectTracker

src/pgen/pgen.cpp
@@ -21,6 +21,7 @@
 #include "hydro/hydro.hpp"
 #include "mhd/mhd.hpp"
 #include "coordinates/adm.hpp"
+#include "z4c/compact_object_tracker.hpp"
 #include "z4c/z4c.hpp"
 #include "radiation/radiation.hpp"
 #include "srcterms/turb_driver.hpp"
@@ -189,6 +190,38 @@ ProblemGenerator::ProblemGenerator(ParameterInput *pin, Mesh *pm, IOWrapper resf
   IOWrapperSizeT data_size;
   std::memcpy(&data_size, &(variabledata[0]), sizeof(IOWrapperSizeT));
   // calculate total number of CC variables
+
+  if (pz4c != nullptr) {
+    Real last_output_time;
+    if (global_variable::my_rank == 0) {
+      if (resfile.Read_Reals(&last_output_time, 1) != 1) {
+        std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
+                  << std::endl << "z4c::last_output_time data size read from restart "
+                  << "file is incorrect, restart file is broken." << std::endl;
+        exit(EXIT_FAILURE);
+      }
+    }
+#if MPI_PARALLEL_ENABLED
+    MPI_Bcast(&last_output_time, sizeof(Real), MPI_CHAR, 0, MPI_COMM_WORLD);
+#endif
+    pz4c->last_output_time = last_output_time;
+
+    for (auto &pt: pz4c->ptracker) {
+      Real pos[3];
+      if (global_variable::my_rank == 0) {
+        if (resfile.Read_Reals(&pos[0], 3) != 3) {
+          std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
+                    << std::endl << "compact object tracker data size read from restart "
+                    << "file is incorrect, restart file is broken." << std::endl;
+          exit(EXIT_FAILURE);
+        }
+      }
+#if MPI_PARALLEL_ENABLED
+      MPI_Bcast(&pos, sizeof(Real), MPI_CHAR, 3, MPI_COMM_WORLD);
+#endif
+      pt.SetPos(pos);
+    }
+  }
 
   if (pturb != nullptr) {
     // root process reads size the random seed

src/CMakeLists.txt
@@ -147,6 +147,7 @@ add_executable(
         utils/lagrange_interpolator.cpp
         utils/tr_table.cpp
 
+        z4c/compact_object_tracker.cpp
         z4c/tmunu.cpp
         z4c/z4c.cpp
         z4c/z4c_adm.cpp
@@ -158,7 +159,6 @@ add_executable(
         z4c/z4c_Sbc.cpp
         z4c/z4c_calculate_weyl_scalars.cpp
         z4c/z4c_wave_extr.cpp
-        z4c/z4c_puncture_tracker.cpp
         z4c/z4c_amr.cpp
 )
 

src/driver/driver.cpp
@@ -577,7 +577,7 @@ void Driver::InitBoundaryValuesAndPrimitives(Mesh *pm) {
       (void) pmhd->ConToPrim(this, 0);
     } else {
       if (pz4c != nullptr) {
-        (void) pz4c->Z4cToADM_(this, 0);
+        (void) pz4c->ConvertZ4cToADM(this, 0);
       }
       (void) pdyngr->ConToPrim(this, 0);
     }

src/mesh/meshblock_pack.cpp
@@ -23,7 +23,6 @@
 #include "tasklist/numerical_relativity.hpp"
 #include "z4c/z4c.hpp"
 #include "dyn_grmhd/dyn_grmhd.hpp"
-#include "z4c/z4c_puncture_tracker.hpp"
 #include "diffusion/viscosity.hpp"
 #include "diffusion/resistivity.hpp"
 #include "radiation/radiation.hpp"
@@ -62,13 +61,7 @@ MeshBlockPack::~MeshBlockPack() {
   if (pnr    != nullptr) {delete pnr;}
   if (pturb  != nullptr) {delete pturb;}
   if (punit  != nullptr) {delete punit;}
-  if (pz4c   != nullptr) {
-    delete pz4c;
-    for (auto ptracker : pz4c_ptracker) {
-      delete ptracker;
-    }
-    pz4c_ptracker.resize(0);
-  }
+  if (pz4c   != nullptr) {delete pz4c;}
   if (ppart  != nullptr) {delete ppart;}
   // must be last, since it calls ~BoundaryValues() which (MPI) uses pmy_pack->pmb->nnghbr
   delete pmb;
@@ -193,15 +186,6 @@ void MeshBlockPack::AddPhysics(ParameterInput *pin) {
     pz4c = new z4c::Z4c(this, pin);
     padm = new adm::ADM(this, pin);
     ptmunu = new Tmunu(this, pin);
-    // init puncture tracker
-    int npunct = pin->GetOrAddInteger("z4c", "npunct", 0);
-    if (npunct > 0) {
-      pz4c_ptracker.reserve(npunct);
-      for (int n = 0; n < npunct; ++n) {
-        pz4c_ptracker.push_back(new z4c::PunctureTracker(pmesh, pin, n));
-      }
-    }
-
     nphysics++;
   } else {
     pz4c = nullptr;

src/mesh/meshblock_pack.hpp
@@ -31,7 +31,6 @@ namespace numrel {class NumericalRelativity;}
 class TurbulenceDriver;
 namespace radiation {class Radiation;}
 namespace z4c {class Z4c;}
-namespace z4c {class PunctureTracker;}
 namespace adm {class ADM;}
 namespace particles {class Particles;}
 namespace units {class Units;}
@@ -72,7 +71,6 @@ class MeshBlockPack {
   ion_neutral::IonNeutral *pionn=nullptr;
   TurbulenceDriver *pturb=nullptr;
   radiation::Radiation *prad=nullptr;
-  std::vector<z4c::PunctureTracker *> pz4c_ptracker;
   particles::Particles *ppart=nullptr;
 
   // units (needed to convert code units to cgs for, e.g., cooling or radiation)

src/outputs/restart.cpp
@@ -25,6 +25,7 @@
 #include "hydro/hydro.hpp"
 #include "mhd/mhd.hpp"
 #include "coordinates/adm.hpp"
+#include "z4c/compact_object_tracker.hpp"
 #include "z4c/z4c.hpp"
 #include "radiation/radiation.hpp"
 #include "srcterms/turb_driver.hpp"
@@ -141,7 +142,7 @@ void RestartOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin) {
   TurbulenceDriver* pturb=pm->pmb_pack->pturb;
   z4c::Z4c* pz4c = pm->pmb_pack->pz4c;
   adm::ADM* padm = pm->pmb_pack->padm;
-  int nhydro=0, nmhd=0, nrad=0, nforce=3, nz4c=0, nadm=0;
+  int nhydro=0, nmhd=0, nrad=0, nforce=3, nz4c=0, nadm=0, nco=0;
   if (phydro != nullptr) {
     nhydro = phydro->nhydro + phydro->nscalars;
   }
@@ -153,6 +154,7 @@ void RestartOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin) {
   }
   if (pz4c != nullptr) {
     nz4c = pz4c->nz4c;
+    nco = pz4c->ptracker.size();
   } else if (padm != nullptr) {
     nadm = padm->nadm;
   }
@@ -214,7 +216,25 @@ void RestartOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin) {
     resfile.Write_any_type(&(pm->cost_eachmb[0]), (pm->nmb_total)*sizeof(float),"byte");
   }
 
-  //--- STEP 3.  All ranks write data over all MeshBlocks (5D arrays) in parallel
+  //--- STEP 3.  Root process writes internal state of objects that require it
+  if (global_variable::my_rank == 0) {
+    // store z4c information
+    if (pz4c != nullptr) {
+      resfile.Write_any_type(&(pz4c->last_output_time), sizeof(Real), "byte");
+    }
+    // output puncture tracker data
+    if (nco > 0) {
+      for (auto & pt: pz4c->ptracker) {
+        resfile.Write_any_type(pt.GetPos(), 3*sizeof(Real), "byte");
+      }
+    }
+    // turbulence driver internal RNG
+    if (pturb != nullptr) {
+      resfile.Write_any_type(&(pturb->rstate), sizeof(RNG_State), "byte");
+    }
+  }
+
+  //--- STEP 4.  All ranks write data over all MeshBlocks (5D arrays) in parallel
   // This data read in ProblemGenerator constructor for restarts
 
   // total size of all cell-centered variables and face-centered fields to be written by
@@ -242,9 +262,6 @@ void RestartOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin) {
   }
   if (global_variable::my_rank == 0) {
     resfile.Write_any_type(&(data_size), sizeof(IOWrapperSizeT), "byte");
-    if (pturb != nullptr) {
-      resfile.Write_any_type(&(pturb->rstate), sizeof(RNG_State), "byte");
-    }
   }
 
   // calculate size of data written in Steps 1-2 above
@@ -252,10 +269,13 @@ void RestartOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin) {
                              sizeof(RegionSize) + 2*sizeof(RegionIndcs);
   IOWrapperSizeT step2size = (pm->nmb_total)*(sizeof(LogicalLocation) + sizeof(float));
 
+  IOWrapperSizeT step3size = 3*nco*sizeof(Real);
+  if (pz4c != nullptr) step3size += sizeof(Real);
+  if (pturb != nullptr) step3size += sizeof(RNG_State);
+
   // write cell-centered variables in parallel
-  IOWrapperSizeT offset_myrank  = step1size + step2size + sizeof(IOWrapperSizeT) +
+  IOWrapperSizeT offset_myrank  = step1size + step2size + step3size + sizeof(IOWrapperSizeT) +
                                   data_size*(pm->gids_eachrank[global_variable::my_rank]);
-  if (pturb != nullptr) offset_myrank += sizeof(RNG_State);
   IOWrapperSizeT myoffset = offset_myrank;
 
   // write cell-centered variables, one MeshBlock at a time (but parallelized over all

src/pgen/z4c_one_puncture.cpp
@@ -134,5 +134,5 @@ void ADMOnePuncture(MeshBlockPack *pmbp, ParameterInput *pin) {
 
 // how decide the refinement
 void RefinementCondition(MeshBlockPack* pmbp) {
-  pmbp->pz4c->pz4c_amr->Refine(pmbp);
+  pmbp->pz4c->pamr->Refine(pmbp);
 }

src/pgen/z4c_two_puncture.cpp
@@ -21,7 +21,6 @@
 #include "parameter_input.hpp"
 #include "z4c/z4c.hpp"
 #include "z4c/z4c_amr.hpp"
-#include "z4c/z4c_puncture_tracker.hpp"
 
 static ini_data *data;
 
@@ -371,5 +370,5 @@ void ADMTwoPunctures(MeshBlockPack *pmbp, ini_data *data) {
 
 // how decide the refinement
 void RefinementCondition(MeshBlockPack* pmbp) {
-  pmbp->pz4c->pz4c_amr->Refine(pmbp);
+  pmbp->pz4c->pamr->Refine(pmbp);
 }

commit dab304cb5b65b2c0b1efaddda58491bde99da517

Fix order in which things are read from restart file
This fixes a bug introduced in d26b7072b32c13b46f0884d2a21b66e25a078a1d

src/pgen/pgen.cpp
@@ -171,26 +171,7 @@ ProblemGenerator::ProblemGenerator(ParameterInput *pin, Mesh *pm, IOWrapper resf
     nadm = padm->nadm;
   }
 
-  // root process reads size of CC and FC data arrays from restart file
-  IOWrapperSizeT variablesize = sizeof(IOWrapperSizeT);
-  char *variabledata = new char[variablesize];
-  if (global_variable::my_rank == 0) { // the master process reads the variables data
-    if (resfile.Read_bytes(variabledata, 1, variablesize) != variablesize) {
-      std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
-                << std::endl << "Variable data size read from restart file is incorrect, "
-                << "restart file is broken." << std::endl;
-      exit(EXIT_FAILURE);
-    }
-  }
-#if MPI_PARALLEL_ENABLED
-  // then broadcast the datasize information
-  MPI_Bcast(variabledata, variablesize, MPI_CHAR, 0, MPI_COMM_WORLD);
-#endif
-
-  IOWrapperSizeT data_size;
-  std::memcpy(&data_size, &(variabledata[0]), sizeof(IOWrapperSizeT));
-  // calculate total number of CC variables
-
+  // root process reads z4c last_output_time and tracker data
   if (pz4c != nullptr) {
     Real last_output_time;
     if (global_variable::my_rank == 0) {
@@ -243,6 +224,25 @@ ProblemGenerator::ProblemGenerator(ParameterInput *pin, Mesh *pm, IOWrapper resf
     std::memcpy(&(pturb->rstate), &(rng_data[0]), sizeof(RNG_State));
   }
 
+  // root process reads size of CC and FC data arrays from restart file
+  IOWrapperSizeT variablesize = sizeof(IOWrapperSizeT);
+  char *variabledata = new char[variablesize];
+  if (global_variable::my_rank == 0) { // the master process reads the variables data
+    if (resfile.Read_bytes(variabledata, 1, variablesize) != variablesize) {
+      std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
+                << std::endl << "Variable data size read from restart file is incorrect, "
+                << "restart file is broken." << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+#if MPI_PARALLEL_ENABLED
+  // then broadcast the datasize information
+  MPI_Bcast(variabledata, variablesize, MPI_CHAR, 0, MPI_COMM_WORLD);
+#endif
+  IOWrapperSizeT data_size;
+  std::memcpy(&data_size, &(variabledata[0]), sizeof(IOWrapperSizeT));
+
+  // calculate total number of CC variables
   IOWrapperSizeT headeroffset;
   // master process gets file offset
   if (global_variable::my_rank == 0) {