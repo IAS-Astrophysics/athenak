6eb7c2bc9b3d74a2b303a6821ad3052dcaf75451
Yue Hu <43769601+wisYue@users.noreply.github.com>
2024-09-06 20:06:27 -0400

    Update driver.cpp
    add new imex integrator

--- src/driver/driver.cpp
+++ src/driver/driver.cpp
@@ -185,6 +185,33 @@ Driver::Driver(ParameterInput *pin, Mesh *pmesh, Real wtlim, Kokkos::Timer* ptim
       a_twid[2][1] = 0.25;
       a_twid[2][2] = 0.25;
       a_impl = 0.5;
+    } else if (integrator == "imex_new") {
+      // IMEX(2,3,2): Krapp et al. (2024, arXiv:2310.04435), Eq.30.
+      // three-stage explicit, two-stage implicit, second-order ImEx
+      // Note explicit steps may not reduce to RK2 based on the parameters chosen
+      nimp_stages = 2;
+      nexp_stages = 3;
+      cfl_limit = 1.0;
+      gamma = 1.7071067811865475;   //1+1/sqrt(2)
+      gam0[0] = 0.0;
+      gam1[0] = 1.0;
+      beta[0] = gamma;
+
+      gam0[1] = (2.0*gamma-1.0)/(2.0*gamma*gamma);
+      gam1[1] = 1.0-(2.0*gamma-1.0)/(2.0*gamma*gamma);
+      beta[1] = 1.0/(2.0*gamma);
+
+      gam0[2] = 1.0;
+      gam1[2] = 0.0;
+      beta[2] = 0.0;
+
+      a_twid[0][0] = (1.0-2*gamma*gamma)/(2.0*gamma);
+      a_twid[0][1] = 0.0;
+
+      a_twid[1][0] = 0.0;
+      a_twid[1][1] = 0.0;
+
+      a_impl = gamma;
     } else if (integrator == "imex3") {
       // IMEX-SSP3(4,3,3): Pareschi & Russo (2005) Table VI.
       // three-stage explicit, four-stage implicit, third-order ImEx
@@ -231,7 +258,7 @@ Driver::Driver(ParameterInput *pin, Mesh *pmesh, Real wtlim, Kokkos::Timer* ptim
     } else {
       std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
          << std::endl << "integrator=" << integrator << " not implemented. "
-         << "Valid choices are [rk1,rk2,rk3,imex2,imex3]." << std::endl;
+         << "Valid choices are [rk1,rk2,rk3,imex2,imex_new,imex3]." << std::endl;
       exit(EXIT_FAILURE);
     }
   }
