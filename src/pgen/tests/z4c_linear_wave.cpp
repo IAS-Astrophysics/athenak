a18551bf98771abead63f15cdfecd2f7091d13d8
Hengrui Zhu <hz0693@princeton.edu>
2024-09-08 19:54:30 -0400

    linting

--- src/pgen/tests/z4c_linear_wave.cpp
+++ src/pgen/tests/z4c_linear_wave.cpp
@@ -87,7 +87,7 @@ void ProblemGenerator::Z4cLinearWave(ParameterInput *pin, const bool restart) {
   // Calculate angular offset of the wavevector from zhat
   Real theta = std::atan2(sqrt(kx2 * kx2 + kx1 * kx1), kx3);
   Real phi = std::atan2(kx1, kx2);
-  
+
   // Find wavelength
   Real lambda = 1/knorm;
 
