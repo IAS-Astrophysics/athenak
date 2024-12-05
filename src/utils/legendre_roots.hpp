4b685f9a61d39897353f875d7d63b6af1096873d
Hengrui Zhu <hz0693@della-gpu.princeton.edu>
2024-12-05 10:16:05 -0500

    revert to an earlier working version

--- src/utils/legendre_roots.hpp
+++ src/utils/legendre_roots.hpp
@@ -1,18 +1,9 @@
-#ifndef UTILS_LEGENDRE_ROOTS_HPP_
-#define UTILS_LEGENDRE_ROOTS_HPP_
-//========================================================================================
-// AthenaXXX astrophysical plasma code
-// Copyright(C) 2020 James M. Stone <jmstone@ias.edu> and the Athena code team
-// Licensed under the 3-clause BSD License (the "LICENSE")
-//========================================================================================
-//! \file legendre_roots.hpp
-//! \brief Find out the roots & weights of Gauss-Legendre Quadrature for given n
-
+/**************************************************************************
+******Find out the roots & weights of Gauss-Legendre Quadrature for given n 
+***************************************************************************/
 #include <iostream>
-#include <cmath> // NAN
+#include <cmath>
 #include <list>
-#include <vector>
-
 #include "athena.hpp"
 
 /*Legendre Polynomial P0(x)*/
@@ -31,7 +22,7 @@ double Pn(int n, double x) {
     return P0(x);
   } else if(n==1) {
     return P1(x);
-  } else {
+  } else{
     //Use the recurrence relation
     return (double )((2*n-1)*x*Pn(n-1,x)-(n-1)*Pn(n-2,x))/n;
   }
@@ -41,9 +32,9 @@ double Pn(int n, double x) {
 double Li(int n, std::vector<double> x, int i, double X) {
   int j;
   double prod=1;
-  for(j=0; j<=n; j++) {
-    if (j!=i) {
-      prod=prod*(X-x[j])/(x[i]-x[j]);
+  for(j=0;j<=n;j++) {
+    if (j!=i){
+      prod=prod*(X-x[j])/(x[i]-x[j]);     
     }
   }
   return prod;
@@ -54,11 +45,12 @@ double Ci(int i, int n, std::vector<double> x, double a, double b, int N) {
   double h,integral,X,sum=0;
   int j,k;
   h=(b-a)/N;
-  for(j=1; j<N; j++) {
+  for(j=1;j<N;j++) {
     X=a+j*h;
     if(j%2==0) {
       sum=sum+2*Li(n-1,x,i,X);
-    } else {
+    }
+    else {
       sum=sum+4*Li(n-1,x,i,X);
     }
   }
@@ -68,12 +60,10 @@ double Ci(int i, int n, std::vector<double> x, double a, double b, int N) {
   return integral;
 }
 
-/*Function definition for bisection procedure
-// [Returns the root if found or NAN for failure]*/
-double Bisection(int n,double f(int n,double x),double a, double b,
-                double eps, int maxSteps) {
+/*Function definition for bisection procedure[Returns the root if found or 999 for failure]*/
+double Bisection(int n,double f(int n,double x),double a, double b, double eps, int maxSteps) {
   double c;
-  if(f(n,a)*f(n,b)<=0) {
+  if(f(n,a)*f(n,b)<=0) {  
     int iter=1;
     /*Bisection Method begins that tabulates the various values at each iteration*/
     do {
@@ -89,14 +79,17 @@ double Bisection(int n,double f(int n,double x),double a, double b,
     } while(fabs(a-b)>=eps&&iter<=maxSteps);
     return c;
   } else {
-    return NAN;
+    return 999;
   }
 }
 
 std::vector<std::vector<Real>> RootsAndWeights(int n) {
+
   //Array to store the roots and weights of Legendre polynomials
   std::vector<std::vector<Real>> roots_weights(2, std::vector<Real>(n));
+  
   std::vector<double> xi;
+
   //window(Step-size) for bisection method
   double h=0.01;
   //dummy variable for bisection method
@@ -105,21 +98,20 @@ std::vector<std::vector<Real>> RootsAndWeights(int n) {
   double root;
   int i=0;
 
-  for(x=-1.0; x<=1.0; x=x+h) {
-    // set the accuracy to approx. 10^-14 but there is also a limit on maxSteps.
+  for(x=-1.0;x<=1.0;x=x+h) {
+    //set the accuracy to approx. 10^-15 but there is also a limit on maxSteps. (Modify these acc. to your needs)
     root=Bisection(n,Pn,x,x+h,1e-14,1000000);
 
-    if(root!=NAN) {
+    if(root!=999) {
       xi.push_back(root);
       roots_weights[0][i]=root;
       i++;
     }
   }
 
-  for(i=0; i<n; i++) {
-    //(Modify the number of sub-intervals according to your needs)
-    roots_weights[1][i]=Ci(i,n,xi,-1,1,1000000);
+  for(i=0;i<n;i++) {
+      //(Modify the number of sub-intervals according to your needs)
+      roots_weights[1][i]=Ci(i,n,xi,-1,1,1000000);
   }
   return roots_weights;
 }
-#endif // UTILS_LEGENDRE_ROOTS_HPP_
