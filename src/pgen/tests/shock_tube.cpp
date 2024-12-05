cf9340eac63689e1607237f4b50351b5c9d8ed3e
Hengrui Zhu <hz0693@princeton.edu>
2024-12-05 10:22:04 -0500

    fix bug

--- src/pgen/tests/shock_tube.cpp
+++ src/pgen/tests/shock_tube.cpp
@@ -220,7 +220,7 @@ void ProblemGenerator::ShockTube(ParameterInput *pin, const bool restart) {
     auto &b0 = pmbp->pmhd->b0;
     auto &bcc0 = pmbp->pmhd->bcc0;
     auto &nscal = pmbp->pmhd->nscalars;
-    auto &shk_dir_ = shk_dir;
+    auto shk_dir_ = shk_dir;
     par_for("pgen_shock1", DevExeSpace(),0,(pmbp->nmb_thispack-1),ks,ke,js,je,is,ie,
     KOKKOS_LAMBDA(int m,int k, int j, int i) {
       Real x,bxl,byl,bzl,bxr,byr,bzr;
