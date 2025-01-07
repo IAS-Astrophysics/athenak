85eacc3bd57ad3cb6264afcf4b9ece49ff470dd6
Magdalena Siwek <mssiwek92@gmail.com>
2025-01-07 15:36:58 -0500

    cbd pgen file

---
+++ src/pgen/cbd.cpp
@@ -0,0 +1,27 @@
+//========================================================================================
+// AthenaXXX astrophysical plasma code
+// Copyright(C) 2020 James M. Stone <jmstone@ias.edu> and the Athena code team
+// Licensed under the 3-clause BSD License (the "LICENSE")
+//========================================================================================
+//! \file cbd.cpp
+//! \brief Problem generator to initialize circumbinary accretion disks 
+//!        around binaries, with or without magnetic fields. Written by MSS.
+
+
+// include headers
+#include <stdio.h>
+#include <math.h>
+
+
+// prototypes for functions used internally to this pgen
+
+
+
+//----------------------------------------------------------------------------------------
+//! \fn void ProblemGenerator::UserProblem()
+//! \brief Sets initial conditions for circumbinary accretion disk
+//! Compile with '-D PROBLEM=cbd' to enroll as user-specific problem generator
+
+void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
+
+};
