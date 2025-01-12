09d8c9f03c46f7bd9066dd9d0cb3920fb9449686
Hengrui Zhu <hz0693@princeton.edu>
2025-01-12 09:57:17 -0500

    strong stability test

--- src/pgen/pgen.cpp
+++ src/pgen/pgen.cpp
@@ -81,6 +81,8 @@ ProblemGenerator::ProblemGenerator(ParameterInput *pin, Mesh *pm) :
   } else if (pgen_fun_name.compare("diffusion") == 0) {
     Diffusion(pin, false);
   // else, name not set on command line or input file, print warning and quit
+  } else if (pgen_fun_name.compare("z4c_stability") == 0) {
+    Z4cStability(pin, false);
   } else {
     std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__ << std::endl
         << "Problem generator name could not be found in <problem> block in input file"
