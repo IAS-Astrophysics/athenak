66019d1c2fefad685e930c72580b350f0ee050e4
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2025-01-21 12:34:29 -0500

    cleaning up time update

--- src/radiation_m1/radiation_m1_macro.hpp
+++ src/radiation_m1/radiation_m1_macro.hpp
@@ -9,27 +9,34 @@
 //! \file radiation_m1_macro.hpp
 //  \brief macros for Grey M1 radiation class
 
-//#ifndef WARN_FOR_SRC_FIX
-//#define WARN_FOR_SRC_FIX
-//#endif
+// #ifndef WARN_FOR_SRC_FIX
+// #define WARN_FOR_SRC_FIX
+// #endif
+#define M1_E_IDX 0
+#define M1_FX_IDX 1
+#define M1_FY_IDX 2
+#define M1_FZ_IDX 3
+#define M1_N_IDX 4
 
-#define RADIATION_M1_SRC_EXPL         1       // explicit RHS
-#define RADIATION_M1_SRC_IMPL         2       // implicit RHS (default)
-#define RADIATION_M1_SRC_BOOST        3       // boost to fluid frame (approximate!)
+#define RADIATION_M1_SRC_EXPL 1  // explicit RHS
+#define RADIATION_M1_SRC_IMPL 2  // implicit RHS (default)
+#define RADIATION_M1_SRC_BOOST 3 // boost to fluid frame (approximate!)
 #ifndef RADIATION_M1_SRC_METHOD
-#define RADIATION_M1_SRC_METHOD       RADIATION_M1_SRC_IMPL
+#define RADIATION_M1_SRC_METHOD RADIATION_M1_SRC_IMPL
 #endif
 
-#define RADIATION_M1_NGHOST           2
+#define RADIATION_M1_NGHOST 2
 
-#define RADIATION_M1_CLS_SHIBATA      1       // optically thin limit from Shibata 2011
-#define RADIATION_M1_CLS_SIMPLE       2       // simplified closure (needed for inv closure to work)
+#define RADIATION_M1_CLS_SHIBATA 1 // optically thin limit from Shibata 2011
+#define RADIATION_M1_CLS_SIMPLE                                                \
+  2 // simplified closure (needed for inv closure to work)
 #ifndef RADIATION_M1_CLS_METHOD
-#define RADIATION_M1_CLS_METHOD       RADIATION_M1_CLS_SIMPLE
+#define RADIATION_M1_CLS_METHOD RADIATION_M1_CLS_SIMPLE
 #endif
 
-#define CGS_GCC (1.619100425158886e-18) // Multiply to convert density from CGS to Cactus
+#define CGS_GCC                                                                \
+  (1.619100425158886e-18) // Multiply to convert density from CGS to Cactus
 
-#define SQ(X) ((X)*(X))
+#define SQ(X) ((X) * (X))
 
-#endif //RADIATION_M1_MACRO_HPP
+#endif // RADIATION_M1_MACRO_HPP
