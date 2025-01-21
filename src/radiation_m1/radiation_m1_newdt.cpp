c5e545d751c55a7134999f64378758603e5c0cb8
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2025-01-21 16:15:27 -0500

    driver working for radiation m1. beam test does not run correctly (no beam)

--- src/radiation_m1/radiation_m1_newdt.cpp
+++ src/radiation_m1/radiation_m1_newdt.cpp
@@ -26,11 +26,27 @@ namespace radiationm1 {
 // \brief calculate the minimum timestep within a MeshBlockPack for radiation problems
 
 TaskStatus RadiationM1::NewTimeStep(Driver *pdrive, int stage) {
-  if (stage != (pdrive->nexp_stages)) {
-    return TaskStatus::complete; // only execute last stage
-  }
+  Real dt1 = std::numeric_limits<float>::max();
+  Real dt2 = std::numeric_limits<float>::max();
+  Real dt3 = std::numeric_limits<float>::max();
 
-  // insert code here
+  // capture class variables for kernel
+  auto &mbsize = pmy_pack->pmb->mb_size;
+  int nmb1 = pmy_pack->nmb_thispack;
+
+  Kokkos::parallel_reduce("radiation_m1_newdt", Kokkos::RangePolicy<>(DevExeSpace(), 0, nmb1),
+                          KOKKOS_LAMBDA(const int &m, Real &min_dt1, Real &min_dt2, Real &min_dt3) {
+                              min_dt1 = fmin(mbsize.d_view(m).dx1, min_dt1);
+                              min_dt2 = fmin(mbsize.d_view(m).dx2, min_dt2);
+                              min_dt3 = fmin(mbsize.d_view(m).dx3, min_dt3);
+                          }, Kokkos::Min<Real>(dt1), Kokkos::Min<Real>(dt2), Kokkos::Min<Real>(dt3));
+
+  // compute minimum of dt1/dt2/dt3 for 1D/2D/3D problems
+  dtnew = dt1;
+  if (pmy_pack->pmesh->multi_d) { dtnew = std::min(dtnew, dt2); }
+  if (pmy_pack->pmesh->three_d) { dtnew = std::min(dtnew, dt3); }
+
+  return TaskStatus::complete;
 
   return TaskStatus::complete;
 }
