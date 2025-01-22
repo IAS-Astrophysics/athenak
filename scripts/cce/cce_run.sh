2d01df15868d8568133e1e6e3716d83fbd3f3006
Alireza <rashti.alireza@gmail.com>
2025-01-22 09:58:13 -0500

    update debug scripts.

--- scripts/cce/cce_run.sh
+++ scripts/cce/cce_run.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env bash
+set -x
 
+if false
+then
 cd runs
-
 echo "running preproc...\n"
-./PreprocessCceWorldtube --input-file PreprocessCceWorldtube_test.yaml
-
+./PreprocessCceWorldtube --input-file PreprocessCceWorldtube_bin.yaml
 echo "-------------------------"
-
-
 echo "running CCE..."
-./CharacteristicExtract --input-file cce_par.yaml
+./CharacteristicExtract --input-file cce_par_bin.yaml
+
+# h5 test
+elif false
+then
+cd runs
+echo "running preproc...\n"
+./PreprocessCceWorldtube --input-file PreprocessCceWorldtube_h5.yaml
+echo "-------------------------"
+echo "running CCE..."
+./CharacteristicExtract --input-file cce_par_h5.yaml
+
+# q2
+elif true
+then
+cd runs
+echo "running preproc...\n"
+./PreprocessCceWorldtube --input-file PreprocessCceWorldtube_q2_h5.yaml
+echo "-------------------------"
+echo "running CCE..."
+./CharacteristicExtract --input-file cce_par_q2_h5.yaml
+fi
 
--- scripts/cce/pre_run.sh
+++ scripts/cce/pre_run.sh
@@ -9,33 +9,44 @@ then
 fi
 
 file="./debug/CceR0030.00.h5"
+dout='./debug/bin'
+
+if false
+then
+./athk_to_spectre.py -fpath dat/bbh_q2.0_chizp0.0_chizm0.0_d10.0_lev13_n128_fixed_cce_decomp_shell_3.h5 \
+-ftype h5 -d_out ./debug/q2/ -radius 100
+fi
+
+file="./debug/q2/CceR0100.00.h5"
+dout='./debug/q2'
+
 # plot fig
 fname="gxx"
-mode="Re(0,0)"
-./debug_athk_to_spectre.py -debug plot_simple -dout ./debug \
+mode="Re(2,2)"
+./debug_athk_to_spectre.py -debug plot_simple -dout "${dout}" \
 	-fpath $file  -field_name $fname -field_mode $mode
 
-mode="Re(2,2)"
-./debug_athk_to_spectre.py -debug plot_simple -dout ./debug \
-	-fpath $file  -field_name $fname -field_mode $mode
+#mode="Re(0,0)"
+#./debug_athk_to_spectre.py -debug plot_simple -dout "${dout}" \
+#	-fpath $file  -field_name $fname -field_mode $mode
 
 
 fname="alp"
-mode="Re(0,0)"
-./debug_athk_to_spectre.py -debug plot_simple -dout ./debug \
-	-fpath $file  -field_name $fname -field_mode $mode
+#mode="Re(0,0)"
+#./debug_athk_to_spectre.py -debug plot_simple -dout "${dout}" \
+#	-fpath $file  -field_name $fname -field_mode $mode
 
 mode="Re(2,2)"
-./debug_athk_to_spectre.py -debug plot_simple -dout ./debug \
+./debug_athk_to_spectre.py -debug plot_simple -dout "${dout}" \
 	-fpath $file  -field_name $fname -field_mode $mode
 
 
 fname="betax"
-mode="Re(0,0)"
-./debug_athk_to_spectre.py -debug plot_simple -dout ./debug \
-	-fpath $file  -field_name $fname -field_mode $mode
+#mode="Re(0,0)"
+#./debug_athk_to_spectre.py -debug plot_simple -dout "${dout}" \
+#	-fpath $file  -field_name $fname -field_mode $mode
 
 mode="Re(2,2)"
-./debug_athk_to_spectre.py -debug plot_simple -dout ./debug \
+./debug_athk_to_spectre.py -debug plot_simple -dout "${dout}" \
 	-fpath $file  -field_name $fname -field_mode $mode
 
