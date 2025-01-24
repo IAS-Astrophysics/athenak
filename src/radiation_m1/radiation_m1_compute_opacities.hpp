60a659ad7bde9177528282b3a7190d2e22c4af2f
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2025-01-23 21:59:12 -0500

    adding support for explicit sources. wip.

---
+++ src/radiation_m1/radiation_m1_compute_opacities.hpp
@@ -0,0 +1,45 @@
+#ifndef RADIATION_M1_COMPUTE_OPACITIES_HPP
+#define RADIATION_M1_COMPUTE_OPACITIES_HPP
+//========================================================================================
+// AthenaXXX astrophysical plasma code
+// Copyright(C) 2020 James M. Stone <jmstone@ias.edu> and the Athena code team
+// Licensed under the 3-clause BSD License (the "LICENSE")
+//========================================================================================
+//! \file radiation_m1_compute_opacities.hpp
+//  \brief compute toy opacities/Weakrates/bns_nurates
+
+#include "athena.hpp"
+#include "radiation_m1/radiation_m1.hpp"
+
+namespace radiationm1 {
+
+struct M1Opacities {
+
+  Real eta_0[RADIATION_M1_TOTAL_NUM_SPECIES];
+  Real abs_0[RADIATION_M1_TOTAL_NUM_SPECIES];
+
+  Real eta_1[RADIATION_M1_TOTAL_NUM_SPECIES];
+  Real abs_1[RADIATION_M1_TOTAL_NUM_SPECIES];
+  Real scat_1[RADIATION_M1_TOTAL_NUM_SPECIES];
+};
+typedef struct M1Opacities M1Opacities;
+
+KOKKOS_INLINE_FUNCTION
+M1Opacities ComputeM1Opacities(const RadiationM1Params &params) {
+  M1Opacities opacities{};
+
+  switch (params.opacity_type) {
+  case RadiationM1OpacityType::Toy:
+    break;
+  case RadiationM1OpacityType::Weakrates:
+    break;
+  case RadiationM1OpacityType::BnsNurates:
+    break;
+  default:
+    break;
+  }
+  return opacities;
+}
+
+} // namespace radiationm1
+#endif // RADIATION_M1_COMPUTE_OPACITIES_HPP
