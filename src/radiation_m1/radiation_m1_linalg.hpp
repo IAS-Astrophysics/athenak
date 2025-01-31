17a541647d44bd65a48972e2b4c93bbc591a0348
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2025-01-31 15:38:47 -0500

    cleaning up hybridsj init and iterate function

--- src/radiation_m1/radiation_m1_linalg.hpp
+++ src/radiation_m1/radiation_m1_linalg.hpp
@@ -9,8 +9,25 @@
 //! \file radiation_m1_linalg.hpp
 //  \brief Linear algebra routines for M1
 
+#include "radiation_m1_macro.hpp"
+
+#include <athena.hpp>
+
 namespace radiationm1 {
 
+//----------------------------------------------------------------------------------------
+//! \fn Real radiationm1::compute_diag
+//  \brief computes the columnwise L2 norm of a matrix J abd store in diag
+KOKKOS_INLINE_FUNCTION
+void qr_factorize(const Real (&J)[M1_MULTIROOTS_DIM][M1_MULTIROOTS_DIM],
+                  Real (&Q)[M1_MULTIROOTS_DIM][M1_MULTIROOTS_DIM],
+                  Real (&R)[M1_MULTIROOTS_DIM][M1_MULTIROOTS_DIM]) {}
+
+KOKKOS_INLINE_FUNCTION
+void qr_update(Real (&Q)[M1_MULTIROOTS_DIM][M1_MULTIROOTS_DIM],
+               Real (&R)[M1_MULTIROOTS_DIM][M1_MULTIROOTS_DIM],  Real (&W)[M1_MULTIROOTS_DIM],
+                Real (&V)[M1_MULTIROOTS_DIM]) {
 
 }
-#endif //RADIATION_M1_LINALG_HPP
+} // namespace radiationm1
+#endif // RADIATION_M1_LINALG_HPP
