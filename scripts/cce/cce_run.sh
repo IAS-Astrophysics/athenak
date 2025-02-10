ea9495c31ba256950b6236c8d1987f4aa23e9e43
Alireza <rashti.alireza@gmail.com>
2025-02-10 17:14:28 -0500

    add: more test scripts.

--- scripts/cce/cce_run.sh
+++ scripts/cce/cce_run.sh
@@ -29,5 +29,15 @@ echo "running preproc...\n"
 echo "-------------------------"
 echo "running CCE..."
 ./CharacteristicExtract --input-file cce_par_q2_h5.yaml
+
+# q2 dev
+elif false
+then
+cd runs
+echo "running preproc...\n"
+./PreprocessCceWorldtube --input-file PreprocessCceWorldtube_q2_dev_h5.yaml
+echo "-------------------------"
+echo "running CCE..."
+./CharacteristicExtract --input-file cce_par_q2_dev_h5.yaml
 fi
 
--- scripts/cce/pre_run.sh
+++ scripts/cce/pre_run.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+set -x
+
 # bin
-if true
+if false
 then
 # create
 dout='./debug/dev'
 file='./dat/bin/cce_bbh/'
 fdebug='./debug/dev/CceR0030.00.h5'
+
+rm -vf ${dout}/*
 
 ./athk_to_spectre.py -ftype bin -d_out "$dout" -debug y \
 -t_deriv fin_diff -r_deriv ChebU -interpolation ChebU \
@@ -20,13 +24,15 @@ mode="Re(2,2)"
 	-fpath $fdebug  -field_name $fname -field_mode $mode
 
 # h5 bbh, q=2
-elif false
+elif true
 then
-./athk_to_spectre.py -fpath dat/bbh_q2.0_chizp0.0_chizm0.0_d10.0_lev13_n128_fixed_cce_decomp_shell_3.h5 \
--ftype h5 -d_out ./debug/q2/ -radius 100
-
 file="./debug/q2/CceR0100.00.h5"
 dout='./debug/q2'
+mkdir -p ${dout}
+rm -rvf ${dout}/*
+
+./athk_to_spectre.py -fpath dat/bbh_q2.0_chizp0.0_chizm0.0_d10.0_lev13_n128_fixed_cce_decomp_shell_3.h5 \
+-ftype h5 -d_out "${dout}" -radius 100
 
 # plot fig:
 fname="gxx"
@@ -74,6 +80,31 @@ fname="gxx"
 mode="Re(2,2)"
 ./debug_athk_to_spectre.py -debug plot_simple -dout "${dout}" \
 	-fpath $file  -field_name $fname -field_mode $mode
+
+elif false
+then
+file="./debug/q2_dev/CceR0100.00.h5"
+dout='./debug/q2_dev/'
+mkdir -p ${dout}
+
+./athk_to_spectre.py -fpath dat/bbh_q2.0_chizp0.0_chizm0.0_d10.0_lev13_n128_fixed_cce_decomp_shell_3.h5 \
+-ftype h5 -d_out "$dout" -radius 100
+
+
+# plot fig:
+fname="gxx"
+mode="Re(2,2)"
+./debug_athk_to_spectre.py -debug plot_simple -dout "${dout}" \
+	-fpath $file  -field_name $fname -field_mode $mode
+
+# h5 quick development
+elif false
+then
+dout='./debug/quick_dev/'
+mkdir -p ${dout}
+
+./athk_to_spectre.py -fpath dat/backup_dilute_d32_fixed_cce_decomp_shell_4.h5 \
+-ftype h5 -d_out "$dout" -radius 70
 
 fi
 
