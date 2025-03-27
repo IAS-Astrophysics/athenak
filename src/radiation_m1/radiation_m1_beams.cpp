b8d180e69b8bee153ca1aec015372145f9b809b4
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2025-03-26 21:40:17 -0400

    beams around black hole seems to be working in isotropic coordinates. adding new beam launching conditions

--- src/radiation_m1/radiation_m1_beams.cpp
+++ src/radiation_m1/radiation_m1_beams.cpp
@@ -27,7 +27,7 @@ void ApplyBeamSources1D(Mesh *pmesh) {
   auto &beam_source_1_vals_ = pmesh->pmb_pack->pradm1->rad_m1_beam.beam_source_vals;
 
   par_for(
-      "radiation_femn_beams_populate_1d", DevExeSpace(), 0, nmb1, 0, nvarstotm1,
+      "radiation_m1_beams_populate_1d", DevExeSpace(), 0, nmb1, 0, nvarstotm1,
       KOKKOS_LAMBDA(int m, int n) {
         switch (mb_bcs.d_view(m, BoundaryFace::inner_x1)) {
           case BoundaryFlag::outflow:
@@ -62,7 +62,7 @@ void ApplyBeamSources2D(Mesh *pmesh) {
   auto &beam_source_1_y2_ = pmesh->pmb_pack->pradm1->rad_m1_beam.beam_ymax;
 
   par_for(
-      "radiation_femn_beams_populate_2d", DevExeSpace(), 0, nmb1, 0, nvarstotm1, 0,
+      "radiation_m1_beams_populate_2d", DevExeSpace(), 0, nmb1, 0, nvarstotm1, 0,
       (n3 - 1), 0, (n2 - 1), KOKKOS_LAMBDA(int m, int n, int k, int j) {
         Real &x2min = size.d_view(m).x2min;
         Real &x2max = size.d_view(m).x2max;
@@ -82,4 +82,99 @@ void ApplyBeamSources2D(Mesh *pmesh) {
         }
       });
 }
+
+// Beam for the M1 beam test around black hole (2d only)
+void ApplyBeamSourcesBlackHole(Mesh *pmesh) {
+  auto &indcs = pmesh->mb_indcs;
+  int &is = indcs.is;
+  int &js = indcs.js;
+
+  int nmb1 = pmesh->pmb_pack->nmb_thispack - 1;
+  auto &mb_bcs = pmesh->pmb_pack->pmb->mb_bcs;
+  auto &size = pmesh->pmb_pack->pmb->mb_size;
+  int &ng = indcs.ng;
+  int n2 = (indcs.nx2 > 1) ? (indcs.nx2 + 2 * ng) : 1;
+  int n3 = (indcs.nx3 > 1) ? (indcs.nx3 + 2 * ng) : 1;
+  auto &params_ = pmesh->pmb_pack->pradm1->params;
+  auto nvars_ = pmesh->pmb_pack->pradm1->nvars;
+
+  auto &u0_ = pmesh->pmb_pack->pradm1->u0;
+  adm::ADM::ADM_vars &adm = pmesh->pmb_pack->padm->adm;
+
+  auto &beam_source_1_y1_ = pmesh->pmb_pack->pradm1->rad_m1_beam.beam_ymin;
+  auto &beam_source_1_y2_ = pmesh->pmb_pack->pradm1->rad_m1_beam.beam_ymax;
+
+  par_for(
+      "radiation_m1_beam_populate_black_hole", DevExeSpace(), 0, nmb1, 0, (n3 - 1), 0,
+      (n2 - 1), KOKKOS_LAMBDA(int m, int k, int j) {
+        Real &x2min = size.d_view(m).x2min;
+        Real &x2max = size.d_view(m).x2max;
+        int nx2 = indcs.nx2;
+        Real x2 = CellCenterX(j - js, nx2, x2min, x2max);
+
+        switch (mb_bcs.d_view(m, BoundaryFace::inner_x1)) {
+          case BoundaryFlag::outflow:
+            if (beam_source_1_y1_ <= x2 && x2 <= beam_source_1_y2_) {
+              for (int i = 0; i < ng; ++i) {
+                // Calculate inverse 4-metric and sqrt(det(3-metric))
+                AthenaPointTensor<Real, TensorSymm::SYM2, 4, 2> g_uu{};
+                Real garr_uu[16];
+                adm::SpacetimeUpperMetric(
+                    adm.alpha(m, k, j, i), adm.beta_u(m, 0, k, j, i),
+                    adm.beta_u(m, 1, k, j, i), adm.beta_u(m, 2, k, j, i),
+                    adm.g_dd(m, 0, 0, k, j, i), adm.g_dd(m, 0, 1, k, j, i),
+                    adm.g_dd(m, 0, 2, k, j, i), adm.g_dd(m, 1, 1, k, j, i),
+                    adm.g_dd(m, 1, 2, k, j, i), adm.g_dd(m, 2, 2, k, j, i), garr_uu);
+                for (int a = 0; a < 4; ++a) {
+                  for (int b = 0; b < 4; ++b) {
+                    g_uu(a, b) = garr_uu[a + b * 4];
+                  }
+                }
+                const Real gam = adm::SpatialDet(
+                    adm.g_dd(m, 0, 0, k, j, i), adm.g_dd(m, 0, 1, k, j, i),
+                    adm.g_dd(m, 0, 2, k, j, i), adm.g_dd(m, 1, 1, k, j, i),
+                    adm.g_dd(m, 1, 2, k, j, i), adm.g_dd(m, 2, 2, k, j, i));
+                const Real volform = Kokkos::sqrt(gam);
+
+                // Solve for beam id
+                const Real g_xx = adm.g_dd(m, 0, 0, k, j, i);
+                Real beta_x = 0;
+                Real beta2 = 0;
+                for (int idx = 0; idx < 3; idx++) {
+                  beta_x += adm.g_dd(m, 0, idx, k, j, i) * adm.beta_u(m, idx, k, j, i);
+                  for (int idx2 = 0; idx2 < 3; idx2++) {
+                    beta2 += adm.g_dd(m, idx, idx2, k, j, i) *
+                             adm.beta_u(m, idx, k, j, i) * adm.beta_u(m, idx2, k, j, i);
+                  }
+                }
+                const Real a =
+                    (-beta_x + sqrt(beta_x * beta_x - beta2 +
+                                    adm.alpha(m, k, j, i) * adm.alpha(m, k, j, i) *
+                                        (1 - params_.rad_eps))) /
+                    g_xx;
+
+                Real E = volform * 1.;
+                Real Fx = a * E / adm.alpha(m, k, j, i) +
+                          adm.beta_u(m, 0, k, j, i) * E / adm.alpha(m, k, j, i);
+                Real Fy = adm.beta_u(m, 1, k, j, i) * E / adm.alpha(m, k, j, i);
+                Real Fz = adm.beta_u(m, 2, k, j, i) * E / adm.alpha(m, k, j, i);
+                AthenaPointTensor<Real, TensorSymm::NONE, 4, 1> F_d{};
+                pack_F_d(adm.beta_u(m, 0, k, j, i), adm.beta_u(m, 1, k, j, i),
+                         adm.beta_u(m, 2, k, j, i), volform * Fx, volform * Fy,
+                         volform * Fz, F_d);
+                apply_floor(g_uu, E, F_d, params_);
+
+                u0_(m, M1_E_IDX, k, j, is - i - 1) = E;
+                u0_(m, M1_FX_IDX, k, j, is - i - 1) = F_d(1);
+                u0_(m, M1_FY_IDX, k, j, is - i - 1) = F_d(2);
+                u0_(m, M1_FZ_IDX, k, j, is - i - 1) = F_d(3);
+              }
+            }
+            break;
+          default:
+            break;
+        }
+      });
+}
+
 }  // namespace radiationm1
