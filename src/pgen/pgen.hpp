f1b43f059bfaa97a7a51c143438223402629f6e3
Einsqe <2229527732@qq.com>
2025-05-08 23:16:41 -0400

    add radiation m1 single zone test pgen v0

--- src/pgen/pgen.hpp
+++ src/pgen/pgen.hpp
@@ -81,6 +81,7 @@ class ProblemGenerator {
   void RadiationM1SphereTest(ParameterInput *pin, const bool restart);
   void RadiationM1DiffusionTest(ParameterInput *pin, const bool restart);
   void RadiationM1VelocityJumpTest(ParameterInput *pin, const bool restart);
+  void RadiationM1SingleZoneTest(ParameterInput *pin, const bool restart);
  private:
   Mesh *pmy_mesh_;
 };
