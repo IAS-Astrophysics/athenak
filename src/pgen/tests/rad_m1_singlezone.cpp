902a7f815c0e7077d7d4ad7ab0832ebc42e8f43e
Einsqe <2229527732@qq.com>
2025-05-08 23:16:41 -0400

    add radiation m1 single zone test pgen v0

---
+++ src/pgen/tests/rad_m1_singlezone.cpp
@@ -0,0 +1,75 @@
+//========================================================================================
+// AthenaXXX astrophysical plasma code
+// Copyright(C) 2020 James M. Stone <jmstone@ias.edu> and the Athena code team
+// Licensed under the 3-clause BSD License (the "LICENSE")
+//========================================================================================
+//! \file rad_m1_latticetest.cpp
+//  \brief Radiation M1 single zone equilibratioon test for grey M1 + hydro
+
+// C++ headers
+
+// Athena++ headers
+#include "athena.hpp"
+#include "coordinates/adm.hpp"
+#include "mesh/mesh.hpp"
+#include "parameter_input.hpp"
+#include "pgen/pgen.hpp"
+#include "radiation_m1/radiation_m1.hpp"
+#include "radiation_m1/radiation_m1_helpers.hpp"
+#include "eos/eos.hpp"
+#include "hydro/hydro.hpp"
+#include "driver/driver.hpp"
+
+//----------------------------------------------------------------------------------------
+//! \fn void MeshBlock::UserProblem(ParameterInput *pin)
+//  \brief Sets initial conditions for radiation M1 single zone equilibratioon test
+
+void ProblemGenerator::RadiationM1SingleZoneTest(ParameterInput *pin,
+                                              const bool restart) {
+  if (restart) return;
+
+  MeshBlockPack *pmbp = pmy_mesh_->pmb_pack;
+
+  pmbp->pradm1->toy_opacity_fn = LatticeOpacities;
+
+  // capture variables for kernel
+  auto &indcs = pmy_mesh_->mb_indcs;
+  int &ng = indcs.ng;
+  int n1 = indcs.nx1 + 2*ng;
+  int n2 = (indcs.nx2 > 1)? (indcs.nx2 + 2*ng) : 1;
+  int n3 = (indcs.nx3 > 1)? (indcs.nx3 + 2*ng) : 1;
+  int &is = indcs.is;
+  int &js = indcs.js;
+  int &ks = indcs.ks;
+  auto &size = pmbp->pmb->mb_size;
+  auto &coord = pmbp->pcoord->coord_data;
+  int nmb1 = (pmbp->nmb_thispack-1);
+  // get problem parameters
+  Real rho = pin->GetReal("problem", "rho");
+  Real temp = pin->GetReal("problem", "temp");
+  Real vx = pin->GetReal("problem", "vx");
+  Real vy = pin->GetReal("problem", "vy");
+  Real vz = pin->GetReal("problem", "vz");
+  Real ye = pin->GetReal("problem", "Y_e");
+
+  // set primitive variables
+  auto &w0 = pmbp->phydro->w0;
+  par_for("pgen_shadow1",DevExeSpace(),0,nmb1,0,(n3-1),0,(n2-1),0,(n1-1),
+  KOKKOS_LAMBDA(int m, int k, int j, int i) {
+    w0(m,IDN,k,j,i) = rho;
+    w0(m,IVX,k,j,i) = vx
+    w0(m,IVY,k,j,i) = vy;
+    w0(m,IVZ,k,j,i) = vz;
+    w0(m,IEN,k,j,i) = temp;
+    w0(m,IYF,k,j,i) = ye;
+  });
+
+  // Convert primitives to conserved
+  auto &u0 = pmbp->phydro->u0;
+  pmbp->phydro->peos->PrimToCons(w0, u0, 0, (n1-1), 0, (n2-1), 0, (n3-1));
+  
+  // initialize ADM variables
+  if (pmbp->padm != nullptr) {
+    pmbp->padm->SetADMVariables(pmbp);
+  }
+}
