af75fa5583cd58d5fcd5165e07560f0b0c5b533b
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2025-05-15 20:49:11 -0400

    adding neutrino density funcs

--- src/radiation_m1/radiation_m1_calc_opacities_nurates.cpp
+++ src/radiation_m1/radiation_m1_calc_opacities_nurates.cpp
@@ -45,14 +45,16 @@ TaskStatus RadiationM1::CalcOpacityNurates_(Driver *pdrive, int stage) {
   int &is = indcs.is, &ie = indcs.ie;
   int &js = indcs.js, &je = indcs.je;
   int &ks = indcs.ks, &ke = indcs.ke;
-  int &ng = indcs.ng;
 
   auto nmb1 = pmy_pack->nmb_thispack - 1;
   auto &mbsize = pmy_pack->pmb->mb_size;
   auto &nspecies_ = nspecies;
   auto nvars_ = nvars;
 
-  auto &params_ = params;
+  auto &adm = pmy_pack->padm->adm;
+  auto &radiation_mask_ = radiation_mask;
+
+  auto &m1_params_ = params;
   auto &nurates_params_ = nurates_params;
 
   auto &eta_0_ = eta_0;
@@ -61,20 +63,14 @@ TaskStatus RadiationM1::CalcOpacityNurates_(Driver *pdrive, int stage) {
   auto &abs_1_ = abs_1;
   auto &scat_1_ = scat_1;
 
-  auto &toy_opacity_fn_ = toy_opacity_fn;
-
-  auto &radiation_mask_ = radiation_mask;
   auto &u0_ = u0;
   auto &w0_ = pmy_pack->pmhd->w0;
   auto &chi_ = chi;
   auto &u_mu_ = u_mu;
 
-  auto &adm = pmy_pack->padm->adm;
-
-  const Real nux_weight = (nspecies == 3 ? 1.0 : 0.5);
-
   Real beta[2] = {0.5, 1.};
   Real beta_dt = (beta[stage - 1]) * (pmy_pack->pmesh->dt);
+  const Real nux_weight = (nspecies == 3 ? 1.0 : 0.5);
 
   Primitive::EOS<EOSPolicy, ErrorPolicy> &eos =
       static_cast<dyngr::DynGRMHDPS<EOSPolicy, ErrorPolicy> *>(pmy_pack->pdyngr)
@@ -133,28 +129,31 @@ TaskStatus RadiationM1::CalcOpacityNurates_(Driver *pdrive, int stage) {
           AthenaPointTensor<Real, TensorSymm::NONE, 4, 2> proj_ud{};
 
           Real w_lorentz{};
-          if (nspecies_ > 1) {
-            w_lorentz = Kokkos::sqrt(1. + w0_(m, IVX, k, j, i) * w0_(m, IVX, k, j, i) +
-                                     w0_(m, IVY, k, j, i) * w0_(m, IVY, k, j, i) +
-                                     w0_(m, IVZ, k, j, i) * w0_(m, IVZ, k, j, i));
-            pack_u_u(w_lorentz / adm.alpha(m, k, j, i),
-                     w0_(m, IVX, k, j, i) -
-                         w_lorentz * adm.beta_u(m, 0, k, j, i) / adm.alpha(m, k, j, i),
-                     w0_(m, IVY, k, j, i) -
-                         w_lorentz * adm.beta_u(m, 1, k, j, i) / adm.alpha(m, k, j, i),
-                     w0_(m, IVZ, k, j, i) -
-                         w_lorentz * adm.beta_u(m, 2, k, j, i) / adm.alpha(m, k, j, i),
-                     u_u);
-          } else {
-            pack_u_u(u_mu_(m, 0, k, j, i), u_mu_(m, 1, k, j, i), u_mu_(m, 2, k, j, i),
-                     u_mu_(m, 3, k, j, i), u_u);
-          }
+          w_lorentz = Kokkos::sqrt(1. + w0_(m, IVX, k, j, i) * w0_(m, IVX, k, j, i) +
+                                   w0_(m, IVY, k, j, i) * w0_(m, IVY, k, j, i) +
+                                   w0_(m, IVZ, k, j, i) * w0_(m, IVZ, k, j, i));
+          pack_u_u(w_lorentz / adm.alpha(m, k, j, i),
+                   w0_(m, IVX, k, j, i) -
+                       w_lorentz * adm.beta_u(m, 0, k, j, i) / adm.alpha(m, k, j, i),
+                   w0_(m, IVY, k, j, i) -
+                       w_lorentz * adm.beta_u(m, 1, k, j, i) / adm.alpha(m, k, j, i),
+                   w0_(m, IVZ, k, j, i) -
+                       w_lorentz * adm.beta_u(m, 2, k, j, i) / adm.alpha(m, k, j, i),
+                   u_u);
           pack_v_u(u_u(0), u_u(1), u_u(2), u_u(3), adm.alpha(m, k, j, i),
                    adm.beta_u(m, 0, k, j, i), adm.beta_u(m, 1, k, j, i),
                    adm.beta_u(m, 2, k, j, i), v_u);
           tensor_contract(g_dd, u_u, u_d);
           tensor_contract(g_dd, v_u, v_d);
           calc_proj(u_d, u_u, proj_ud);
+
+          Real n_nue = u0_(m, CombinedIdx(id_nue, M1_N_IDX, nvars_), k, j, i);
+          Real n_nua = u0_(m, CombinedIdx(id_anue, M1_N_IDX, nvars_), k, j, i);
+          Real n_nux = u0_(m, CombinedIdx(id_nux, M1_N_IDX, nvars_), k, j, i);
+          Real en_nue = u0_(m, CombinedIdx(id_nue, M1_E_IDX, nvars_), k, j, i);
+          Real en_nua = u0_(m, CombinedIdx(id_anue, M1_E_IDX, nvars_), k, j, i);
+          Real en_nux = u0_(m, CombinedIdx(id_nux, M1_E_IDX, nvars_), k, j, i);
+          Real n_nu[6] = {n_nue, n_nua, n_nux, n_nux, 0, 0};
 
           Real J[4]{};
           Real rnnu[4]{};
@@ -168,14 +167,14 @@ TaskStatus RadiationM1::CalcOpacityNurates_(Driver *pdrive, int stage) {
             const Real E = u0_(m, CombinedIdx(nuidx, M1_E_IDX, nvars_), k, j, i);
             AthenaPointTensor<Real, TensorSymm::SYM2, 4, 2> P_dd{};
             apply_closure(g_dd, g_uu, n_d, w_lorentz, u_u, v_d, proj_ud, E, F_d,
-                          chi_(m, nuidx, k, j, i), P_dd, params_);
+                          chi_(m, nuidx, k, j, i), P_dd, m1_params_);
 
             // compute radiation quantities in fluid frame
             AthenaPointTensor<Real, TensorSymm::SYM2, 4, 2> T_dd{};
             assemble_rT(n_d, E, F_d, P_dd, T_dd);
             J[nuidx] = calc_J_from_rT(T_dd, u_u);
 
-            Real Gamma = compute_Gamma(w_lorentz, v_u, J[nuidx], E, F_d, params_);
+            Real Gamma = compute_Gamma(w_lorentz, v_u, J[nuidx], E, F_d, m1_params_);
             rnnu[nuidx] = u0_(m, CombinedIdx(nuidx, M1_N_IDX, nvars_), k, j, i) / Gamma;
           }
 
@@ -192,7 +191,7 @@ TaskStatus RadiationM1::CalcOpacityNurates_(Driver *pdrive, int stage) {
           Real mu_p = mu_b + mu_q;
           Real mu_e = mu_le - mu_q;
 
-          // Local neutrino quantities (undesitized)
+          // local undensitized neutrino quantities
           Real nudens_0[4], nudens_1[4], chi_loc[4];
           for (int nuidx = 0; nuidx < nspecies_; ++nuidx) {
             nudens_0[nuidx] = rnnu[nuidx] / volform;
@@ -200,11 +199,12 @@ TaskStatus RadiationM1::CalcOpacityNurates_(Driver *pdrive, int stage) {
             chi_loc[nuidx] = chi_(m, nuidx, k, j, i);
           }
 
-          // Get emissivities and opacities
+          // get emissivities and opacities
           Real eta_0_loc[4]{}, eta_1_loc[4]{};
           Real abs_0_loc[4]{}, abs_1_loc[4]{};
           Real scat_0_loc[4]{}, scat_1_loc[4]{};
 
+          //@TODO: check the signature of this function
           // Note: everything sent and received are in code units
           bns_nurates(nb, T, Y, mu_n, mu_p, mu_e, nudens_0[0], nudens_1[0], chi_loc[0],
                       nudens_0[1], nudens_1[1], chi_loc[1], nudens_0[2], nudens_1[2],
@@ -216,138 +216,143 @@ TaskStatus RadiationM1::CalcOpacityNurates_(Driver *pdrive, int stage) {
                       scat_0_loc[1], scat_0_loc[2], scat_0_loc[3], scat_1_loc[0],
                       scat_1_loc[1], scat_1_loc[2], scat_1_loc[3], nurates_params_);
 
-          assert(isfinite(scat_0_loc[0]));
-          assert(isfinite(scat_0_loc[1]));
-          assert(isfinite(scat_0_loc[2]));
-          assert(isfinite(scat_1_loc[0]));
-          assert(isfinite(scat_1_loc[1]));
-          assert(isfinite(scat_1_loc[2]));
+          assert(isfinite(eta_0_loc[0]));
+          assert(isfinite(eta_0_loc[1]));
+          assert(isfinite(eta_0_loc[2]));
+
+          assert(isfinite(eta_1_loc[0]));
+          assert(isfinite(eta_1_loc[1]));
+          assert(isfinite(eta_1_loc[2]));
 
           assert(isfinite(abs_0_loc[0]));
           assert(isfinite(abs_0_loc[1]));
           assert(isfinite(abs_0_loc[2]));
+
           assert(isfinite(abs_1_loc[0]));
           assert(isfinite(abs_1_loc[1]));
           assert(isfinite(abs_1_loc[2]));
-          abs_0_loc[3] = abs_0_loc[2];
-          abs_1_loc[3] = abs_1_loc[2];
 
-          assert(isfinite(eta_0_loc[0]));
-          assert(isfinite(eta_0_loc[1]));
-          assert(isfinite(eta_0_loc[2]));
-          assert(isfinite(eta_1_loc[0]));
-          assert(isfinite(eta_1_loc[1]));
-          assert(isfinite(eta_1_loc[2]));
-          eta_0_loc[2] = nux_weight * eta_0_loc[2];
-          eta_1_loc[2] = nux_weight * eta_1_loc[2];
-          eta_0_loc[3] = eta_0_loc[2];
-          eta_1_loc[3] = eta_1_loc[2];
+          assert(isfinite(scat_0_loc[0]));
+          assert(isfinite(scat_0_loc[1]));
+          assert(isfinite(scat_0_loc[2]));
 
-          // An effective optical depth used to decide whether to compute
-          // the black body function for neutrinos assuming neutrino trapping
-          // or at a fixed temperature and Ye
-          Real nudens_0_trap[4], nudens_1_trap[4];
-          Real nudens_0_thin[4], nudens_1_thin[4];
-          Real tau = Kokkos::min(Kokkos::sqrt(abs_1_loc[0] * scat_1_loc[0]),
-                                 Kokkos::sqrt(abs_1_loc[1] * scat_1_loc[1])) *
-                     beta_dt;
+          assert(isfinite(scat_1_loc[0]));
+          assert(isfinite(scat_1_loc[1]));
+          assert(isfinite(scat_1_loc[2]));
 
-          // Compute the neutrino black body function assuming trapped neutrinos
-          if (nurates_params_.opacity_tau_trap >= 0 &&
-              tau > nurates_params_.opacity_tau_trap) {
-            Real temperature_trap{}, Ye_trap{};
+          Real tau{};
+          Real nudens_0_trap[4]{}, nudens_1_trap[4]{};
+          Real nudens_0_thin[4]{}, nudens_1_thin[4]{};
 
-            // Compute local neutrino densities (undensitized)
-            Real nudens_0[3] = {rnnu[0] / volform, rnnu[1] / volform, rnnu[2] / volform};
-            Real nudens_1[3] = {J[0] / volform, J[1] / volform, J[2] / volform};
+          if (nurates_params_.use_kirchhoff_law) {
+            // effective optical depth to decide whether to compute black body function
+            // for neutrinos assuming neutrino tapping or at fixed temperature and Ye
+            Real tau =
+                Kokkos::min(Kokkos::sqrt(abs_1_loc[0] * (abs_1_loc[0] + scat_1_loc[0])),
+                            Kokkos::sqrt(abs_1_loc[1] * (abs_1_loc[1] + scat_1_loc[1]))) *
+                beta_dt;
 
-            if (nspecies_ == 4) {
-              nudens_0[2] += rnnu[3] / volform;
-              nudens_1[2] += J[3] / volform;
+            // compute neutrino black body function assuming trapped neutrinos
+            if (nurates_params_.opacity_tau_trap >= 0 &&
+                tau > nurates_params_.opacity_tau_trap) {
+              Real Yl[6]{};
+              eos.GetLeptonFractions(0, Y, &n_nu, &Yl);
+              bool ierr = eos.GetBetaEquilibriumTrapped();
+              if (!ierr) {
+                ierr = eos.GetBetaEquilibriumTrapped();
+              }
+
+              assert(Kokkos::isfinite(nudens_0_trap[0]));
+              assert(Kokkos::isfinite(nudens_0_trap[1]));
+              assert(Kokkos::isfinite(nudens_0_trap[2]));
+              assert(Kokkos::isfinite(nudens_1_trap[0]));
+              assert(Kokkos::isfinite(nudens_1_trap[1]));
+              assert(Kokkos::isfinite(nudens_1_trap[2]));
+              nudens_0_trap[2] *= 0.5;
+              nudens_1_trap[2] *= 0.5;
+              nudens_0_trap[3] = nudens_0_trap[2];
+              nudens_1_trap[3] = nudens_1_trap[2];
             }
 
-            // @TODO: call weak equilibrium
-            bool ierr{};
+            // compute neutrino black body function assuming fixed temperature and Ye
+            NeutrinoDens(mu_n, mu_p, mu_e, nb, T, n_nue, n_nua, n_nux, en_nue, en_nua,
+                         en_nux, nurates_params_);
 
-            if (ierr) {
-            }
-            assert(Kokkos::isfinite(nudens_0_trap[0]));
-            assert(Kokkos::isfinite(nudens_0_trap[1]));
-            assert(Kokkos::isfinite(nudens_0_trap[2]));
-            assert(Kokkos::isfinite(nudens_1_trap[0]));
-            assert(Kokkos::isfinite(nudens_1_trap[1]));
-            assert(Kokkos::isfinite(nudens_1_trap[2]));
-            nudens_0_trap[2] = nux_weight * nudens_0_trap[2];
-            nudens_1_trap[2] = nux_weight * nudens_1_trap[2];
-            nudens_0_trap[3] = nudens_0_trap[2];
-            nudens_1_trap[3] = nudens_1_trap[2];
+            nudens_0_thin[2] *= 0.5;
+            nudens_1_thin[2] *= 0.5;
+            nudens_0_thin[3] = nudens_0_thin[2];
+            nudens_1_thin[3] = nudens_1_thin[2];
           }
 
-          // Compute the neutrino black body function assuming fixed temperature and Y_e
-          // NeutrinoDens(&eos, w0_(m,IDN,k,j,i), Real temp,
-          //             w0_(m,ID,k,j,i), Real &n_nue, Real &n_nua, Real &n_nux, Real
-          //             &en_nue, Real &en_nua, Real &en_nux, nurates_params_);
+          for (int nuidx = 0; nuidx < nspecies_; ++nuidx) {
+            eta_0(m, nuidx, k, j, i) = eta_0_loc[nuidx];
+            eta_1(m, nuidx, k, j, i) = eta_1_loc[nuidx];
+            abs_0(m, nuidx, k, j, i) = abs_0_loc[nuidx];
+            abs_1(m, nuidx, k, j, i) = abs_1_loc[nuidx];
+            scat_1(m, nuidx, k, j, i) = scat_1_loc[nuidx];
 
-          nudens_0_thin[2] = nux_weight * nudens_0_thin[2];
-          nudens_1_thin[2] = nux_weight * nudens_1_thin[2];
-          nudens_0_thin[3] = nudens_0_thin[2];
-          nudens_1_thin[3] = nudens_1_thin[2];
+            Real my_nudens_0{}, my_nudens_1{}, corr_fac{};
+            if (nurates_params_.use_kirchhoff_law ||
+                nurates_params_.use_equilibrium_distribution) {
+              // combine optically thin and optically thick limits
+              if (nurates_params_.opacity_tau_trap < 0 ||
+                  tau <= nurates_params_.opacity_tau_trap) {
+                my_nudens_0 = nudens_0_thin[is];
+                my_nudens_1 = nudens_1_thin[is];
+              } else if (tau > nurates_params_.opacity_tau_trap +
+                                   nurates_params_.opacity_tau_delta) {
+                my_nudens_0 = nudens_0_trap[is];
+                my_nudens_1 = nudens_1_trap[is];
+              } else {
+                Real const lam = (tau - nurates_params_.opacity_tau_trap) /
+                                 nurates_params_.opacity_tau_delta;
+                my_nudens_0 = lam * nudens_0_trap[is] + (1 - lam) * nudens_0_thin[is];
+                my_nudens_1 = lam * nudens_1_trap[is] + (1 - lam) * nudens_1_thin[is];
+              }
 
-          // Correct cross-sections for incoming neutrino energy
-          for (int ig = 0; ig < nspecies_; ++ig) {
-            // Set the neutrino black body function
-            Real nudens_0, nudens_1;
-            if (nurates_params_.opacity_tau_trap < 0 ||
-                tau <= nurates_params_.opacity_tau_trap) {
-              nudens_0 = nudens_0_thin[ig];
-              nudens_1 = nudens_1_thin[ig];
-            } else if (tau > nurates_params_.opacity_tau_trap +
-                                 nurates_params_.opacity_tau_delta) {
-              nudens_0 = nudens_0_trap[ig];
-              nudens_1 = nudens_1_trap[ig];
-            } else {
-              const Real lam = (tau - nurates_params_.opacity_tau_trap) /
-                               nurates_params_.opacity_tau_delta;
-              nudens_0 = lam * nudens_0_trap[ig] + (1 - lam) * nudens_0_thin[ig];
-              nudens_1 = lam * nudens_1_trap[ig] + (1 - lam) * nudens_1_thin[ig];
-            }
-
-            // Correct absorption opacities for non-LTE effects
-            // (kappa ~ E_nu^2)
-            Real corr_fac = 1.0;
-            corr_fac = (J[ig] / rnnu[ig]) * (nudens_0 / nudens_1);
-            if (!isfinite(corr_fac)) {
+              // Correction factor for absorption opacities for non-LTE effects
+              // (kappa ~ E_nu^2)
               corr_fac = 1.0;
+              corr_fac = (J[nuidx] / rnnu[nuidx]) * (my_nudens_0 / my_nudens_1);
+              if (!Kokkos::isfinite(corr_fac)) {
+                corr_fac = 1.0;
+              }
+              corr_fac *= corr_fac;
+              corr_fac = Kokkos::max(
+                  1.0 / nurates_params_.opacity_corr_fac_max,
+                  Kokkos::min(corr_fac, nurates_params_.opacity_corr_fac_max));
             }
-            corr_fac *= corr_fac;
-            corr_fac =
-                Kokkos::max(1.0 / nurates_params_.opacity_corr_fac_max,
-                            Kokkos::min(corr_fac, nurates_params_.opacity_corr_fac_max));
 
-            // Extract scattering opacity
-            scat_1_(m, 0, k, j, i) = corr_fac * (scat_1_loc[ig] - abs_1_loc[ig]);
+            if (nurates_params_.use_equilibrium_distribution) {
+              eta_0(m, nuidx, k, j, i) *= corr_fac;
+              eta_1(m, nuidx, k, j, i) *= corr_fac;
+              abs_0(m, nuidx, k, j, i) *= corr_fac;
+              abs_1(m, nuidx, k, j, i) *= corr_fac;
+              scat_1(m, nuidx, k, j, i) *= corr_fac;
+            }
 
-            // Enforce Kirchhoff's laws.
-            // . For the heavy lepton neutrinos this is implemented by
-            //   changing the opacities.
-            // . For the electron type neutrinos this is implemented by
-            //   changing the emissivities.
-            // It would be better to have emissivities and absorptivities
-            // that satisfy Kirchhoff's law.
-            if (ig == 2 || ig == 3) {
-              eta_0_(m, ig, k, j, i) = eta_0_loc[ig];
-              eta_1_(m, ig, k, j, i) = eta_1_loc[ig];
-              abs_0_(m, ig, k, j, i) =
-                  (nudens_0 > params_.rad_N_floor ? eta_0_(m, ig, k, j, i) / nudens_0
-                                                  : 0);
-              abs_1_(m, 0, k, j, i) =
-                  (nudens_1 > params_.rad_E_floor ? eta_1_(m, ig, k, j, i) / nudens_1
-                                                  : 0);
+            if (nurates_params_.use_kirchhoff_law) {
+              // enforce Kirchhoff's laws.
+              if (is == 0 || is == 1) {
+                eta_0(m, nuidx, k, j, i) = abs_0(m, nuidx, k, j, i) * my_nudens_0;
+                eta_1(m, nuidx, k, j, i) = abs_1(m, nuidx, k, j, i) * my_nudens_1;
+              } else {
+                abs_0(m, nuidx, k, j, i) = (my_nudens_0 > m1_params_.rad_N_floor
+                                                ? eta_0(m, nuidx, k, j, i) / my_nudens_0
+                                                : 0);
+                abs_1(m, nuidx, k, j, i) = (my_nudens_1 > m1_params_.rad_E_floor
+                                                ? eta_1(m, nuidx, k, j, i) / my_nudens_1
+                                                : 0);
+              }
             } else {
-              abs_0_(m, ig, k, j, i) = corr_fac * abs_0_loc[ig];
-              abs_1_(m, ig, k, j, i) = corr_fac * abs_1_loc[ig];
-              eta_0_(m, ig, k, j, i) = abs_0_(m, ig, k, j, i) * nudens_0;
-              eta_1_(m, ig, k, j, i) = abs_1_(m, ig, k, j, i) * nudens_1;
+              if (nuidx == 0 || nuidx == 1) {
+                if (nudens_0[nuidx] < m1_params_.rad_N_floor) {
+                  abs_0(m, nuidx, k, j, i) = 0.;
+                }
+                if (nudens_1[nuidx] < m1_params_.rad_E_floor) {
+                  abs_1(m, nuidx, k, j, i) = 0.;
+                }
+              }
             }
           }
         }
