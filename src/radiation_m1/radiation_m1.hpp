91c1b50e722a75b4a38463590fb3f5f7ab8bc360
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2025-05-25 14:30:26 -0400

    adding several minor changes to M1, cleanup.

--- src/radiation_m1/radiation_m1.hpp
+++ src/radiation_m1/radiation_m1.hpp
@@ -19,11 +19,11 @@
 #include "radiation_m1/radiation_m1_params.hpp"
 #include "radiation_m1/radiation_m1_roots_fns.hpp"
 #include "radiation_m1/radiation_m1_toy.hpp"
-
 #include "tasklist/task_list.hpp"
+
 #ifdef ENABLE_NURATES
-#include "radiation_m1/radiation_m1_nurates.hpp"
 #include "bns_nurates/include/bns_nurates.hpp"
+#include "radiation_m1/radiation_m1_nurates.hpp"
 #endif
 
 namespace radiationm1 {
@@ -32,22 +32,22 @@ namespace radiationm1 {
 //! \struct RadiationTaskIDs
 //  \brief container to hold TaskIDs of all radiation M1 tasks
 struct RadiationM1TaskIDs {
-  TaskID irecv;
-  TaskID copyu;
-  TaskID closure;
-  TaskID flux;
-  TaskID sendf;
-  TaskID recvf;
-  TaskID rkupdt;
-  TaskID mattersrc;
-  TaskID restu;
-  TaskID sendu;
-  TaskID recvu;
-  TaskID bcs;
-  TaskID prol;
-  TaskID newdt;
-  TaskID csend;
-  TaskID crecv;
+  TaskID M1_irecv;
+  TaskID M1_copyu;
+  TaskID M1_closure;
+  TaskID M1_flux;
+  TaskID M1_sendf;
+  TaskID M1_recvf;
+  TaskID M1_rkupdt;
+  TaskID M1_mattersrc;
+  TaskID M1_restu;
+  TaskID M1_sendu;
+  TaskID M1_recvu;
+  TaskID M1_bcs;
+  TaskID M1_prol;
+  TaskID M1_newdt;
+  TaskID M1_csend;
+  TaskID M1_crecv;
 };
 
 //----------------------------------------------------------------------------------------
@@ -61,10 +61,7 @@ class RadiationM1 {
   BrentFunctor BrentFunc;        // function to minimize for closure
   HybridsjFunctor HybridsjFunc;  // function to minimize for multiroots solver
 
-#ifdef ENABLE_NURATES
-  NuratesParams nurates_params;  // pars for nurates (choice of reactions, quadratures)
-#endif
-  ToyOpacity toy_opacity_fn;  // use only if toy opacities enabled
+  ToyOpacity toy_opacity_fn{};  // use only if toy opacities enabled
 
   int nvars;                   // no. of evolved variables per species
   int nspecies;                // no. of species
@@ -77,19 +74,23 @@ class RadiationM1 {
   DvceArray4D<bool> radiation_mask;  // radiation mask
   DvceArray5D<Real> u1;              // evolved variables at intermediate step
   DvceFaceFld5D<Real> uflx;          // fluxes of evo. quantities on cell faces
-  DvceArray5D<Real> u_mu_data;       // fluid velocity
   DvceArray5D<Real> eta_0;           // number emissivity coefficient
   DvceArray5D<Real> abs_0;           // number absorptivity coefficient
   DvceArray5D<Real> eta_1;           // energy emissivity coefficient
   DvceArray5D<Real> abs_1;           // energy absorptivity coefficient
   DvceArray5D<Real> scat_1;          // energy scattering coefficient
-  AthenaTensor<Real, TensorSymm::NONE, 4, 1> u_mu;  // fluid 4-velocity
 
   MeshBoundaryValuesCC* pbval_u;  // Communication buffers and functions for u
   RadiationM1TaskIDs id;          // container to hold names of TaskIDs
   Real dtnew{};
 
-  RadiationM1Beam rad_m1_beam;
+  // conditional quantities
+  DvceArray5D<Real> u_mu_data;                      // fluid velocity (when mhd is off)
+  AthenaTensor<Real, TensorSymm::NONE, 4, 1> u_mu;  // fluid 4-velocity (when mhd is off)
+  RadiationM1Beam rad_m1_beam;  // beam ID values (only needed when beams on)
+#ifdef ENABLE_NURATES
+  NuratesParams nurates_params{};  // pars for nurates (choice of reactions, quadratures)
+#endif
 
   // functions...
   void AssembleRadiationM1Tasks(std::map<std::string, std::shared_ptr<TaskList>> tl);
@@ -114,10 +115,12 @@ class RadiationM1 {
   TaskStatus ClearSend(Driver* d, int stage);
   TaskStatus ClearRecv(Driver* d, int stage);  // also in Driver::Initialize
 
-  template<class EOSPolicy, class ErrorPolicy>
+  // eos related quantites (only when mhd is on)
+  template <class EOSPolicy, class ErrorPolicy>
   TaskStatus CalcOpacityNurates_(Driver* pdrive, int stage);
   template <class EOSPolicy, class ErrorPolicy>
   TaskStatus TimeUpdate_(Driver* d, int stage);
+
  private:
   MeshBlockPack* pmy_pack;  // ptr to MeshBlockPack
 };
