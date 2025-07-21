7962400c84c50f3b552b15258e5d55a753653b80
Jacob Fields <fields.jacobm@gmail.com>
2025-07-21 09:57:09 -0400

    Flush and abort on table load failure.

--- src/eos/primitive-solver/eos_hybrid.cpp
+++ src/eos/primitive-solver/eos_hybrid.cpp
@@ -30,8 +30,8 @@ void EOSHybrid<LogPolicy>::ReadTableFromFile(std::string fname) {
     TableReader::Table table;
     auto read_result = table.ReadTable(fname);
     if (read_result.error != TableReader::ReadResult::SUCCESS) {
-      std::cout << "Table could not be read.\n";
-      assert (false);
+      std::cout << "Table could not be read.\n" << std::flush;
+      abort();
     }
     // Make sure table has correct dimentions
     assert(table.GetNDimensions()==1);
