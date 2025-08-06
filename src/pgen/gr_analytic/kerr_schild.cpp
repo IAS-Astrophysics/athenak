0605b01edfc365c5e1e379c3b3685d6d0bc05e91
Hengrui Zhu <hz0693@della-gpu.princeton.edu>
2025-08-06 12:38:26 -0400

    add pgen for testing

--- src/pgen/gr_analytic/kerr_schild.cpp
+++ src/pgen/gr_analytic/kerr_schild.cpp
@@ -27,10 +27,8 @@
 #include "coordinates/cartesian_ks.hpp"
 
 
-void KerrSchild(MeshBlockPack *pmbp, ParameterInput *pin);
+void ADMKerrSchild(MeshBlockPack *pmbp, ParameterInput *pin);
 void RefinementCondition(MeshBlockPack* pmbp);
-KOKKOS_INLINE_FUNCTION
-bool invertSymmetricMatrix4x4(const double g_dd[4][4], double g_dd_inv[4][4]);
 
 //----------------------------------------------------------------------------------------
 //! \fn ProblemGenerator::UserProblem_()
@@ -42,12 +40,13 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
 
   if (pmbp->pz4c == nullptr) {
     std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__ << std::endl
-              << "One Puncture test can only be run in Z4c, but no <z4c> block "
+              << "Kerr-Schild test can only be run in Z4c, but no <z4c> block "
               << "in input file" << std::endl;
     exit(EXIT_FAILURE);
   }
 
-  KerrSchild(pmbp, pin);
+  ADMKerrSchild(pmbp, pin);
+  // pmbp->pz4c->GaugePreCollapsedLapse(pmbp, pin);
   switch (indcs.ng) {
     case 2: pmbp->pz4c->ADMToZ4c<2>(pmbp, pin);
             break;
@@ -57,7 +56,6 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
             break;
   }
   pmbp->pz4c->Z4cToADM(pmbp);
-  pmbp->pz4c->GaugePreCollapsedLapse(pmbp, pin);
   switch (indcs.ng) {
     case 2: pmbp->pz4c->ADMConstraints<2>(pmbp);
             break;
@@ -66,35 +64,42 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
     case 4: pmbp->pz4c->ADMConstraints<4>(pmbp);
             break;
   }
-  std::cout<<"Kerr Schild initialized."<<std::endl;
+  std::cout<<"Kerr-Schild initialized."<<std::endl;
+
   return;
 }
 
+// inlined spherical Kerr-Schild r evaluated at CKS x1, x2, x3
+KOKKOS_INLINE_FUNCTION
+Real KSRX(const Real x1, const Real x2, const Real x3, const Real a) {
+  Real rad = sqrt(SQR(x1) + SQR(x2) + SQR(x3));
+  return sqrt((SQR(rad)-SQR(a)+sqrt(SQR(SQR(rad)-SQR(a))+4.0*SQR(a)*SQR(x3)))/2.0);
+}
+
+
 //----------------------------------------------------------------------------------------
-//! \fn void ADMOnePuncture(MeshBlockPack *pmbp, ParameterInput *pin)
-//! \brief Initialize ADM vars to single puncture (no spin)
+//! \fn void ADMKerrSchild(MeshBlockPack *pmbp, ParameterInput *pin)
+//! \brief Initialize ADM vars to Kerr-Schild solution
+void ADMKerrSchild(MeshBlockPack *pmbp, ParameterInput *pin) {
+  // read bh spin
+  Real a = pin->GetOrAddReal("problem", "bh_spin", 0.);
+  Real excise_ratio = pin->GetOrAddReal("problem", "excise_ratio", 0.5);
 
-void KerrSchild(MeshBlockPack *pmbp, ParameterInput *pin) {
-  Real ADM_mass = pin->GetOrAddReal("problem", "punc_ADM_mass", 1.);
-  Real center_x1 = pin->GetOrAddReal("problem", "punc_center_x1", 0.);
-  Real center_x2 = pin->GetOrAddReal("problem", "punc_center_x2", 0.);
-  Real center_x3 = pin->GetOrAddReal("problem", "punc_center_x3", 0.);
-  Real a = pin->GetOrAddReal("problem", "punc_spin", 0.);
-  bool minkowski = false;
-
-  // capture variables for the kernel
-  auto &indcs = pmbp->pmesh->mb_indcs;
-  auto &size = pmbp->pmb->mb_size;
-  int &is = indcs.is; int &ie = indcs.ie;
-  int &js = indcs.js; int &je = indcs.je;
-  int &ks = indcs.ks; int &ke = indcs.ke;
-  int nmb = pmbp->nmb_thispack;
+  // set transition radius
+  // transit from Kerr-Schild to Minkolsky from 2rH/3 to rH/1
+  Real rH = 1 + sqrt(1 - SQR(a));
+  Real rexci = excise_ratio*rH;
   auto &adm = pmbp->padm->adm;
+  auto &size = pmbp->pmb->mb_size;
+  auto &indcs = pmbp->pmesh->mb_indcs;
   int &ng = indcs.ng;
+  int is = indcs.is, js = indcs.js, ks = indcs.ks;
+  int ie = indcs.ie, je = indcs.je, ke = indcs.ke;
+  int nmb = pmbp->nmb_thispack;
   int n1 = indcs.nx1 + 2*ng;
-  int n2 = indcs.nx2 + 2*ng;
-  int n3 = indcs.nx3 + 2*ng;
-  par_for("pgen_adm_vars", DevExeSpace(), 0,nmb-1,0,(n3-1),0,(n2-1),0,(n1-1),
+  int n2 = (indcs.nx2 > 1) ? (indcs.nx2 + 2*ng) : 1;
+  int n3 = (indcs.nx3 > 1) ? (indcs.nx3 + 2*ng) : 1;
+  par_for("update_adm_vars", DevExeSpace(), 0,nmb-1,0,(n3-1),0,(n2-1),0,(n1-1),
   KOKKOS_LAMBDA(int m, int k, int j, int i) {
     Real &x1min = size.d_view(m).x1min;
     Real &x1max = size.d_view(m).x1max;
@@ -108,7 +113,9 @@ void KerrSchild(MeshBlockPack *pmbp, ParameterInput *pin) {
     Real &x3max = size.d_view(m).x3max;
     Real x3v = CellCenterX(k-ks, indcs.nx3, x3min, x3max);
 
-    ComputeADMDecomposition(x1v, x2v, x3v, minkowski, a,
+    Real r = KSRX(x1v,x2v,x3v,a);
+
+    ComputeADMDecomposition(x1v, x2v, x3v, false, a,
       &adm.alpha(m,k,j,i),
       &adm.beta_u(m,0,k,j,i), &adm.beta_u(m,1,k,j,i), &adm.beta_u(m,2,k,j,i),
       &adm.psi4(m,k,j,i),
@@ -116,6 +123,23 @@ void KerrSchild(MeshBlockPack *pmbp, ParameterInput *pin) {
       &adm.g_dd(m,1,1,k,j,i), &adm.g_dd(m,1,2,k,j,i), &adm.g_dd(m,2,2,k,j,i),
       &adm.vK_dd(m,0,0,k,j,i), &adm.vK_dd(m,0,1,k,j,i), &adm.vK_dd(m,0,2,k,j,i),
       &adm.vK_dd(m,1,1,k,j,i), &adm.vK_dd(m,1,2,k,j,i), &adm.vK_dd(m,2,2,k,j,i));
+    if (r<rexci) {
+      // weight function to attenuate to zero
+      Real w = exp(1/(SQR(r)-SQR(rexci))+1/SQR(rexci));
+      Real invw = 1-w;
+      adm.alpha(m,k,j,i) *= invw;
+      adm.alpha(m,k,j,i) += w * 1;
+      for (int a = 0; a <= 2; ++a) {
+        adm.beta_u(m,a,k,j,i) *= invw;
+        for (int b = a; b<=2; ++b) {
+          adm.g_dd(m,a,b,k,j,i) *= invw;
+          adm.vK_dd(m,a,b,k,j,i) *= invw;
+          if (a == b) {
+            adm.g_dd(m,a,b,k,j,i) += w * 1;
+          }
+        }
+      }
+    }
   });
 }
 
