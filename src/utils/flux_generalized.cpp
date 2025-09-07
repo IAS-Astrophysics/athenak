3ba29c6cec61bb6e820ec84a625075d9d078d761
Hengrui Zhu <hz0693@princeton.edu>
2025-09-07 10:30:09 -0400

    fix bugs and commit local changes

--- src/utils/flux_generalized.cpp
+++ src/utils/flux_generalized.cpp
@@ -142,63 +142,78 @@ void TorusFluxes_General(HistoryData *pdata,
         const Real ivol = 1.0/sqrt_det_g;
         const Real B_u[3] = {B_vals_d(p,0)*ivol, B_vals_d(p,1)*ivol, B_vals_d(p,2)*ivol};
 
-        Real v_prim_d[3]={0.};
-        Real v_sq = 0.;
-        for(int i=0; i<3; ++i) {
-          for(int j=0; j<3; ++j) { v_prim_d[i] += g_dd[i][j] * v_prim_u[j]; }
-          v_sq += v_prim_u[i] * v_prim_d[i];
-        }
+        const Real g_dd_1d[6] = {g_dd[0][0], g_dd[0][1], g_dd[0][2],
+                                 g_dd[1][1], g_dd[1][2], g_dd[2][2]};
+
+        Real v_sq = adm::SquareVector(v_prim_u, g_dd_1d);
         if (v_sq >= 1.0) v_sq = 1.0 - 1.0e-16;
         const Real W = 1.0/sqrt(1.0 - v_sq);
+        const Real Wsq = W * W;
         const Real h = 1.0 + (pgas/(gamma_eos-1.0) + pgas)/rho;
+        const Real H_enthalpy = rho * h;
+        
+        //--- C1. Calculate helper quantities from source-term formulation ---
+        const Real B_sq = adm::SquareVector(B_u, g_dd_1d);
 
-        //--- C. Calculate 3+1 components of the stress-energy tensor ---
-        // 1. Fluid (advective) part
-        const Real E_fluid = rho * h * W * W - pgas;
-        Real S_d_fluid[3];
-        for(int i=0; i<3; ++i) { S_d_fluid[i] = rho * h * W * W * v_prim_d[i]; }
-        Real S_dd_fluid[3][3];
+        Real v_prim_d[3]={0.};
+        Real B_d[3]={0.};
         for(int i=0; i<3; ++i) {
           for(int j=0; j<3; ++j) {
-            S_dd_fluid[i][j] = S_d_fluid[i] * v_prim_d[j] + pgas * g_dd[i][j];
+            v_prim_d[i] += g_dd[i][j] * v_prim_u[j];
+            B_d[i]      += g_dd[i][j] * B_u[j];
           }
         }
-        
-        // 2. EM part
-        Real B_d[3]={0.};
-        Real B_sq = 0.;
-        for(int i=0; i<3; ++i) {
-          for(int j=0; j<3; ++j) { B_d[i] += g_dd[i][j] * B_u[j]; }
-          B_sq += B_u[i] * B_d[i];
-        }
-        const Real E_d[3] = {-sqrt_det_g * (v_prim_u[1]*B_u[2] - v_prim_u[2]*B_u[1]),
-                             -sqrt_det_g * (v_prim_u[2]*B_u[0] - v_prim_u[0]*B_u[2]),
-                             -sqrt_det_g * (v_prim_u[0]*B_u[1] - v_prim_u[1]*B_u[0])};
 
-        Real g_uu[6], g_u[3][3];
-        adm::SpatialInv(1.0/det_g, g_dd[0][0],g_dd[0][1],g_dd[0][2],g_dd[1][1],g_dd[1][2],g_dd[2][2],
-                        &g_uu[0], &g_uu[1], &g_uu[2], &g_uu[3], &g_uu[4], &g_uu[5]);
-        g_u[0][0]=g_uu[0]; g_u[0][1]=g_uu[1]; g_u[0][2]=g_uu[2];
-        g_u[1][0]=g_uu[1]; g_u[1][1]=g_uu[3]; g_u[1][2]=g_uu[4];
-        g_u[2][0]=g_uu[2]; g_u[2][1]=g_uu[4]; g_u[2][2]=g_uu[5];
-        
-        Real E_sq = 0.;
+        const Real B_dot_v_prim = B_u[0]*v_prim_d[0] + B_u[1]*v_prim_d[1] + B_u[2]*v_prim_d[2];
+        const Real b_sq = (B_dot_v_prim * B_dot_v_prim) + B_sq/Wsq;
+
+        //--- C2. Separate Energy Density (E) by term ---
+        const Real E_fluid = H_enthalpy * Wsq - pgas;
+        const Real E_em = B_sq - 0.5*b_sq;
+
+        //--- C3. Separate Momentum Density (S_i) by term ---
+        Real S_d_fluid[3];
+        Real S_d_em[3];
         for(int i=0; i<3; ++i) {
-          for(int j=0; j<3; ++j) { E_sq += g_u[i][j] * E_d[i] * E_d[j]; }
+            S_d_fluid[i] = H_enthalpy * Wsq * v_prim_d[i];
+            S_d_em[i] = b_sq * v_prim_d[i] - (B_dot_v_prim * B_d[i] / Wsq);
         }
-        const Real E_em = 0.5 * (B_sq + E_sq);
-        
-        Real E_u[3]={0.};
-        for(int i=0; i<3; ++i) {
-          for(int j=0; j<3; ++j) { E_u[i] += g_u[i][j] * E_d[j]; }
+
+        //--- C4. Separate Spatial Stress (S_ij) by term ---
+        Real g_u[3][3];
+        {
+            Real g_uu[6];
+            adm::SpatialInv(1.0/det_g, g_dd[0][0],g_dd[0][1],g_dd[0][2],g_dd[1][1],g_dd[1][2],g_dd[2][2],
+                            &g_uu[0], &g_uu[1], &g_uu[2], &g_uu[3], &g_uu[4], &g_uu[5]);
+            g_u[0][0]=g_uu[0]; g_u[0][1]=g_uu[1]; g_u[0][2]=g_uu[2];
+            g_u[1][0]=g_uu[1]; g_u[1][1]=g_uu[3]; g_u[1][2]=g_uu[4];
+            g_u[2][0]=g_uu[2]; g_u[2][1]=g_uu[4]; g_u[2][2]=g_uu[5];
         }
-        const Real S_d_em[3] = {sqrt_det_g * (E_u[1]*B_u[2] - E_u[2]*B_u[1]),
-                                sqrt_det_g * (E_u[2]*B_u[0] - E_u[0]*B_u[2]),
-                                sqrt_det_g * (E_u[0]*B_u[1] - E_u[1]*B_u[0])};
-        Real S_dd_em[3][3];
-        for(int i=0; i<3; ++i) {
-          for(int j=0; j<3; ++j) {
-            S_dd_em[i][j] = -E_d[i]*E_d[j] - B_d[i]*B_d[j] + E_em*g_dd[i][j];
+
+        Real S_uu_fluid[3][3];
+        Real S_uu_em[3][3];
+        for (int i = 0; i < 3; i++) {
+          for (int j = 0; j < 3; j++) {
+            // ----- FIX: Added Wsq to the fluid stress tensor for relativistic consistency -----
+            S_uu_fluid[i][j] = H_enthalpy * Wsq * v_prim_u[i] * v_prim_u[j] + pgas * g_u[i][j];
+
+            S_uu_em[i][j] = b_sq * v_prim_u[i] * v_prim_u[j]
+                          - (B_u[i]*B_u[j]/Wsq)
+                          - (B_dot_v_prim/W)*(B_u[i]*v_prim_u[j] + B_u[j]*v_prim_u[i])
+                          + (0.5*b_sq)*g_u[i][j];
+          }
+        }
+
+        Real S_dd_fluid[3][3] = {{0.}};
+        Real S_dd_em[3][3] = {{0.}};
+        for (int i=0; i<3; ++i) {
+          for (int j=0; j<3; ++j) {
+            for (int a=0; a<3; ++a) {
+              for (int b=0; b<3; ++b) {
+                S_dd_fluid[i][j] += g_dd[i][a] * g_dd[j][b] * S_uu_fluid[a][b];
+                S_dd_em[i][j] += g_dd[i][a] * g_dd[j][b] * S_uu_em[a][b];
+              }
+            }
           }
         }
         
@@ -231,11 +246,10 @@ void TorusFluxes_General(HistoryData *pdata,
         update.edot_fluid += -(FE_fluid[0]*dS_d[0] + FE_fluid[1]*dS_d[1] + FE_fluid[2]*dS_d[2]);
         update.edot_em    += -(FE_em[0]*dS_d[0]    + FE_em[1]*dS_d[1]    + FE_em[2]*dS_d[2]);
 
-        // ----- START FIX: Corrected momentum and angular momentum flux calculation -----
         // 3. Momentum and Angular Momentum Fluxes
-        Real FS_fluid[3][3], FS_em[3][3]; // Momentum flux density T^i_j
-        for(int i=0; i<3; ++i) { // contravariant index i
-          for(int j=0; j<3; ++j) { // covariant index j
+        Real FS_fluid[3][3], FS_em[3][3];
+        for(int i=0; i<3; ++i) {
+          for(int j=0; j<3; ++j) {
             Real S_ui_fluid_j = 0., S_ui_em_j = 0.;
             for(int k=0; k<3; ++k) {
               S_ui_fluid_j += g_u[i][k] * S_dd_fluid[k][j];
@@ -246,16 +260,14 @@ void TorusFluxes_General(HistoryData *pdata,
           }
         }
         
-        // Calculate momentum flux through the surface element dS_d at this point
         Real d_pdot_fluid[3]={0.}, d_pdot_em[3]={0.};
-        for (int j=0; j<3; ++j) { // j-th component of momentum
-          for (int i=0; i<3; ++i) { // flux through i-th surface direction
+        for (int j=0; j<3; ++j) {
+          for (int i=0; i<3; ++i) {
             d_pdot_fluid[j] += FS_fluid[i][j] * dS_d[i];
             d_pdot_em[j]    += FS_em[i][j]    * dS_d[i];
           }
         }
         
-        // Update total linear momentum flux (negative sign for flux *into* volume)
         update.pdot_x_fluid += -d_pdot_fluid[0];
         update.pdot_y_fluid += -d_pdot_fluid[1];
         update.pdot_z_fluid += -d_pdot_fluid[2];
@@ -263,7 +275,6 @@ void TorusFluxes_General(HistoryData *pdata,
         update.pdot_y_em += -d_pdot_em[1];
         update.pdot_z_em += -d_pdot_em[2];
 
-        // Update total angular momentum flux using the local coordinates
         const Real x = current_surf->Coords().d_view(p,0);
         const Real y = current_surf->Coords().d_view(p,1);
         const Real z = current_surf->Coords().d_view(p,2);
@@ -273,23 +284,24 @@ void TorusFluxes_General(HistoryData *pdata,
         update.ldot_x_em += -(y * d_pdot_em[2] - z * d_pdot_em[1]);
         update.ldot_y_em += -(z * d_pdot_em[0] - x * d_pdot_em[2]);
         update.ldot_z_em += -(x * d_pdot_em[1] - y * d_pdot_em[0]);
-        // ----- END FIX -----
 
-        // ----- START FIX: Corrected magnetic flux calculation -----
-        // 4. Unsigned Magnetic Flux (purely EM)
+        // 4. Unsigned Magnetic Flux (Generalized TorusFluxes version)
         {
-          // The magnetic flux is the integral of the normal component of the Eulerian
-          // magnetic field B^i over the surface.
-          // B_u is the contravariant magnetic field component B^i.
-          // dS_d is the covariant surface element dS_i.
-          // The scalar product B^i dS_i is the flux through the surface element.
-          const Real B_dot_dS = B_u[0] * dS_d[0] + B_u[1] * dS_d[1] + B_u[2] * dS_d[2];
-
-          // We sum the absolute value of the flux through each element to get the
-          // total unsigned magnetic flux piercing the surface.
-          update.phiB += Kokkos::fabs(B_dot_dS);
+          const Real u_u_t = W / alp;
+          const Real u_u[3] = {W*v_prim_u[0], W*v_prim_u[1], W*v_prim_u[2]};
+          const Real b_u_t = B_dot_v_prim / alp;
+          Real b_u[3];
+          for (int i=0; i<3; ++i) {
+            b_u[i] = (B_u[i] / W) + (B_dot_v_prim * v_prim_u[i]);
+          }
+          const Real M_u[3] = {
+            u_u_t * b_u[0] - u_u[0] * b_u_t,
+            u_u_t * b_u[1] - u_u[1] * b_u_t,
+            u_u_t * b_u[2] - u_u[2] * b_u_t
+          };
+          const Real flux_dot_dS = M_u[0]*dS_d[0] + M_u[1]*dS_d[1] + M_u[2]*dS_d[2];
+          update.phiB += 0.5 * Kokkos::fabs(flux_dot_dS);
         }
-        // ----- END FIX -----
         
         // 5. Surface Area
         Real dS_u[3] = {0.};
