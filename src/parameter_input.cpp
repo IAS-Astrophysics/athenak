2148e6119a0ec0f0b95fcaf4ccffc83d7d84dfe6
Jim Stone <jmstone@ias.edu>
2025-09-13 11:31:55 -0400

    Added more input block names to allowed values in ParameterInput class.

--- src/parameter_input.cpp
+++ src/parameter_input.cpp
@@ -103,7 +103,8 @@ void ParameterInput::CheckBlockNames() {
     "mesh", "meshblock", "mesh_refinement", "refined_region", "amr_criterion",
     "coord", "adm", "shearing_box",
     "time", "problem", "output",
-    "hydro", "mhd", "ion-neutral", "radiation", "z4c", "z4c_amr"
+    "hydro", "mhd", "ion-neutral", "radiation", "z4c", "z4c_amr",
+    "rad_srcterms", "hydro_srcterms", "mhd_srcterms"
     };
 
   for (auto it1 = block.begin(); it1 != block.end(); ++it1) {
