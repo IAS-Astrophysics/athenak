13c245afba663f2bcb19890e0ba6d11b55fb3421
Jacob Fields <fields.jacobm@gmail.com>
2025-10-13 13:28:16 -0400

    Fixed memory access error for dyngr_tov

--- src/pgen/dyngr_tov.cpp
+++ src/pgen/dyngr_tov.cpp
@@ -198,7 +198,7 @@ void SetupTOV(ParameterInput *pin, Mesh* pmy_mesh_) {
   // If use_pcut_rel = true, we take pcut to be a percentage of pmax rather than
   // an absolute cutoff
   if (pin->GetOrAddBoolean("problem", "use_pcut_rel", false)) {
-    Real pmax = eos_.template GetPFromRho<tov::LocationTag::Device>(tov_.rhoc);
+    Real pmax = eos_.template GetPFromRho<tov::LocationTag::Host>(tov_.rhoc);
     pcut = pcut * pmax;
   }
 
