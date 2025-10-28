74caec3681b4d041a721b6beb897a1bba68a9b82
davidvelasco07 <david.velasco@princeton.edu>
2025-10-28 16:20:41 -0500

    Further changes

--- src/mhd/rsolvers/hlld_mhd.hpp
+++ src/mhd/rsolvers/hlld_mhd.hpp
@@ -15,6 +15,13 @@
 namespace mhd {
 
 #define HLLD_SMALL_NUMBER 1.0e-4
+#define vbstr ((urst.mx*bxi+(urst.my*urst.by+urst.mz*urst.bz))*urst_d_inv)
+#define vbstl ((ulst.mx*bxi+(ulst.my*ulst.by+ulst.mz*ulst.bz))*ulst_d_inv)
+
+#define _ivy IVX + ((ivx-IVX)+1)%3
+#define _ivz IVX + ((ivx-IVX)+2)%3
+#define _iby ((ivx-IVX) + 1)%3
+#define _ibz ((ivx-IVX) + 2)%3
 
 //----------------------------------------------------------------------------------------
 //! \fn
@@ -26,10 +33,10 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
      const ScrArray2D<Real> &wl, const ScrArray2D<Real> &wr,
      const ScrArray2D<Real> &bl, const ScrArray2D<Real> &br, const DvceArray4D<Real> &bx,
      DvceArray5D<Real> flx, DvceArray4D<Real> ey, DvceArray4D<Real> ez) {
-  int ivy = IVX + ((ivx-IVX)+1)%3;
-  int ivz = IVX + ((ivx-IVX)+2)%3;
-  int iby = ((ivx-IVX) + 1)%3;
-  int ibz = ((ivx-IVX) + 2)%3;
+  //int ivy = IVX + ((ivx-IVX)+1)%3;
+  //int ivz = IVX + ((ivx-IVX)+2)%3;
+  //int iby = ((ivx-IVX) + 1)%3;
+  //int ibz = ((ivx-IVX) + 2)%3;
   Real spd[5];         // signal speeds, left to right
 
   //------------------------ ADIABATIC HLLD solver ---------------------------------------
@@ -63,32 +70,32 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
       {
         Real bxsq = bxi*bxi;
         // (KGF): group transverse components for floating-point associativity symmetry
-        Real pbl = 0.5*(bxsq + (SQR(wl(iby,i)) + SQR(wl(ibz,i))));  // magnetic pressure (l/r)
-        Real pbr = 0.5*(bxsq + (SQR(wr(iby,i)) + SQR(wr(ibz,i))));
-        Real kel = 0.5*wl(IDN,i)*(SQR(wl(ivx,i)) + (SQR(wl(ivy,i)) + SQR(wl(ivz,i))));
-        Real ker = 0.5*wr(IDN,i)*(SQR(wr(ivx,i)) + (SQR(wr(ivy,i)) + SQR(wr(ivz,i))));
+        Real pbl = 0.5*(bxsq + (SQR(wl(_iby,i)) + SQR(wl(_ibz,i))));  // magnetic pressure (l/r)
+        Real pbr = 0.5*(bxsq + (SQR(wr(_iby,i)) + SQR(wr(_ibz,i))));
+        Real kel = 0.5*wl(IDN,i)*(SQR(wl(ivx,i)) + (SQR(wl(_ivy,i)) + SQR(wl(_ivz,i))));
+        Real ker = 0.5*wr(IDN,i)*(SQR(wr(ivx,i)) + (SQR(wr(_ivy,i)) + SQR(wr(_ivz,i))));
 
         MHDCons1D ul,ur;  // L/R states, conserved variables (computed)
         ul.d  = wl(IDN,i);
         ul.mx = wl(ivx,i)*ul.d;
-        ul.my = wl(ivy,i)*ul.d;
-        ul.mz = wl(ivz,i)*ul.d;
+        ul.my = wl(_ivy,i)*ul.d;
+        ul.mz = wl(_ivz,i)*ul.d;
         ul.e  = wl_ipr*igm1 + kel + pbl;
-        ul.by = wl(iby,i);
-        ul.bz = wl(ibz,i);
+        ul.by = wl(_iby,i);
+        ul.bz = wl(_ibz,i);
 
         ur.d  = wr(IDN,i);
         ur.mx = wr(ivx,i)*ur.d;
-        ur.my = wr(ivy,i)*ur.d;
-        ur.mz = wr(ivz,i)*ur.d;
+        ur.my = wr(_ivy,i)*ur.d;
+        ur.mz = wr(_ivz,i)*ur.d;
         ur.e  = wr_ipr*igm1 + ker + pbr;
-        ur.by = wr(iby,i);
-        ur.bz = wr(ibz,i);
+        ur.by = wr(_iby,i);
+        ur.bz = wr(_ibz,i);
 
         //--- Step 2.  Compute L & R wave speeds according to Miyoshi & Kusano, eqn. (67)
 
-        Real cfl = eos.IdealMHDFastSpeed(wl(IDN,i), wl_ipr, bxi, wl(iby,i), wl(ibz,i));
-        Real cfr = eos.IdealMHDFastSpeed(wr(IDN,i), wr_ipr, bxi, wr(iby,i), wr(ibz,i));
+        Real cfl = eos.IdealMHDFastSpeed(wl(IDN,i), wl_ipr, bxi, wl(_iby,i), wl(_ibz,i));
+        Real cfr = eos.IdealMHDFastSpeed(wr(IDN,i), wr_ipr, bxi, wr(_iby,i), wr(_ibz,i));
 
         spd[0] = fmin( wl(ivx,i)-cfl, wr(ivx,i)-cfr );
         spd[4] = fmax( wl(ivx,i)+cfl, wr(ivx,i)+cfr );
@@ -112,17 +119,17 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
         fl.mx = ul.mx*wl(ivx,i) + ptl - bxsq;
         fl.my = ul.my*wl(ivx,i) - bxi*ul.by;
         fl.mz = ul.mz*wl(ivx,i) - bxi*ul.bz;
-        fl.e  = wl(ivx,i)*(ul.e + ptl - bxsq) - bxi*(wl(ivy,i)*ul.by + wl(ivz,i)*ul.bz);
-        fl.by = ul.by*wl(ivx,i) - bxi*wl(ivy,i);
-        fl.bz = ul.bz*wl(ivx,i) - bxi*wl(ivz,i);
+        fl.e  = wl(ivx,i)*(ul.e + ptl - bxsq) - bxi*(wl(_ivy,i)*ul.by + wl(_ivz,i)*ul.bz);
+        fl.by = ul.by*wl(ivx,i) - bxi*wl(_ivy,i);
+        fl.bz = ul.bz*wl(ivx,i) - bxi*wl(_ivz,i);
 
         fr.d  = ur.mx;
         fr.mx = ur.mx*wr(ivx,i) + ptr - bxsq;
         fr.my = ur.my*wr(ivx,i) - bxi*ur.by;
         fr.mz = ur.mz*wr(ivx,i) - bxi*ur.bz;
-        fr.e  = wr(ivx,i)*(ur.e + ptr - bxsq) - bxi*(wr(ivy,i)*ur.by + wr(ivz,i)*ur.bz);
-        fr.by = ur.by*wr(ivx,i) - bxi*wr(ivy,i);
-        fr.bz = ur.bz*wr(ivx,i) - bxi*wr(ivz,i);
+        fr.e  = wr(ivx,i)*(ur.e + ptr - bxsq) - bxi*(wr(_ivy,i)*ur.by + wr(_ivz,i)*ur.bz);
+        fr.by = ur.by*wr(ivx,i) - bxi*wr(_ivy,i);
+        fr.bz = ur.bz*wr(ivx,i) - bxi*wr(_ivz,i);
 
         //--- Step 4.  Compute middle and Alfven wave speeds
 
@@ -152,26 +159,26 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
         //--- Step 5.  Compute intermediate states
         // eqn (23) explicitly becomes eq (41) of Miyoshi & Kusano
         // TODO(felker): place an assertion that ptstl==ptstr
-        Real ptstl = ptl + ul.d*sdl*(spd[2]-wl(ivx,i));
-        Real ptstr = ptr + ur.d*sdr*(spd[2]-wr(ivx,i));
+        //Real ptstl = ptl + ul.d*sdl*(spd[2]-wl(ivx,i));
+        //Real ptstr = ptr + ur.d*sdr*(spd[2]-wr(ivx,i));
         // Real ptstl = ptl + ul.d*sdl*(sdl-sdml); // these eqns had issues when averaged
         // Real ptstr = ptr + ur.d*sdr*(sdr-sdmr);
-        Real ptst = 0.5*(ptstr + ptstl);  // total pressure (star state)
+        Real ptst = 0.5*(ptl + ul.d*sdl*(spd[2]-wl(ivx,i)) + ptr + ur.d*sdr*(spd[2]-wr(ivx,i)));  // total pressure (star state)
 
         // ul* - eqn (39) of M&K
         ulst.mx = ulst.d * spd[2];
         if (fabs(ul.d*sdl*sdml-bxsq) < (HLLD_SMALL_NUMBER)*ptst) {
           // Degenerate case
-          ulst.my = ulst.d * wl(ivy,i);
-          ulst.mz = ulst.d * wl(ivz,i);
+          ulst.my = ulst.d * wl(_ivy,i);
+          ulst.mz = ulst.d * wl(_ivz,i);
 
           ulst.by = ul.by;
           ulst.bz = ul.bz;
         } else {
           // eqns (44) and (46) of M&K
           Real tmp = bxi*(sdl - sdml)/(ul.d*sdl*sdml - bxsq);
-          ulst.my = ulst.d * (wl(ivy,i) - ul.by*tmp);
-          ulst.mz = ulst.d * (wl(ivz,i) - ul.bz*tmp);
+          ulst.my = ulst.d * (wl(_ivy,i) - ul.by*tmp);
+          ulst.mz = ulst.d * (wl(_ivz,i) - ul.bz*tmp);
 
           // eqns (45) and (47) of M&K
           tmp = (ul.d*SQR(sdl) - bxsq)/(ul.d*sdl*sdml - bxsq);
@@ -180,26 +187,26 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
         }
         // v_i* dot B_i*
         // (KGF): group transverse momenta terms for floating-point associativity symmetry
-        Real vbstl = (ulst.mx*bxi+(ulst.my*ulst.by+ulst.mz*ulst.bz))*ulst_d_inv;
+        //Real vbstl = (ulst.mx*bxi+(ulst.my*ulst.by+ulst.mz*ulst.bz))*ulst_d_inv;
         // eqn (48) of M&K
         // (KGF): group transverse by, bz terms for floating-point associativity symmetry
         ulst.e = (sdl*ul.e - ptl*wl(ivx,i) + ptst*spd[2] +
-                  bxi*(wl(ivx,i)*bxi + (wl(ivy,i)*ul.by + wl(ivz,i)*ul.bz) - vbstl))/sdml;
+                  bxi*(wl(ivx,i)*bxi + (wl(_ivy,i)*ul.by + wl(_ivz,i)*ul.bz) - vbstl))/sdml;
 
         // ur* - eqn (39) of M&K
         urst.mx = urst.d * spd[2];
         if (fabs(ur.d*sdr*sdmr - bxsq) < (HLLD_SMALL_NUMBER)*ptst) {
           // Degenerate case
-          urst.my = urst.d * wr(ivy,i);
-          urst.mz = urst.d * wr(ivz,i);
+          urst.my = urst.d * wr(_ivy,i);
+          urst.mz = urst.d * wr(_ivz,i);
 
           urst.by = ur.by;
           urst.bz = ur.bz;
         } else {
           // eqns (44) and (46) of M&K
           Real tmp = bxi*(sdr - sdmr)/(ur.d*sdr*sdmr - bxsq);
-          urst.my = urst.d * (wr(ivy,i) - ur.by*tmp);
-          urst.mz = urst.d * (wr(ivz,i) - ur.bz*tmp);
+          urst.my = urst.d * (wr(_ivy,i) - ur.by*tmp);
+          urst.mz = urst.d * (wr(_ivz,i) - ur.bz*tmp);
 
           // eqns (45) and (47) of M&K
           tmp = (ur.d*SQR(sdr) - bxsq)/(ur.d*sdr*sdmr - bxsq);
@@ -208,11 +215,11 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
         }
         // v_i* dot B_i*
         // (KGF): group transverse momenta terms for floating-point associativity symmetry
-        Real vbstr = (urst.mx*bxi+(urst.my*urst.by+urst.mz*urst.bz))*urst_d_inv;
+        //Real vbstr = (urst.mx*bxi+(urst.my*urst.by+urst.mz*urst.bz))*urst_d_inv;
         // eqn (48) of M&K
         // (KGF): group transverse by, bz terms for floating-point associativity symmetry
         urst.e = (sdr*ur.e - ptr*wr(ivx,i) + ptst*spd[2] +
-                  bxi*(wr(ivx,i)*bxi + (wr(ivy,i)*ur.by + wr(ivz,i)*ur.bz) - vbstr))/sdmr;
+                  bxi*(wr(ivx,i)*bxi + (wr(_ivy,i)*ur.by + wr(_ivz,i)*ur.bz) - vbstr))/sdmr;
         // ul** and ur** - if Bx is near zero, same as *-states
         if (0.5*bxsq < (HLLD_SMALL_NUMBER)*ptst) {
           uldst = ulst;
@@ -292,8 +299,8 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
           // return Fl if flow is supersonic
           flx(m,IDN,k,j,i) = fl.d;
           flx(m,ivx,k,j,i) = fl.mx;
-          flx(m,ivy,k,j,i) = fl.my;
-          flx(m,ivz,k,j,i) = fl.mz;
+          flx(m,_ivy,k,j,i) = fl.my;
+          flx(m,_ivz,k,j,i) = fl.mz;
           flx(m,IEN,k,j,i) = fl.e;
           ey(m,k,j,i) = -fl.by;
           ez(m,k,j,i) =  fl.bz;
@@ -301,8 +308,8 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
           // return Fr if flow is supersonic
           flx(m,IDN,k,j,i) = fr.d;
           flx(m,ivx,k,j,i) = fr.mx;
-          flx(m,ivy,k,j,i) = fr.my;
-          flx(m,ivz,k,j,i) = fr.mz;
+          flx(m,_ivy,k,j,i) = fr.my;
+          flx(m,_ivz,k,j,i) = fr.mz;
           flx(m,IEN,k,j,i) = fr.e;
           ey(m,k,j,i) = -fr.by;
           ez(m,k,j,i) =  fr.bz;
@@ -310,8 +317,8 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
           // return Fl*
           flx(m,IDN,k,j,i) = fl.d  + ulst.d;
           flx(m,ivx,k,j,i) = fl.mx + ulst.mx;
-          flx(m,ivy,k,j,i) = fl.my + ulst.my;
-          flx(m,ivz,k,j,i) = fl.mz + ulst.mz;
+          flx(m,_ivy,k,j,i) = fl.my + ulst.my;
+          flx(m,_ivz,k,j,i) = fl.mz + ulst.mz;
           flx(m,IEN,k,j,i) = fl.e  + ulst.e;
           ey(m,k,j,i) = -(fl.by + ulst.by);
           ez(m,k,j,i) =  (fl.bz + ulst.bz);
@@ -319,8 +326,8 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
           // return Fl**
           flx(m,IDN,k,j,i) = fl.d  + ulst.d + uldst.d;
           flx(m,ivx,k,j,i) = fl.mx + ulst.mx + uldst.mx;
-          flx(m,ivy,k,j,i) = fl.my + ulst.my + uldst.my;
-          flx(m,ivz,k,j,i) = fl.mz + ulst.mz + uldst.mz;
+          flx(m,_ivy,k,j,i) = fl.my + ulst.my + uldst.my;
+          flx(m,_ivz,k,j,i) = fl.mz + ulst.mz + uldst.mz;
           flx(m,IEN,k,j,i) = fl.e  + ulst.e + uldst.e;
           ey(m,k,j,i) = -(fl.by + ulst.by + uldst.by);
           ez(m,k,j,i) =  (fl.bz + ulst.bz + uldst.bz);
@@ -328,8 +335,8 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
           // return Fr**
           flx(m,IDN,k,j,i) = fr.d + urst.d + urdst.d;
           flx(m,ivx,k,j,i) = fr.mx + urst.mx + urdst.mx;
-          flx(m,ivy,k,j,i) = fr.my + urst.my + urdst.my;
-          flx(m,ivz,k,j,i) = fr.mz + urst.mz + urdst.mz;
+          flx(m,_ivy,k,j,i) = fr.my + urst.my + urdst.my;
+          flx(m,_ivz,k,j,i) = fr.mz + urst.mz + urdst.mz;
           flx(m,IEN,k,j,i) = fr.e + urst.e + urdst.e;
           ey(m,k,j,i) = -(fr.by + urst.by + urdst.by);
           ez(m,k,j,i) =  (fr.bz + urst.bz + urdst.bz);
@@ -337,8 +344,8 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
           // return Fr*
           flx(m,IDN,k,j,i) = fr.d  + urst.d;
           flx(m,ivx,k,j,i) = fr.mx + urst.mx;
-          flx(m,ivy,k,j,i) = fr.my + urst.my;
-          flx(m,ivz,k,j,i) = fr.mz + urst.mz;
+          flx(m,_ivy,k,j,i) = fr.my + urst.my;
+          flx(m,_ivz,k,j,i) = fr.mz + urst.mz;
           flx(m,IEN,k,j,i) = fr.e  + urst.e;
           ey(m,k,j,i) = -(fr.by + urst.by);
           ez(m,k,j,i) =  (fr.bz + urst.bz);
@@ -374,22 +381,22 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
         MHDCons1D ul,ur;
         ul.d  = wl(IDN,i);
         ul.mx = wl(ivx,i)*ul.d;
-        ul.my = wl(ivy,i)*ul.d;
-        ul.mz = wl(ivz,i)*ul.d;
-        ul.by = wl(iby,i);
-        ul.bz = wl(ibz,i);
+        ul.my = wl(_ivy,i)*ul.d;
+        ul.mz = wl(_ivz,i)*ul.d;
+        ul.by = wl(_iby,i);
+        ul.bz = wl(_ibz,i);
 
         ur.d  = wr(IDN,i);
         ur.mx = wr(ivx,i)*ur.d;
-        ur.my = wr(ivy,i)*ur.d;
-        ur.mz = wr(ivz,i)*ur.d;
-        ur.by = wr(iby,i);
-        ur.bz = wr(ibz,i);
+        ur.my = wr(_ivy,i)*ur.d;
+        ur.mz = wr(_ivz,i)*ur.d;
+        ur.by = wr(_iby,i);
+        ur.bz = wr(_ibz,i);
 
         //--- Step 2.  Compute L & R wave speeds according to Miyoshi & Kusano, eqn. (67)
 
-        Real cfl = eos.IdealMHDFastSpeed(wl(IDN,i), bxi, wl(iby,i), wl(ibz,i));
-        Real cfr = eos.IdealMHDFastSpeed(wr(IDN,i), bxi, wr(iby,i), wr(ibz,i));
+        Real cfl = eos.IdealMHDFastSpeed(wl(IDN,i), bxi, wl(_iby,i), wl(_ibz,i));
+        Real cfr = eos.IdealMHDFastSpeed(wr(IDN,i), bxi, wr(_iby,i), wr(_ibz,i));
 
         spd[0] = fmin( wl(ivx,i)-cfl, wr(ivx,i)-cfr );
         spd[4] = fmax( wl(ivx,i)+cfl, wr(ivx,i)+cfr );
@@ -398,23 +405,23 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
 
         // total pressures L,R
         Real bxsq = bxi*bxi;
-        Real ptl = SQR(iso_cs)*wl(IDN,i) + 0.5*(bxsq + SQR(wl(iby,i)) + SQR(wl(ibz,i)));
-        Real ptr = SQR(iso_cs)*wr(IDN,i) + 0.5*(bxsq + SQR(wr(iby,i)) + SQR(wr(ibz,i)));
+        Real ptl = SQR(iso_cs)*wl(IDN,i) + 0.5*(bxsq + SQR(wl(_iby,i)) + SQR(wl(_ibz,i)));
+        Real ptr = SQR(iso_cs)*wr(IDN,i) + 0.5*(bxsq + SQR(wr(_iby,i)) + SQR(wr(_ibz,i)));
 
         MHDCons1D fl,fr;  // Fluxes for left & right states
         fl.d  = ul.mx;
         fl.mx = ul.mx*wl(ivx,i) + ptl - bxsq;
         fl.my = ul.my*wl(ivx,i) - bxi*ul.by;
         fl.mz = ul.mz*wl(ivx,i) - bxi*ul.bz;
-        fl.by = ul.by*wl(ivx,i) - bxi*wl(ivy,i);
-        fl.bz = ul.bz*wl(ivx,i) - bxi*wl(ivz,i);
+        fl.by = ul.by*wl(ivx,i) - bxi*wl(_ivy,i);
+        fl.bz = ul.bz*wl(ivx,i) - bxi*wl(_ivz,i);
 
         fr.d  = ur.mx;
         fr.mx = ur.mx*wr(ivx,i) + ptr - bxsq;
         fr.my = ur.my*wr(ivx,i) - bxi*ur.by;
         fr.mz = ur.mz*wr(ivx,i) - bxi*ur.bz;
-        fr.by = ur.by*wr(ivx,i) - bxi*wr(ivy,i);
-        fr.bz = ur.bz*wr(ivx,i) - bxi*wr(ivz,i);
+        fr.by = ur.by*wr(ivx,i) - bxi*wr(_ivy,i);
+        fr.bz = ur.bz*wr(ivx,i) - bxi*wr(_ivz,i);
 
         //--- Step 4.  Compute hll averages and Alfven wave speed
 
@@ -459,8 +466,8 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
           Real mfact = bxi*(ustar-wl(ivx,i))/tmp;
           Real bfact = (ul.d*SQR(spd[0]-wl(ivx,i)) - bxsq)/(dhll*tmp);
 
-          ulst.my = dhll*wl(ivy,i) - ul.by*mfact; // eqn. (30) of Mignone
-          ulst.mz = dhll*wl(ivz,i) - ul.bz*mfact; // eqn. (31) of Mignone
+          ulst.my = dhll*wl(_ivy,i) - ul.by*mfact; // eqn. (30) of Mignone
+          ulst.mz = dhll*wl(_ivz,i) - ul.bz*mfact; // eqn. (31) of Mignone
           ulst.by = ul.by*bfact; // eqn. (32) of Mignone
           ulst.bz = ul.bz*bfact; // eqn. (33) of Mignone
         }
@@ -480,8 +487,8 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
           Real mfact = bxi*(ustar-wr(ivx,i))/tmp;
           Real bfact = (ur.d*SQR(spd[4]-wr(ivx,i)) - bxsq)/(dhll*tmp);
 
-          urst.my = dhll*wr(ivy,i) - ur.by*mfact; // eqn. (30) of Mignone
-          urst.mz = dhll*wr(ivz,i) - ur.bz*mfact; // eqn. (31) of Mignone
+          urst.my = dhll*wr(_ivy,i) - ur.by*mfact; // eqn. (30) of Mignone
+          urst.mz = dhll*wr(_ivz,i) - ur.bz*mfact; // eqn. (31) of Mignone
           urst.by = ur.by*bfact; // eqn. (32) of Mignone
           urst.bz = ur.bz*bfact; // eqn. (33) of Mignone
         }
@@ -501,40 +508,40 @@ void HLLD(TeamMember_t const &member, const EOS_Data &eos,
           // return Fl if flow is supersonic, eqn. (38a) of Mignone
           flx(m,IDN,k,j,i) = fl.d;
           flx(m,ivx,k,j,i) = fl.mx;
-          flx(m,ivy,k,j,i) = fl.my;
-          flx(m,ivz,k,j,i) = fl.mz;
+          flx(m,_ivy,k,j,i) = fl.my;
+          flx(m,_ivz,k,j,i) = fl.mz;
           ey(m,k,j,i) = -fl.by;
           ez(m,k,j,i) =  fl.bz;
         } else if (spd[4] <= 0.0) {
           // return Fr if flow is supersonic, eqn. (38e) of Mignone
           flx(m,IDN,k,j,i) = fr.d;
           flx(m,ivx,k,j,i) = fr.mx;
-          flx(m,ivy,k,j,i) = fr.my;
-          flx(m,ivz,k,j,i) = fr.mz;
+          flx(m,_ivy,k,j,i) = fr.my;
+          flx(m,_ivz,k,j,i) = fr.mz;
           ey(m,k,j,i) = -fr.by;
           ez(m,k,j,i) =  fr.bz;
         } else if (spd[1] >= 0.0) {
           // return (Fl+Sl*(Ulst-Ul)), eqn. (38b) of Mignone
           flx(m,IDN,k,j,i) = fl.d  + spd[0]*(ulst.d  - ul.d);
           flx(m,ivx,k,j,i) = fl.mx + spd[0]*(ulst.mx - ul.mx);
-          flx(m,ivy,k,j,i) = fl.my + spd[0]*(ulst.my - ul.my);
-          flx(m,ivz,k,j,i) = fl.mz + spd[0]*(ulst.mz - ul.mz);
+          flx(m,_ivy,k,j,i) = fl.my + spd[0]*(ulst.my - ul.my);
+          flx(m,_ivz,k,j,i) = fl.mz + spd[0]*(ulst.mz - ul.mz);
           ey(m,k,j,i) = -(fl.by + spd[0]*(ulst.by - ul.by));
           ez(m,k,j,i) =  (fl.bz + spd[0]*(ulst.bz - ul.bz));
         } else if (spd[3] <= 0.0) {
           // return (Fr+Sr*(Urst-Ur)), eqn. (38d) of Mignone
           flx(m,IDN,k,j,i) = fr.d  + spd[4]*(urst.d  - ur.d);
           flx(m,ivx,k,j,i) = fr.mx + spd[4]*(urst.mx - ur.mx);
-          flx(m,ivy,k,j,i) = fr.my + spd[4]*(urst.my - ur.my);
-          flx(m,ivz,k,j,i) = fr.mz + spd[4]*(urst.mz - ur.mz);
+          flx(m,_ivy,k,j,i) = fr.my + spd[4]*(urst.my - ur.my);
+          flx(m,_ivz,k,j,i) = fr.mz + spd[4]*(urst.mz - ur.mz);
           ey(m,k,j,i) = -(fr.by + spd[4]*(urst.by - ur.by));
           ez(m,k,j,i) =  (fr.bz + spd[4]*(urst.bz - ur.bz));
         } else {
           // return Fcst, eqn. (38c) of Mignone, using eqn. (24)
           flx(m,IDN,k,j,i) = dhll*ustar;
           flx(m,ivx,k,j,i) = fmxhll;
-          flx(m,ivy,k,j,i) = ucst.my*ustar - bxi*ucst.by;
-          flx(m,ivz,k,j,i) = ucst.mz*ustar - bxi*ucst.bz;
+          flx(m,_ivy,k,j,i) = ucst.my*ustar - bxi*ucst.by;
+          flx(m,_ivz,k,j,i) = ucst.mz*ustar - bxi*ucst.bz;
           ey(m,k,j,i) = -(ucst.by*ustar - bxi*ucst.my/ucst.d);
           ez(m,k,j,i) =  (ucst.bz*ustar - bxi*ucst.mz/ucst.d);
         }
