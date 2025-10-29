2c888dc4e3e66ddcf40f8d33a7b8720c3f348f38
davidvelasco07 <david.velasco@princeton.edu>
2025-10-29 12:01:09 -0500

    Moved RS to the same kernel as reconstruction

--- src/mhd/mhd_fluxes.cpp
+++ src/mhd/mhd_fluxes.cpp
@@ -62,11 +62,6 @@ void MHD::CalculateFluxes(Driver *pdriver, int stage) {
   //--------------------------------------------------------------------------------------
   // i-direction
 
-  auto &flx1_ = uflx.x1f;
-  auto &e31_ = e3x1;
-  auto &e21_ = e2x1;
-  auto &bx_ = b0.x1f;
-
   // set the loop limits for 1D/2D/3D problems
   int jl,ju,kl,ku;
   if (pmy_pack->pmesh->one_d) {
@@ -79,28 +74,20 @@ void MHD::CalculateFluxes(Driver *pdriver, int stage) {
   int il = is, iu = ie+1;
   if (use_fofc) { il = is-1, iu = ie+2; }
 
-  par_for("mhd_recon_flux1",DevExeSpace(), 0, nmb1, kl, ku, jl, ju, il, iu,
+  par_for("mhd_flux1",DevExeSpace(), 0, nmb1, kl, ku, jl, ju, il, iu,
   KOKKOS_LAMBDA(int m, int k, int j, int i) {
+    // Reconstruct qR[i] and qL[i+1], for both W and Bcc
     PiecewiseLinear(m, k, j, i, w0_, wl.x1f, wr.x1f, 1);
     PiecewiseLinear(m, k, j, i, b0_, bl.x1f, br.x1f, 1);
-    });
     // compute fluxes over [is,ie+1].  MHD RS also computes electric fields, where
     // (IBY) component of flx = E_{z} = -(v x B)_{z} = -(v1*b2 - v2*b1)
     // (IBZ) component of flx = E_{y} = -(v x B)_{y} =  (v1*b3 - v3*b1)
     // NOTE(@pdmullen): Capture variables prior to if constexpr.  Required for cuda 11.6+.
-  par_for("mhd_rs_flux1",DevExeSpace(), 0, nmb1, kl, ku, jl, ju, il, iu,
-    KOKKOS_LAMBDA(int m, int k, int j, int i) {
     HLLD(eos_,indcs_,size_,coord_,m,k,j,i,IVX,wl.x1f,wr.x1f,bl.x1f,br.x1f,b0.x1f,uflx.x1f,e3x1,e2x1);
-  });
-
+    });
   //--------------------------------------------------------------------------------------
   // j-direction
   if (pmy_pack->pmesh->multi_d) {
-    auto &flx2_ = uflx.x2f;
-    auto &by_ = b0.x2f;
-    auto &e12_ = e1x2;
-    auto &e32_ = e3x2;
-
     // set the loop limits for 2D/3D problems
     if (pmy_pack->pmesh->two_d) {
       kl = ks, ku = ke;
@@ -110,49 +97,34 @@ void MHD::CalculateFluxes(Driver *pdriver, int stage) {
     jl = js-1, ju = je+1;
     if (use_fofc) { jl = js-2, ju = je+2; }
 
-    // Reconstruct qR[j] and qL[j+1], for both W and Bcc
-    par_for("mhd_recon_flux2",DevExeSpace(), 0, nmb1, kl, ku, jl, ju, il, iu,
+    
+    par_for("mhd_flux2",DevExeSpace(), 0, nmb1, kl, ku, jl, ju, il, iu,
       KOKKOS_LAMBDA(int m, int k, int j, int i) {
+        // Reconstruct qR[j] and qL[j+1], for both W and Bcc
         PiecewiseLinear(m, k, j, i, w0_, wl.x2f, wr.x2f, 2);
         PiecewiseLinear(m, k, j, i, b0_, bl.x2f, br.x2f, 2);
-      });
-
-    // compute fluxes over [js,je+1].  MHD RS also computes electric fields, where
-    // (IBY) component of flx = E_{x} = -(v x B)_{x} = -(v2*b3 - v3*b2)
-    // (IBZ) component of flx = E_{z} = -(v x B)_{z} =  (v2*b1 - v1*b2)
-    par_for("mhd_rs_flux2",DevExeSpace(), 0, nmb1, kl, ku, jl, ju, il, iu,
-      KOKKOS_LAMBDA(int m, int k, int j, int i) {
+        // compute fluxes over [js,je+1].  MHD RS also computes electric fields, where
+        // (IBY) component of flx = E_{x} = -(v x B)_{x} = -(v2*b3 - v3*b2)
+        // (IBZ) component of flx = E_{z} = -(v x B)_{z} =  (v2*b1 - v1*b2)
         HLLD(eos_,indcs_,size_,coord_,m,k,j,i,IVY,wl.x2f,wr.x2f,bl.x2f,br.x2f,b0.x2f,uflx.x2f,e1x2,e3x2);
-    });
+      });
   }
 
   //--------------------------------------------------------------------------------------
   // k-direction. Note order of k,j loops switched
 
   if (pmy_pack->pmesh->three_d) {
-    auto &flx3_ = uflx.x3f;
-    auto &bz_ = b0.x3f;
-    auto &e23_ = e2x3;
-    auto &e13_ = e1x3;
-
     // set the loop limits
     kl = ks-1, ku = ke+1;
     if (use_fofc) { kl = ks-2, ku = ke+2; }
 
-    // Reconstruct qR[k] and qL[k+1], for both W and Bcc
-    par_for("mhd_recon_flux3",DevExeSpace(), 0, nmb1, kl, ku, jl, ju, il, iu,
+    par_for("mhd_flux3",DevExeSpace(), 0, nmb1, kl, ku, jl, ju, il, iu,
       KOKKOS_LAMBDA(int m, int k, int j, int i) {
+        // Reconstruct qR[k] and qL[k+1], for both W and Bcc
         PiecewiseLinear(m, k, j, i, w0_, wl.x3f, wr.x3f, 3);
         PiecewiseLinear(m, k, j, i, b0_, bl.x3f, br.x3f, 3);
-      });
-
-    // compute fluxes over [ks,ke+1].  MHD RS also computes electric fields, where
-        // (IBY) component of flx = E_{y} = -(v x B)_{y} = -(v3*b1 - v1*b3)
-        // (IBZ) component of flx = E_{x} = -(v x B)_{x} =  (v3*b2 - v2*b3)
-    par_for("mhd_rs_flux3",DevExeSpace(), 0, nmb1, kl, ku, jl, ju, il, iu,
-      KOKKOS_LAMBDA(int m, int k, int j, int i) {
         HLLD(eos_,indcs_,size_,coord_,m,k,j,i,IVZ,wl.x3f,wr.x3f,bl.x3f,br.x3f,b0.x3f,uflx.x3f,e2x3,e1x3);
-    });
+      });
   }
  
   return;
