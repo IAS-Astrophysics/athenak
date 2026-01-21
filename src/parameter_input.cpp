9d7c602b9be875f1743516324b6b9869539a7bfc
Yuyang HUA <yuyanghua@stu.pku.edu.cn>
2026-01-22 02:34:34 +0800

    fix: Add 'turb_driving' to allowed input blocks (#689)

--- src/parameter_input.cpp
+++ src/parameter_input.cpp
@@ -105,7 +105,7 @@ void ParameterInput::CheckBlockNames() {
     "coord", "adm", "shearing_box",
     "time", "problem", "output", "units",
     "hydro", "mhd", "ion-neutral", "radiation", "z4c", "z4c_amr", "cce",
-    "rad_srcterms", "hydro_srcterms", "mhd_srcterms", "particles"
+    "rad_srcterms", "hydro_srcterms", "mhd_srcterms", "particles", "turb_driving"
     };
 
   for (auto it1 = block.begin(); it1 != block.end(); ++it1) {
