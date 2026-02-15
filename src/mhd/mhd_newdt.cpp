9e1a98680b9a66022b952cdd38907f81f063433d
Minghao Guo <mhguo@princeton.edu>
2026-02-15 14:36:31 -0500

    Fix out-of-bounds access in MHD NewTimeStep for isothermal EOS
    - IdealGasPressure(w0_(m,IEN,...)) was evaluated unconditionally in the Newtonian MHD timestep kernel. For isothermal EOS, nmhd=4 so IEN=4 is out of bounds, causing segfaults on some MPI ranks.
    - Fix: restructure the ideal/isothermal paths so IEN is only accessed when eos.is_ideal.

--- src/mhd/mhd_newdt.cpp
+++ src/mhd/mhd_newdt.cpp
@@ -124,27 +124,22 @@ TaskStatus MHD::NewTimeStep(Driver *pdriver, int stage) {
         Real &w_by = bcc0_(m,IBY,k,j,i);
         Real &w_bz = bcc0_(m,IBZ,k,j,i);
         Real cf;
-        Real p = eos.IdealGasPressure(w0_(m,IEN,k,j,i));
         if (eos.is_ideal) {
+          Real p = eos.IdealGasPressure(w0_(m,IEN,k,j,i));
           cf = eos.IdealMHDFastSpeed(w_d, p, w_bx, w_by, w_bz);
+          max_dv1 = fabs(w0_(m,IVX,k,j,i)) + cf;
+          cf = eos.IdealMHDFastSpeed(w_d, p, w_by, w_bz, w_bx);
+          max_dv2 = fabs(w0_(m,IVY,k,j,i)) + cf;
+          cf = eos.IdealMHDFastSpeed(w_d, p, w_bz, w_bx, w_by);
+          max_dv3 = fabs(w0_(m,IVZ,k,j,i)) + cf;
         } else {
           cf = eos.IdealMHDFastSpeed(w_d, w_bx, w_by, w_bz);
-        }
-        max_dv1 = fabs(w0_(m,IVX,k,j,i)) + cf;
-
-        if (eos.is_ideal) {
-          cf = eos.IdealMHDFastSpeed(w_d, p, w_by, w_bz, w_bx);
-        } else {
+          max_dv1 = fabs(w0_(m,IVX,k,j,i)) + cf;
           cf = eos.IdealMHDFastSpeed(w_d, w_by, w_bz, w_bx);
-        }
-        max_dv2 = fabs(w0_(m,IVY,k,j,i)) + cf;
-
-        if (eos.is_ideal) {
-          cf = eos.IdealMHDFastSpeed(w_d, p, w_bz, w_bx, w_by);
-        } else {
+          max_dv2 = fabs(w0_(m,IVY,k,j,i)) + cf;
           cf = eos.IdealMHDFastSpeed(w_d, w_bz, w_bx, w_by);
+          max_dv3 = fabs(w0_(m,IVZ,k,j,i)) + cf;
         }
-        max_dv3 = fabs(w0_(m,IVZ,k,j,i)) + cf;
       }
 
       min_dt1 = fmin((mbsize.d_view(m).dx1/max_dv1), min_dt1);
