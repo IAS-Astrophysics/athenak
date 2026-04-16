3b3199573c2d919a08a1de398f134de0c9de2f0e
Oliver Steppohn <steppohnoliver3@gmail.com>
2026-04-16 16:31:38 +0200

    Add smoothing to C2P inversion

--- src/eos/primitive_solver_hyd.hpp
+++ src/eos/primitive_solver_hyd.hpp
@@ -317,6 +317,7 @@ class PrimitiveSolverHydro {
 
     // Some problem-specific parameters
     auto &excise = pmy_pack->pcoord->coord_data.bh_excise;
+    auto &smoothing = pmy_pack->pcoord->coord_data.smooth_excision;
     auto &excision_floor_ = pmy_pack->pcoord->excision_floor;
     auto &excision_flux_ = pmy_pack->pcoord->excision_flux;
     auto &dexcise_ = pmy_pack->pcoord->coord_data.dexcise;
@@ -419,27 +420,32 @@ class PrimitiveSolverHydro {
       // If we're in an excised region, set the primitives to some default value.
       Primitive::SolverResult result;
       if (excise) {
-        if (excision_floor_(m,k,j,i)) {
-          prim_pt[PRH] = dexcise_/mb;
-          prim_pt[PVX] = 0.0;
-          prim_pt[PVY] = 0.0;
-          prim_pt[PVZ] = 0.0;
-          prim_pt[PPR] = pexcise_;
-          for (int n = 0; n < nscal; n++) {
-            // FIXME: Particle abundances should probably be set to a
-            // default inside an excised region.
-            prim_pt[PYF + n] = cons_pt[CYD]/cons_pt[CDN];
-          }
-          prim_pt[PTM] =
-            eos_.GetTemperatureFromP(prim_pt[PRH], prim_pt[PPR], &prim_pt[PYF]);
-          result.error = Primitive::Error::SUCCESS;
-          result.iterations = 0;
-          result.cons_floor = false;
-          result.prim_floor = false;
-          result.cons_adjusted = true;
-          ps_.PrimToCon(prim_pt, cons_pt, b3u, g3d);
-        } else {
+        // If smooth excision is enabled, do C2P everywhere.
+        if (smoothing) {
           result = ps_.ConToPrim(prim_pt, cons_pt, b3u, g3d, g3u);
+        } else {
+          if (excision_floor_(m,k,j,i)) {
+            prim_pt[PRH] = dexcise_/mb;
+            prim_pt[PVX] = 0.0;
+            prim_pt[PVY] = 0.0;
+            prim_pt[PVZ] = 0.0;
+            prim_pt[PPR] = pexcise_;
+            for (int n = 0; n < nscal; n++) {
+              // FIXME: Particle abundances should probably be set to a
+              // default inside an excised region.
+              prim_pt[PYF + n] = cons_pt[CYD]/cons_pt[CDN];
+            }
+            prim_pt[PTM] =
+              eos_.GetTemperatureFromP(prim_pt[PRH], prim_pt[PPR], &prim_pt[PYF]);
+            result.error = Primitive::Error::SUCCESS;
+            result.iterations = 0;
+            result.cons_floor = false;
+            result.prim_floor = false;
+            result.cons_adjusted = true;
+            ps_.PrimToCon(prim_pt, cons_pt, b3u, g3d);
+          } else {
+            result = ps_.ConToPrim(prim_pt, cons_pt, b3u, g3d, g3u);
+          }
         }
       } else {
         result = ps_.ConToPrim(prim_pt, cons_pt, b3u, g3d, g3u);
