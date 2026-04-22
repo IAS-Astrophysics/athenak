2d5da298c3141f058fa61af8cadd47d8aa80e7ad
Hengrui Zhu <hz0693@princeton.edu>
2026-04-22 16:58:30 -0400

    Add linear-wave background-subtraction comparison notebook

--- src/pgen/tests/z4c_linear_wave.cpp
+++ src/pgen/tests/z4c_linear_wave.cpp
@@ -33,30 +33,9 @@ void LWRefinementCondition(MeshBlockPack* pmbp);
 namespace {
 // global variable to control computation of initial conditions versus errors
 bool set_initial_conditions = true;
-} // end anonymous namespace
 
-//----------------------------------------------------------------------------------------
-//! \fn void ProblemGenerator::Z4cLinearWave()
-//! \brief Sets initial conditions for gw linear wave tests
-
-void ProblemGenerator::Z4cLinearWave(ParameterInput *pin, const bool restart) {
-  pgen_final_func = Z4cLinearWaveErrors;
-  user_ref_func  = LWRefinementCondition;
-
-  if (restart)
-    return;
-
-  MeshBlockPack *pmbp = pmy_mesh_->pmb_pack;
-  if (pmbp->pz4c == nullptr) {
-    std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__ << std::endl
-              << "Z4c Wave test can only be run in Z4c, but no <z4c> block "
-              << "in input file" << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-  // Prepare Initial Data
-
-  // capture variables for the kernel
+template <typename Z4cState>
+void FillLinearWaveZ4c(MeshBlockPack *pmbp, Z4cState &state, ParameterInput *pin) {
   auto &indcs = pmbp->pmesh->mb_indcs;
   auto &size = pmbp->pmb->mb_size;
   int &is = indcs.is;
@@ -67,17 +46,146 @@ void ProblemGenerator::Z4cLinearWave(ParameterInput *pin, const bool restart) {
   int &ke = indcs.ke;
   auto &pz4c = pmbp->pz4c;
 
+  Real x1size = pmbp->pmesh->mesh_size.x1max - pmbp->pmesh->mesh_size.x1min;
+  Real x2size = pmbp->pmesh->mesh_size.x2max - pmbp->pmesh->mesh_size.x2min;
+  Real x3size = pmbp->pmesh->mesh_size.x3max - pmbp->pmesh->mesh_size.x3min;
+
+  Real amp = pin->GetOrAddReal("problem", "amp", 0.001);
+  Real kx1 = pin->GetOrAddReal("problem", "kx1", 1. / x1size);
+  Real kx2 = pin->GetOrAddReal("problem", "kx2", 1. / x2size);
+  Real kx3 = pin->GetOrAddReal("problem", "kx3", 1. / x3size);
+
+  Real knorm = sqrt(SQR(kx1) + SQR(kx2) + SQR(kx3));
+  Real theta = std::atan2(sqrt(kx2 * kx2 + kx1 * kx1), kx3);
+  Real phi = std::atan2(kx1, kx2);
+
+  Real axx = -SQR(cos(theta))*cos(2*phi)-SQR(cos(phi))*SQR(sin(theta));
+  Real axy = -0.25*(3+cos(2*theta))*sin(2*phi);
+  Real axz = -cos(theta)*sin(theta)*sin(phi);
+  Real ayy = SQR(cos(theta))*cos(2*phi)-SQR(sin(theta))*SQR(sin(phi));
+  Real ayz = cos(theta)*sin(theta)*cos(phi);
+  Real azz = SQR(sin(theta));
+
+  par_for("pgen_linwave1", DevExeSpace(), 0, (pmbp->nmb_thispack - 1),
+      ks, ke, js, je, is, ie, KOKKOS_LAMBDA(int m, int k, int j, int i) {
+        Real &x1min = size.d_view(m).x1min;
+        Real &x1max = size.d_view(m).x1max;
+        Real &x2min = size.d_view(m).x2min;
+        Real &x2max = size.d_view(m).x2max;
+        Real &x3min = size.d_view(m).x3min;
+        Real &x3max = size.d_view(m).x3max;
+
+        int nx1 = indcs.nx1;
+        int nx2 = indcs.nx2;
+        int nx3 = indcs.nx3;
+
+        Real x1v = CellCenterX(i - is, nx1, x1min, x1max);
+        Real x2v = CellCenterX(j - js, nx2, x2min, x2max);
+        Real x3v = CellCenterX(k - ks, nx3, x3min, x3max);
+        Real phase = 2 * M_PI * (kx1 * x1v + kx2 * x2v + kx3 * x3v);
+        Real sinkx = sin(phase);
+        Real coskx = knorm * M_PI * cos(phase);
+
+        state(m,pz4c->I_Z4C_GXX,k,j,i) = 1 + axx * amp * sinkx;
+        state(m,pz4c->I_Z4C_GXY,k,j,i) = axy * amp * sinkx;
+        state(m,pz4c->I_Z4C_GXZ,k,j,i) = axz * amp * sinkx;
+        state(m,pz4c->I_Z4C_GYY,k,j,i) = 1 + ayy * amp * sinkx;
+        state(m,pz4c->I_Z4C_GYZ,k,j,i) = ayz * amp * sinkx;
+        state(m,pz4c->I_Z4C_GZZ,k,j,i) = 1 + azz * amp * sinkx;
+
+        state(m,pz4c->I_Z4C_AXX,k,j,i) = axx * amp * coskx;
+        state(m,pz4c->I_Z4C_AXY,k,j,i) = axy * amp * coskx;
+        state(m,pz4c->I_Z4C_AXZ,k,j,i) = axz * amp * coskx;
+        state(m,pz4c->I_Z4C_AYY,k,j,i) = ayy * amp * coskx;
+        state(m,pz4c->I_Z4C_AYZ,k,j,i) = ayz * amp * coskx;
+        state(m,pz4c->I_Z4C_AZZ,k,j,i) = azz * amp * coskx;
+
+        state(m,pz4c->I_Z4C_ALPHA,k,j,i) = 1.0;
+        state(m,pz4c->I_Z4C_CHI,k,j,i) = 1.0;
+        state(m,pz4c->I_Z4C_KHAT,k,j,i) = 0.0;
+        state(m,pz4c->I_Z4C_THETA,k,j,i) = 0.0;
+        state(m,pz4c->I_Z4C_GAMX,k,j,i) = 0.0;
+        state(m,pz4c->I_Z4C_GAMY,k,j,i) = 0.0;
+        state(m,pz4c->I_Z4C_GAMZ,k,j,i) = 0.0;
+        state(m,pz4c->I_Z4C_BETAX,k,j,i) = 0.0;
+        state(m,pz4c->I_Z4C_BETAY,k,j,i) = 0.0;
+        state(m,pz4c->I_Z4C_BETAZ,k,j,i) = 0.0;
+        state(m,pz4c->I_Z4C_BX,k,j,i) = 0.0;
+        state(m,pz4c->I_Z4C_BY,k,j,i) = 0.0;
+        state(m,pz4c->I_Z4C_BZ,k,j,i) = 0.0;
+      });
+}
+
+template <typename ADMState>
+void FillMinkowskiADM(MeshBlockPack *pmbp, ADMState &adm_state) {
+  auto &indcs = pmbp->pmesh->mb_indcs;
+  int isg = indcs.is - indcs.ng;
+  int ieg = indcs.ie + indcs.ng;
+  int jsg = indcs.js - indcs.ng;
+  int jeg = indcs.je + indcs.ng;
+  int ksg = indcs.ks - indcs.ng;
+  int keg = indcs.ke + indcs.ng;
+
+  par_for("pgen_flat_background", DevExeSpace(), 0, pmbp->nmb_thispack - 1,
+          ksg, keg, jsg, jeg, isg, ieg,
+          KOKKOS_LAMBDA(int m, int k, int j, int i) {
+    adm_state.g_dd(m,0,0,k,j,i) = 1.0;
+    adm_state.g_dd(m,0,1,k,j,i) = 0.0;
+    adm_state.g_dd(m,0,2,k,j,i) = 0.0;
+    adm_state.g_dd(m,1,1,k,j,i) = 1.0;
+    adm_state.g_dd(m,1,2,k,j,i) = 0.0;
+    adm_state.g_dd(m,2,2,k,j,i) = 1.0;
+
+    adm_state.vK_dd(m,0,0,k,j,i) = 0.0;
+    adm_state.vK_dd(m,0,1,k,j,i) = 0.0;
+    adm_state.vK_dd(m,0,2,k,j,i) = 0.0;
+    adm_state.vK_dd(m,1,1,k,j,i) = 0.0;
+    adm_state.vK_dd(m,1,2,k,j,i) = 0.0;
+    adm_state.vK_dd(m,2,2,k,j,i) = 0.0;
+
+    adm_state.psi4(m,k,j,i) = 1.0;
+    adm_state.alpha(m,k,j,i) = 1.0;
+    adm_state.beta_u(m,0,k,j,i) = 0.0;
+    adm_state.beta_u(m,1,k,j,i) = 0.0;
+    adm_state.beta_u(m,2,k,j,i) = 0.0;
+  });
+}
+
+void SetADMBackgroundFlat(MeshBlockPack *pmbp, Real /*time*/) {
+  FillMinkowskiADM(pmbp, pmbp->pz4c->adm_bg);
+}
+} // end anonymous namespace
+
+//----------------------------------------------------------------------------------------
+//! \fn void ProblemGenerator::Z4cLinearWave()
+//! \brief Sets initial conditions for gw linear wave tests
+
+void ProblemGenerator::Z4cLinearWave(ParameterInput *pin, const bool restart) {
+  pgen_final_func = Z4cLinearWaveErrors;
+  user_ref_func  = LWRefinementCondition;
+
+  MeshBlockPack *pmbp = pmy_mesh_->pmb_pack;
+  if (pmbp->pz4c != nullptr && pmbp->pz4c->use_analytic_background) {
+    pmbp->pz4c->SetADMBackground = &SetADMBackgroundFlat;
+  }
+
+  if (restart)
+    return;
+
+  if (pmbp->pz4c == nullptr) {
+    std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__ << std::endl
+              << "Z4c Wave test can only be run in Z4c, but no <z4c> block "
+              << "in input file" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  auto &pz4c = pmbp->pz4c;
+
   // Code below will automatically calculate wavevector along grid diagonal, imposing the
   // conditions of periodicity and exactly one wavelength along each grid direction
   Real x1size = pmy_mesh_->mesh_size.x1max - pmy_mesh_->mesh_size.x1min;
   Real x2size = pmy_mesh_->mesh_size.x2max - pmy_mesh_->mesh_size.x2min;
   Real x3size = pmy_mesh_->mesh_size.x3max - pmy_mesh_->mesh_size.x3min;
-
-  // Wave amplitude
-  Real amp = pin->GetOrAddReal("problem", "amp", 0.001);
-
-  // compute solution in u1 register. For initial conditions, set u1 -> u0.
-  auto &u1 = (set_initial_conditions)? pmbp->pz4c->u0 : pmbp->pz4c->u1;
 
   // Initialize wavevector
   Real kx1 = pin->GetOrAddReal("problem", "kx1", 1. / x1size);
@@ -102,57 +210,18 @@ void ProblemGenerator::Z4cLinearWave(ParameterInput *pin, const bool restart) {
     pin->SetReal("time", "tlim", tlim*lambda);
   }
 
-  // rotated weight for each tensor element
-  Real axx, axy, axz, ayy, ayz, azz;
-  axx = -SQR(cos(theta))*cos(2*phi)-SQR(cos(phi))*SQR(sin(theta));
-  axy = -0.25*(3+cos(2*theta))*sin(2*phi);
-  axz = -cos(theta)*sin(theta)*sin(phi);
-  ayy = SQR(cos(theta))*cos(2*phi)-SQR(sin(theta))*SQR(sin(phi));
-  ayz = cos(theta)*sin(theta)*cos(phi);
-  azz = SQR(sin(theta));
-
-  par_for("pgen_linwave1", DevExeSpace(), 0, (pmbp->nmb_thispack - 1),
-      ks, ke, js, je, is, ie, KOKKOS_LAMBDA(int m, int k, int j, int i) {
-        Real &x1min = size.d_view(m).x1min;
-        Real &x1max = size.d_view(m).x1max;
-        Real &x2min = size.d_view(m).x2min;
-        Real &x2max = size.d_view(m).x2max;
-        Real &x3min = size.d_view(m).x3min;
-        Real &x3max = size.d_view(m).x3max;
-
-        int nx1 = indcs.nx1;
-        int nx2 = indcs.nx2;
-        int nx3 = indcs.nx3;
-
-        Real x1v = CellCenterX(i - is, nx1, x1min, x1max);
-        Real x2v = CellCenterX(j - js, nx2, x2min, x2max);
-        Real x3v = CellCenterX(k - ks, nx3, x3min, x3max);
-        Real sinkx = sin(2 * M_PI * (kx1 * x1v + kx2 * x2v + kx3 * x3v));
-        Real coskx = knorm * M_PI * cos(2 * M_PI * (kx1 * x1v + kx2 * x2v + kx3 * x3v));
-
-        u1(m,pz4c->I_Z4C_GXX,k,j,i) = 1 + axx * amp * sinkx;
-        u1(m,pz4c->I_Z4C_GXY,k,j,i) = axy * amp * sinkx;
-        u1(m,pz4c->I_Z4C_GXZ,k,j,i) = axz * amp * sinkx;
-        u1(m,pz4c->I_Z4C_GYY,k,j,i) = 1 + ayy * amp * sinkx;
-        u1(m,pz4c->I_Z4C_GYZ,k,j,i) = ayz * amp * sinkx;
-        u1(m,pz4c->I_Z4C_GZZ,k,j,i) = 1 + azz * amp * sinkx;
-
-        u1(m,pz4c->I_Z4C_AXX,k,j,i) = axx * amp * coskx;
-        u1(m,pz4c->I_Z4C_AXY,k,j,i) = axy * amp * coskx;
-        u1(m,pz4c->I_Z4C_AXZ,k,j,i) = axz * amp * coskx;
-        u1(m,pz4c->I_Z4C_AYY,k,j,i) = ayy * amp * coskx;
-        u1(m,pz4c->I_Z4C_AYZ,k,j,i) = ayz * amp * coskx;
-        u1(m,pz4c->I_Z4C_AZZ,k,j,i) = azz * amp * coskx;
-
-        u1(m,pz4c->I_Z4C_ALPHA,k,j,i) = 1;
-        u1(m,pz4c->I_Z4C_CHI,k,j,i) = 1;
-        u1(m,pz4c->I_Z4C_KHAT,k,j,i) = 0;
-        u1(m,pz4c->I_Z4C_THETA,k,j,i) = 0;
-
-        u1(m,pz4c->I_Z4C_GAMX,k,j,i) = 0;
-        u1(m,pz4c->I_Z4C_GAMY,k,j,i) = 0;
-        u1(m,pz4c->I_Z4C_GAMZ,k,j,i) = 0;
-      });
+  if (set_initial_conditions) {
+    if (pz4c->use_analytic_background && pz4c->SetADMBackground != nullptr) {
+      FillLinearWaveZ4c(pmbp, pz4c->u_full, pin);
+      pz4c->UpdateBackgroundState(pmbp->pmesh->time);
+      pz4c->RecastResidualState();
+      pz4c->PrescribeGaugeResidual();
+    } else {
+      FillLinearWaveZ4c(pmbp, pz4c->u0, pin);
+    }
+  } else {
+    FillLinearWaveZ4c(pmbp, pz4c->u1, pin);
+  }
   return;
 }
 
@@ -184,8 +253,14 @@ void Z4cLinearWaveErrors(ParameterInput *pin, Mesh *pm) {
   if (pmbp->pz4c != nullptr) {
     nvars = 6; // 6 metric components
     auto &pz4c = pmbp->pz4c;
-    auto &u0_ = pmbp->pz4c->u0;
+    auto &u0_ = (pz4c->use_analytic_background && pz4c->SetADMBackground != nullptr) ?
+                pz4c->u_full : pz4c->u0;
     auto &u1_ = pmbp->pz4c->u1;
+
+    if (pz4c->use_analytic_background && pz4c->SetADMBackground != nullptr) {
+      pz4c->UpdateBackgroundState(pm->time);
+      pz4c->ReconstructFullState();
+    }
 
     const int nmkji = (pmbp->nmb_thispack)*nx3*nx2*nx1;
     const int nkji = nx3*nx2*nx1;
