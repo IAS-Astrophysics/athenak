0af33ad30442e6c2de0c4152f93de365e3068e21
Hengrui Zhu <hz0693@princeton.edu>
2026-04-26 13:59:57 -0400

    Add ADM geometric source for dynamical radiation

--- src/dyn_radiation/dyn_radiation_update.cpp
+++ src/dyn_radiation/dyn_radiation_update.cpp
@@ -12,6 +12,7 @@
 #include "athena.hpp"
 #include "mesh/mesh.hpp"
 #include "driver/driver.hpp"
+#include "coordinates/adm.hpp"
 #include "coordinates/cartesian_ks.hpp"
 #include "coordinates/coordinates.hpp"
 #include "coordinates/cell_locations.hpp"
@@ -55,10 +56,63 @@ TaskStatus DynRadiation::RKUpdate(Driver *pdriver, int stage) {
   auto &angular_fluxes_ = angular_fluxes;
   auto &divfa_ = divfa;
   bool use_adm_geometry_ = use_adm_geometry;
+  bool adm_metric_source_ = adm_metric_source;
 
   auto &excise = pmy_pack->pcoord->coord_data.bh_excise;
   auto &rad_mask_ = pmy_pack->pcoord->excision_floor;
   Real &n_0_floor_ = n_0_floor;
+
+  if (use_adm_geometry_) {
+    auto &adm_ = pmy_pack->padm->adm;
+    par_for("dynrad_adm_update",DevExeSpace(),0,nmb1,0,nang1,ks,ke,js,je,is,ie,
+    KOKKOS_LAMBDA(int m, int n, int k, int j, int i) {
+      Real divf_s = (flx1(m,n,k,j,i+1) - flx1(m,n,k,j,i))/mbsize.d_view(m).dx1;
+      if (multi_d) {
+        divf_s += (flx2(m,n,k,j+1,i) - flx2(m,n,k,j,i))/mbsize.d_view(m).dx2;
+      }
+      if (three_d) {
+        divf_s += (flx3(m,n,k+1,j,i) - flx3(m,n,k,j,i))/mbsize.d_view(m).dx3;
+      }
+      i0_(m,n,k,j,i) = gam0*i0_(m,n,k,j,i)+gam1*i1_(m,n,k,j,i)-beta_dt*divf_s;
+      if (angular_fluxes_) { i0_(m,n,k,j,i) -= beta_dt*divfa_(m,n,k,j,i); }
+
+      if (adm_metric_source_) {
+        Real s[3] = {0.0, 0.0, 0.0};
+        for (int a=0; a<3; ++a) {
+          for (int d=0; d<3; ++d) {
+            s[d] += tt(m,a+1,d+1,k,j,i)*nh_c_.d_view(n,a+1);
+          }
+        }
+
+        Real grad_alpha[3] = {0.0, 0.0, 0.0};
+        grad_alpha[0] = 0.5*(adm_.alpha(m,k,j,i+1) - adm_.alpha(m,k,j,i-1))/
+                        mbsize.d_view(m).dx1;
+        if (multi_d) {
+          grad_alpha[1] = 0.5*(adm_.alpha(m,k,j+1,i) - adm_.alpha(m,k,j-1,i))/
+                          mbsize.d_view(m).dx2;
+        }
+        if (three_d) {
+          grad_alpha[2] = 0.5*(adm_.alpha(m,k+1,j,i) - adm_.alpha(m,k-1,j,i))/
+                          mbsize.d_view(m).dx3;
+        }
+
+        Real kss = 0.0;
+        Real sdalpha = 0.0;
+        for (int a=0; a<3; ++a) {
+          sdalpha += s[a]*grad_alpha[a];
+          for (int b=0; b<3; ++b) {
+            kss += adm_.vK_dd(m,a,b,k,j,i)*s[a]*s[b];
+          }
+        }
+        Real geom = adm_.alpha(m,k,j,i)*kss - sdalpha;
+        i0_(m,n,k,j,i) += beta_dt*i0_(m,n,k,j,i)*geom;
+      }
+
+      i0_(m,n,k,j,i) = fmax(i0_(m,n,k,j,i), 0.0);
+      if (excise && rad_mask_(m,k,j,i)) { i0_(m,n,k,j,i) = 0.0; }
+    });
+    return TaskStatus::complete;
+  }
 
   par_for("r_update",DevExeSpace(),0,nmb1,0,nang1,ks,ke,js,je,is,ie,
   KOKKOS_LAMBDA(int m, int n, int k, int j, int i) {
@@ -76,24 +130,20 @@ TaskStatus DynRadiation::RKUpdate(Driver *pdriver, int stage) {
     if (angular_fluxes_) { i0_(m,n,k,j,i) -= beta_dt*divfa_(m,n,k,j,i); }
 
     Real n_0 = 1.0;
-    if (use_adm_geometry_) {
-      i0_(m,n,k,j,i) = fmax(i0_(m,n,k,j,i), 0.0);
-    } else {
-      // zero intensity if negative
-      Real n0  = tt(m,0,0,k,j,i);
-      n_0 = tc(m,0,0,k,j,i)*nh_c_.d_view(n,0) +
-            tc(m,1,0,k,j,i)*nh_c_.d_view(n,1) +
-            tc(m,2,0,k,j,i)*nh_c_.d_view(n,2) +
-            tc(m,3,0,k,j,i)*nh_c_.d_view(n,3);
-      i0_(m,n,k,j,i) = n0*n_0*fmax((i0_(m,n,k,j,i)/(n0*n_0)), 0.0);
-    }
+    // zero intensity if negative
+    Real n0  = tt(m,0,0,k,j,i);
+    n_0 = tc(m,0,0,k,j,i)*nh_c_.d_view(n,0) +
+          tc(m,1,0,k,j,i)*nh_c_.d_view(n,1) +
+          tc(m,2,0,k,j,i)*nh_c_.d_view(n,2) +
+          tc(m,3,0,k,j,i)*nh_c_.d_view(n,3);
+    i0_(m,n,k,j,i) = n0*n_0*fmax((i0_(m,n,k,j,i)/(n0*n_0)), 0.0);
 
     // handle excision
     // NOTE(@pdmullen): exicision criterion are not finalized.  The below zeroes all
     // intensities within rks <= 1.0 and zeroes intensities within angles where n_0
     // is about zero.  This needs future attention.
     if (excise) {
-      if (rad_mask_(m,k,j,i) || (!use_adm_geometry_ && fabs(n_0) < n_0_floor_)) {
+      if (rad_mask_(m,k,j,i) || fabs(n_0) < n_0_floor_) {
         i0_(m,n,k,j,i) = 0.0;
       }
     }
