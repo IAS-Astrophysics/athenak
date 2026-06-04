2e06912da8bb92cebdb3c1a1d9bb017150eeb08c
yiqiu0714 <yfq5054@psu.edu>
2026-06-04 18:38:21 +0000

    Fixed duplicated z4c bc options

--- src/z4c/z4c_Sbc.cpp
+++ src/z4c/z4c_Sbc.cpp
@@ -157,7 +157,6 @@ TaskStatus Z4c::Z4cBoundaryRHS(Driver *pdriver, int stage) {
       switch(mb_bcs.d_view(m,BoundaryFace::inner_x1)) {
         case BoundaryFlag::vacuum:
         case BoundaryFlag::diode:
-        case BoundaryFlag::vacuum:
         case BoundaryFlag::outflow:
             Z4cSommerfeld(z4c_, rhs_, indcs, size, m, k, j, is);
           break;
@@ -173,7 +172,6 @@ TaskStatus Z4c::Z4cBoundaryRHS(Driver *pdriver, int stage) {
       switch (mb_bcs.d_view(m,BoundaryFace::outer_x1)) {
         case BoundaryFlag::vacuum:
         case BoundaryFlag::diode:
-        case BoundaryFlag::vacuum:
         case BoundaryFlag::outflow:
             Z4cSommerfeld(z4c_, rhs_, indcs, size, m, k, j, ie);
           break;
@@ -201,7 +199,6 @@ TaskStatus Z4c::Z4cBoundaryRHS(Driver *pdriver, int stage) {
       switch(mb_bcs.d_view(m,BoundaryFace::inner_x2)) {
         case BoundaryFlag::vacuum:
         case BoundaryFlag::diode:
-        case BoundaryFlag::vacuum:
         case BoundaryFlag::outflow:
             Z4cSommerfeld(z4c_, rhs_, indcs, size, m, k, js, i);
           break;
@@ -217,7 +214,6 @@ TaskStatus Z4c::Z4cBoundaryRHS(Driver *pdriver, int stage) {
       switch (mb_bcs.d_view(m,BoundaryFace::outer_x2)) {
         case BoundaryFlag::vacuum:
         case BoundaryFlag::diode:
-        case BoundaryFlag::vacuum:
         case BoundaryFlag::outflow:
             Z4cSommerfeld(z4c_, rhs_, indcs, size, m, k, je, i);
           break;
@@ -245,7 +241,6 @@ TaskStatus Z4c::Z4cBoundaryRHS(Driver *pdriver, int stage) {
       switch(mb_bcs.d_view(m,BoundaryFace::inner_x3)) {
         case BoundaryFlag::vacuum:
         case BoundaryFlag::diode:
-        case BoundaryFlag::vacuum:
         case BoundaryFlag::outflow:
             Z4cSommerfeld(z4c_, rhs_, indcs, size, m, ks, j, i);
           break;
@@ -261,7 +256,6 @@ TaskStatus Z4c::Z4cBoundaryRHS(Driver *pdriver, int stage) {
       switch (mb_bcs.d_view(m,BoundaryFace::outer_x3)) {
         case BoundaryFlag::vacuum:
         case BoundaryFlag::diode:
-        case BoundaryFlag::vacuum:
         case BoundaryFlag::outflow:
             Z4cSommerfeld(z4c_, rhs_, indcs, size, m, ke, j, i);
           break;
