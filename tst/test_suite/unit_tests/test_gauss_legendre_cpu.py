9b43f1d8c2b95ee6d342e496191fdf8eafba97aa
James Stone <jmstone@ias.edu>
2026-07-01 12:00:48 -0400

    Fixed style errors.  Removed deprecated python script.

--- tst/test_suite/unit_tests/test_gauss_legendre_cpu.py
+++ tst/test_suite/unit_tests/test_gauss_legendre_cpu.py
@@ -3,11 +3,9 @@ Unit tests for EOSCompOSE
 """
 
 # Modules
-import pytest
 import test_suite.testutils as testutils
-import numpy as np
 
 
 def test_gauss_legendre():
-   input_file = "inputs/ut_gauss_legendre.athinput"
-   testutils.run(input_file)
+    input_file = "inputs/ut_gauss_legendre.athinput"
+    testutils.run(input_file)
--- tst/test_suite/unit_tests/test_ut_compose_cpu.py
+++ tst/test_suite/unit_tests/test_ut_compose_cpu.py
@@ -3,14 +3,13 @@ Unit tests for EOSCompOSE
 """
 
 # Modules
-import pytest
 import test_suite.testutils as testutils
-import numpy as np
 
 
 def test_logs():
-   input_file = "inputs/ut_compose_log.athinput"
-   testutils.run(input_file)
+    input_file = "inputs/ut_compose_log.athinput"
+    testutils.run(input_file)
+
 
 def test_NQTs():
     input_file = "inputs/ut_compose_NQT.athinput"
